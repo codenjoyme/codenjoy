d39558919ef37bcd5f33d3b1162bf235d4fabf0b
sergey <somemail@some.com>
2012-03-25 00:48:43 +0200

    new cases for glass

--- src/main/java/net/tetris/dom/TetrisGlass.java
+++ src/main/java/net/tetris/dom/TetrisGlass.java
@@ -12,29 +12,67 @@ public class TetrisGlass implements Glass {
     }
 
     public boolean accept(Figure figure, int x, int y) {
-        if (x - figure.getLeft() < 0) {
+        if (isOutside(figure, x, y)) {
             return false;
         }
-        if (x + figure.getRight() > width) {
-            return false;
-        }
-        if (y - figure.getBottom() < 0) {
-            return false;
-        }
+
         int[] alignedRows = alignFigureRowCoordinatesWithGlass(figure, x);
         boolean isOccupied = false;
         for (int i = 0; i < alignedRows.length; i++) {
             int alignedRow = alignedRows[i];
-            isOccupied |= (occupied[y - i + figure.getTop()] & alignedRow) > 0;
+            int rowPosition = y - i + figure.getTop();
+            if (rowPosition >= height) {
+                continue;
+            }
+            isOccupied |= (occupied[rowPosition] & alignedRow) > 0;
         }
         return !isOccupied;
     }
 
+    private boolean isOutside(Figure figure, int x, int y) {
+        if (isOutsideLeft(figure, x)) {
+            return true;
+        }
+        if (isOutsideRight(figure, x)) {
+            return true;
+        }
+        if (isOutsideBottom(figure, y)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isOutsideBottom(Figure figure, int y) {
+        return y - figure.getBottom() < 0;
+    }
+
+    private boolean isOutsideLeft(Figure figure, int x) {
+        return x - figure.getLeft() < 0;
+    }
+
+    private boolean isOutsideRight(Figure figure, int x) {
+        return x + figure.getRight() >= width;
+    }
+
     public void drop(Figure figure, int x, int y) {
+        if (isOutside(figure, x, y)) {
+            return;
+        }
+        int position = findAvailableYPosition(figure, x, y) - figure.getBottom();
+        if (position >= height) {
+            return;
+        }
         int[] alignedRows = alignFigureRowCoordinatesWithGlass(figure, x);
         for (int i = 0; i < alignedRows.length; i++) {
-            occupied[alignedRows.length - i - 1] |= alignedRows[i];
+            occupied[position + alignedRows.length - i - 1] |= alignedRows[i];
         }
+    }
+
+    private int findAvailableYPosition(Figure figure, int x, int y) {
+        int position = y;
+        while (accept(figure, x, --position)){}
+        position++;
+        return position;
     }
 
     private int[] alignFigureRowCoordinatesWithGlass(Figure figure, int x) {
@@ -48,5 +86,14 @@ public class TetrisGlass implements Glass {
 
     public void empty() {
 
+    }
+
+    public boolean isEmpty() {
+        for (int anOccupied : occupied) {
+            if (anOccupied != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/net/tetris/dom/TetrisGlassTest.java
+++ src/test/java/net/tetris/dom/TetrisGlassTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 
 public class TetrisGlassTest {
 
@@ -21,7 +22,7 @@ public class TetrisGlassTest {
     }
 
     @Test
-    public void shouldAcceptWhenEmpty(){
+    public void shouldAcceptWhenEmpty() {
         assertTrue(glass.accept(new TetrisFigure(), 1, 1));
     }
 
@@ -33,50 +34,50 @@ public class TetrisGlassTest {
     }
 
     @Test
-    public void shouldPerformDropWhenDropRequested(){
+    public void shouldPerformDropWhenDropRequested() {
         glass.drop(new TetrisFigure(), 0, HEIGHT);
 
         assertFalse(glass.accept(new TetrisFigure(), 0, 0));
     }
 
     @Test
-    public void shouldRejectWhenFigurePartlyOutsideFromLeft(){
+    public void shouldRejectWhenFigurePartlyOutsideFromLeft() {
         assertFalse(glass.accept(new TetrisFigure(1, 0, "##"), 0, HEIGHT));
     }
 
     @Test
-    public void shouldRejectWhenFigurePartlyOutsideFromRight(){
+    public void shouldRejectWhenFigurePartlyOutsideFromRight() {
         assertFalse(glass.accept(new TetrisFigure(0, 0, "##"), WIDTH, HEIGHT));
     }
 
     @Test
-    public void shouldRejectWhenFigurePartlyOutsideBottom(){
+    public void shouldRejectWhenFigurePartlyOutsideBottom() {
         assertFalse(glass.accept(new TetrisFigure(0, 0, "#", "#"), WIDTH, 0));
     }
 
     @Test
-    public void shouldRejectWhenFigurePartlyOverDroppedRow(){
+    public void shouldRejectWhenFigurePartlyOverDroppedRow() {
         TetrisFigure figure = new TetrisFigure(1, 0, "##");
 
-        glass.drop(figure, WIDTH/2, HEIGHT);
-        
-        assertFalse(glass.accept(figure, WIDTH/2 + 1, 0));
-        assertFalse(glass.accept(figure, WIDTH/2, 0));
-        assertFalse(glass.accept(figure, WIDTH/2 - 1, 0));
+        glass.drop(figure, WIDTH / 2, HEIGHT);
+
+        assertFalse(glass.accept(figure, WIDTH / 2 + 1, 0));
+        assertFalse(glass.accept(figure, WIDTH / 2, 0));
+        assertFalse(glass.accept(figure, WIDTH / 2 - 1, 0));
     }
 
     @Test
-    public void shouldAcceptWhenFigureAboveDropped(){
-        glass.drop(new TetrisFigure(1, 0, "##"), WIDTH/2, HEIGHT);
+    public void shouldAcceptWhenFigureAboveDropped() {
+        glass.drop(new TetrisFigure(1, 0, "##"), WIDTH / 2, HEIGHT);
 
         assertTrue(glass.accept(new TetrisFigure(1, 0, "##"), WIDTH / 2, 1));
     }
 
     @Test
-    public void shouldRejectWhenFigurePartlyOverDroppedColumn(){
+    public void shouldRejectWhenFigurePartlyOverDroppedColumn() {
         TetrisFigure figure = new TetrisFigure(0, 1, "#", "#");
 
-        glass.drop(figure, WIDTH/2, HEIGHT);
+        glass.drop(figure, WIDTH / 2, HEIGHT);
 
         assertFalse(glass.accept(figure, WIDTH / 2, 0));
         assertFalse(glass.accept(figure, WIDTH / 2, 1));
@@ -84,31 +85,72 @@ public class TetrisGlassTest {
     }
 
     @Test
-    public void shouldRejectWhenFigureOverlapDroppedColumnByBottomSize(){
+    public void shouldRejectWhenFigureOverlapDroppedColumnByBottomSize() {
         TetrisFigure figure = new TetrisFigure(0, 0, "#", "#");
 
-        glass.drop(figure, WIDTH/2, HEIGHT);
+        glass.drop(figure, WIDTH / 2, HEIGHT);
 
         assertFalse(glass.accept(figure, WIDTH / 2, 2));
     }
 
     @Test
-    public void shouldRejectWhenFigureIsNotSymmetric(){
+    public void shouldRejectWhenFigureIsNotSymmetric() {
         TetrisFigure figure = new TetrisFigure(1, 0, "##", " #");
 
-        glass.drop(figure, WIDTH/2, HEIGHT);
+        glass.drop(figure, WIDTH / 2, HEIGHT);
 
         assertFalse(glass.accept(point, WIDTH / 2 - 1, 1));
     }
 
     @Test
-    public void shouldRejectWhenFigureIsNotSymmetricPositive(){
+    public void shouldRejectWhenFigureIsNotSymmetricPositive() {
         TetrisFigure figure = new TetrisFigure(1, 0, "##", " #");
 
-        glass.drop(figure, WIDTH/2, HEIGHT);
+        glass.drop(figure, WIDTH / 2, HEIGHT);
 
         assertTrue(glass.accept(figure, WIDTH / 2 + 1, 2));
     }
 
+    @Test
+    public void shouldRejectWhenFigureDroppedToFilledGlass() {
+        glass.drop(point, 0, HEIGHT);
 
+        glass.drop(point, 0, HEIGHT);
+
+        assertFalse(glass.accept(point, 0, 1));
+    }
+
+
+    @Test
+    public void shouldIgnoreWhenDropOutside() {
+        glass.drop(new TetrisFigure(0, 0, "##"), -1, HEIGHT - 1);
+
+        assertTrue(glass.accept(point, 0, 0));
+    }
+
+    @Test
+    public void shouldIgnoreWhenOutOfBoundsLeft() {
+        glass.drop(new TetrisFigure(1, 0, "##"), 0, HEIGHT - 1);
+
+        assertTrue(glass.isEmpty());
+    }
+
+    @Test
+    public void shouldIgnoreWhenOutOfBoundsRightPartially() {
+        glass.drop(new TetrisFigure(0, 0, "##"), WIDTH - 1, HEIGHT - 1);
+
+        assertTrue(glass.isEmpty());
+    }
+
+    @Test
+    public void shouldIgnoreWhenOutOfBoundsBottom() {
+        glass.drop(new TetrisFigure(0, 0, "#", "#"), 0, 0);
+
+        assertTrue(glass.isEmpty());
+    }
+
+    @Test
+    public void shouldRejectWhenPartiallyOutsideOnRight() {
+        assertFalse(glass.accept(new TetrisFigure(0, 0, "##"), WIDTH - 1, 0));
+    }
 }
