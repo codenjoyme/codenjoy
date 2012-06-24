19a8adf38f63916e09349f14b2e17058d6b945df
sergey <somemail@some.com>
2012-06-24 12:51:17 +0300

    test commit

--- src/main/java/net/tetris/web/test/TestServlet.java
+++ src/main/java/net/tetris/web/test/TestServlet.java
@@ -37,6 +37,6 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        //test
     }
 }
