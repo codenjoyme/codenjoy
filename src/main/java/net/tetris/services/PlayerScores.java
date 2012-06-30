9f0a187ce1b75a471f3a92956b471e16f535b186
sergey <somemail@some.com>
2012-06-30 11:52:29 +0300

    Score calculation implemented

--- src/main/java/net/tetris/services/PlayerScores.java
+++ src/main/java/net/tetris/services/PlayerScores.java
@@ -5,15 +5,42 @@ import net.tetris.dom.ScoreBoard;
 
 public class PlayerScores implements ScoreBoard {
 
+    private volatile int score;
+
     @Override
     public void glassOverflown() {
+        int delta = score - 2000;
+        score = delta;
     }
 
     @Override
     public void linesRemoved(int amount) {
+        int delta = 0;
+        switch (amount) {
+            case 1:
+                delta = 100;
+                break;
+            case 2:
+                delta = 300;
+                break;
+            case 3:
+                delta = 700;
+                break;
+            case 4:
+                delta = 1500;
+                break;
+        }
+        delta += score;
+        score = delta;
     }
 
     @Override
     public void figureDropped(Figure figure) {
+        int delta = score + 10;
+        score = delta;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
---
+++ src/test/java/net/tetris/services/PlayerScoresTest.java
@@ -0,0 +1,79 @@
+package net.tetris.services;
+
+import net.tetris.dom.TetrisFigure;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class PlayerScoresTest {
+
+    private PlayerScores playerScores;
+
+    @Before
+    public void setUp() throws Exception {
+        playerScores = new PlayerScores();
+    }
+
+    @Test
+    public void shouldCalcScoreWhen1LineRemoved(){
+        playerScores.linesRemoved(1);
+
+        assertEquals(100, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldCalcScoreWhen2LineRemoved(){
+        playerScores.linesRemoved(2);
+
+        assertEquals(300, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldCalcScoreWhen3LineRemoved(){
+        playerScores.linesRemoved(3);
+
+        assertEquals(700, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldCalcScoreWhen4LineRemoved(){
+        playerScores.linesRemoved(4);
+
+        assertEquals(1500, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldAccumulateScoreWhenLineRemoved(){
+        playerScores.linesRemoved(1);
+        playerScores.linesRemoved(3);
+
+        assertEquals(100 + 700, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldCalculateGlassOwerFlow(){
+        playerScores.glassOverflown();
+
+        assertEquals(-2000, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldAccumulateGlassOwerFlow(){
+        playerScores.linesRemoved(1);
+
+        playerScores.glassOverflown();
+
+        assertEquals(100 - 2000, playerScores.getScore());
+    }
+
+    @Test
+    public void shouldCalculateWhenFigureDropped(){
+        playerScores.linesRemoved(1);
+
+        playerScores.figureDropped(new TetrisFigure(0,0, "#"));
+
+        assertEquals(100 + 10, playerScores.getScore());
+    }
+}
