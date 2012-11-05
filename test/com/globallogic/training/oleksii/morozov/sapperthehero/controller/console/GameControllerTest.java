82441f8ad3e65ecf5ac36872851f4d9689543f50
morozovmipt@gmail.com <morozovmipt@gmail.com@46c6ed03-7e28-42c9-d97a-c6df3b90a356>
2012-11-05 12:51:23 +0000

    подправил тесты.

--- test/com/globallogic/training/oleksii/morozov/sapperthehero/controller/console/GameControllerTest.java
+++ test/com/globallogic/training/oleksii/morozov/sapperthehero/controller/console/GameControllerTest.java
@@ -1,30 +1,31 @@
 package com.globallogic.training.oleksii.morozov.sapperthehero.controller.console;
 
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import com.globallogic.training.oleksii.morozov.sapperthehero.controller.console.input.Reader;
 import com.globallogic.training.oleksii.morozov.sapperthehero.controller.console.output.Printer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GameControllerTest {
-	private Reader input = mock(Reader.class);
-
-	private Printer printer = mock(Printer.class);
+	private Reader input;
+	private Printer printer;
 	private GameController gameController;
 
-	@Before
-	public void setUp() {
-		gameController = new GameController(printer, input);
-	}
-
 	@Test
-	public void testGameController() {
-		verify(gameController);
+	public void shouldGameController() {
+		// given
+		input = mock(Reader.class);
+		printer = mock(Printer.class);
+		// when
+		gameController = new GameController(printer, input);
+		// then
+		verify(input).setPrinter(printer);
+		assertNotNull(gameController);
 	}
 
 }
--- test/com/globallogic/training/oleksii/morozov/sapperthehero/controller/console/input/ConsoleReaderTest.java
+++ test/com/globallogic/training/oleksii/morozov/sapperthehero/controller/console/input/ConsoleReaderTest.java
@@ -25,7 +25,6 @@ public class ConsoleReaderTest {
 	@Test
 	public void shouldReaderContainsPrinter_whenSet() {
 		consoleReader.setPrinter(printer);
-		verify(printer);
 	}
 
 }
