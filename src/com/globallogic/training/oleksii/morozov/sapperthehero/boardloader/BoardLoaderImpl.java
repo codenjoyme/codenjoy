165dc2fa0b7f01fe789e802de360b635d64af5bd
morozovmipt@gmail.com <morozovmipt@gmail.com@46c6ed03-7e28-42c9-d97a-c6df3b90a356>
2012-11-07 07:13:19 +0000

    Порефакторил

--- src/com/globallogic/training/oleksii/morozov/sapperthehero/boardloader/BoardLoaderImpl.java
+++ src/com/globallogic/training/oleksii/morozov/sapperthehero/boardloader/BoardLoaderImpl.java
@@ -1,34 +1,36 @@
 package com.globallogic.training.oleksii.morozov.sapperthehero.boardloader;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.globallogic.training.oleksii.morozov.sapperthehero.game.Board;
 import com.globallogic.training.oleksii.morozov.sapperthehero.game.BoardImpl;
 import com.globallogic.training.oleksii.morozov.sapperthehero.game.minegenerator.FileMinesGenerator;
-import com.globallogic.training.oleksii.morozov.sapperthehero.game.objects.Cell;
 import com.globallogic.training.oleksii.morozov.sapperthehero.game.objects.Mine;
 
 public class BoardLoaderImpl implements BoardLoader {
 	private static final String WHITESPACE_SPLITTER = "\\s";
-	private String dataFromfile;
+	private static final String FILE_PATH = "boards/board";
 	int boarSize;
 	int charge;
 	List<Mine> mines;
 
 	@Override
-	public void readFile(int fileNumber) throws IOException,
+	public void readFromFile(int fileNumber) throws IOException,
 			FileNotFoundException {
-		String filePath = "boards/board";
+	
 		BufferedReader reader = null;
 		mines = new ArrayList<Mine>();
 		try {
 			String text = null;
-			reader = new BufferedReader(new FileReader(filePath + fileNumber));
+			reader = new BufferedReader(new FileReader(FILE_PATH + fileNumber));
 			boolean isChargeAndBoardSizeData = true;
 			while ((text = reader.readLine()) != null) {
 				int firstCollumn = Integer.parseInt((text
@@ -53,7 +55,7 @@ public class BoardLoaderImpl implements BoardLoader {
 	@Override
 	public Board getBoard(int fileNumber) throws IOException,
 			FileNotFoundException {
-		readFile(fileNumber);
+		readFromFile(fileNumber);
 
 		return new BoardImpl(getBoardSize(), getMinesCount(), getCharge(),
 				new FileMinesGenerator(mines));
@@ -74,4 +76,37 @@ public class BoardLoaderImpl implements BoardLoader {
 		return mines.size();
 	}
 
+	@Override
+	public void saveBoard(Board board, int fileNumber) {
+		String fileName = FILE_PATH + fileNumber;
+		
+		
+		ObjectOutputStream os = null;
+		try {
+			os = new ObjectOutputStream(
+					new BufferedOutputStream(new FileOutputStream(fileName)));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			os.writeObject(board);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			os.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+
+	
+	
 }
