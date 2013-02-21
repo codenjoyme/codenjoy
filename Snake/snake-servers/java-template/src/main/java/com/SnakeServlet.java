62bf35bedb10f5f9fc0720faef5bba04c9fde75c
apofig <apofig@gmail.com>
2013-02-21 04:05:14 +0200

    Добавил отдельно java-темплейт который раздается участникам.

---
+++ Snake/snake-servers/java-template/src/main/java/com/SnakeServlet.java
@@ -0,0 +1,47 @@
+package com;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class SnakeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+        String board = req.getParameter("board");
+        board = split(board, BOARD_SIZE);
+        System.out.println(board);
+        resp.getWriter().write(answer(board));
+    }
+
+    private static final int BOARD_SIZE = 15;
+
+    private String split(String board, int size) {
+        StringBuffer result = new StringBuffer();
+        for (int index = 0; index < size; index++) {
+            result.append(board.substring(index*size, (index + 1)*size)).append("\n");
+        }
+        return result.toString();
+    }
+
+    String answer(String board) {
+        return Direction.RIGHT;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8888);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(new ServletHolder(new SnakeServlet()), "/*");
+        server.setHandler(context);
+        server.start();
+    }
+}
