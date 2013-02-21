d92d83a0e14df59577702ea4b153f87f8c3345a6
apofig <apofig@gmail.com>
2013-02-21 06:25:31 +0200

    Дописал тест к прошлому фиксу. + игнор файл.

--- Snake/src/test/java/com/globallogic/snake/services/PlayerScoresTest.java
+++ Snake/src/test/java/com/globallogic/snake/services/PlayerScoresTest.java
@@ -26,4 +26,39 @@ public class PlayerScoresTest {
 
         assertEquals(-10 + 2 + 3 + 4 + 5 - 10 - 50, scores.getScore());
     }
+
+    @Test
+    public void shouldShortLengthWhenEatStone() {
+        PlayerScores scores = new PlayerScores(0);
+
+        scores.snakeEatApple();  //+2
+        scores.snakeEatApple();  //+3
+        scores.snakeEatApple();  //+4
+        scores.snakeEatApple();  //+5
+        scores.snakeEatApple();  //+6
+        scores.snakeEatApple();  //+7
+        scores.snakeEatApple();  //+8
+        scores.snakeEatApple();  //+9
+        scores.snakeEatApple();  //+10
+        scores.snakeEatApple();  //+11
+
+        scores.snakeEatStone();  //-10
+
+        scores.snakeEatApple();  //+2
+        scores.snakeEatApple();  //+3
+
+        assertEquals(2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 - 10 + 2 + 3, scores.getScore());
+    }
+
+    @Test
+    public void shouldStartsFrom2AfterDead() {
+        PlayerScores scores = new PlayerScores(100);
+
+        scores.snakeIsDead();    //-50
+
+        scores.snakeEatApple();  //+2
+        scores.snakeEatApple();  //+3
+
+        assertEquals(100 - 50 + 2 + 3, scores.getScore());
+    }
 }
