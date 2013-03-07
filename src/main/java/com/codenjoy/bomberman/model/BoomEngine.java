75de43fc96b4e8c5afcf34530539e3ba877c8d12
oleksandr.baglai <oleksandr.baglai@78bbc0bb-f289-3343-a0ba-2756f9540430>
2013-03-07 21:45:32 +0000

    Готово! Алгоритм считается правильно даже на оригинальной карте.

--- src/main/java/com/codenjoy/bomberman/model/BoomEngine.java
+++ src/main/java/com/codenjoy/bomberman/model/BoomEngine.java
@@ -1,5 +1,7 @@
 package com.codenjoy.bomberman.model;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,19 +13,22 @@ import java.util.List;
 public class BoomEngine {
 
     public static List<Point> boom(List<Point> barriers, int boardSize, Point source, int radius) {
+        radius = radius + 1;
         List<Point> container = new LinkedList<Point>();
         double dn = 0.01d / radius;
         double n = 0;
         while (n < 2d * Math.PI) {
-            int x = (int) (source.getX() + radius * Math.cos(n));
-            int y = (int) (source.getY() + radius * Math.sin(n));
+            int x = (int)Math.round (source.getX() + radius * Math.cos(n));
+            int y = (int)Math.round (source.getY() + radius * Math.sin(n));
 
             List<Point> line = new Line().draw(source.getX(), source.getY(), x, y);
             for (Point barrier : barriers) {
                 if (line.contains(barrier)) {
                     line = new Line().draw(source.getX(), source.getY(), barrier.getX(), barrier.getY());
                 }
+                line.remove(barrier);
             }
+            removeFar(source, line);
 
             for (Point pt : line) {
                 if (isOnBoard(pt, boardSize) && !container.contains(pt) && !barriers.contains(pt)) {
@@ -34,6 +39,26 @@ public class BoomEngine {
             n = n + dn;
         }
         return container;
+    }
+
+    private static void removeFar(Point source, List<Point> elements) {
+        if (elements.size() == 0) {
+            return;
+        }
+        Collections.sort(elements, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                double a = o1.getX() - o2.getX();
+                double a2 = a*a;
+
+                double b = o1.getY() - o2.getY();
+                double b2 = b*b;
+
+                double c2 = a2 + b2;
+                return (int)c2;
+            }
+        });
+        elements.remove(elements.size() - 1);
     }
 
     private static boolean isOnBoard(Point pt, int boardSize) {
