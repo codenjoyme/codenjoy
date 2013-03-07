05915bb6ef352afbe210800741aacbb8f3ed123f
oleksandr.baglai <oleksandr.baglai@78bbc0bb-f289-3343-a0ba-2756f9540430>
2013-03-07 07:17:35 +0000

    Есть квадратная доска

---
+++ src/main/java/com/codenjoy/bomberman/Board.java
@@ -0,0 +1,18 @@
+package com.codenjoy.bomberman;
+
+/**
+ * User: oleksandr.baglai
+ * Date: 3/7/13
+ * Time: 9:11 AM
+ */
+public class Board {
+    private int size;
+
+    public Board(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/test/java/com/codenjoy/bomberman/BoardTest.java
@@ -0,0 +1,25 @@
+package com.codenjoy.bomberman;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * User: oleksandr.baglai
+ * Date: 3/7/13
+ * Time: 9:07 AM
+ */
+public class BoardTest {
+
+    @Test
+    public void shouldBoard_whenStartGame() {
+        Board board = new Board(10);
+        assertEquals(10, board.getSize());
+    }
+
+    @Test
+    public void shouldBoard_whenStartGame2() {
+        Board board = new Board(20);
+        assertEquals(20, board.getSize());
+    }
+}
