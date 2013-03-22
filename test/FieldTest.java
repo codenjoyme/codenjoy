9e54e9114e4d102a3cb134d7dcd92e95cd79847b
javatrainee6 <javatrainee6@5bd19c2e-2f6b-b5bc-35c5-93bf815d8431>
2013-03-22 12:18:14 +0000

    first test

---
+++ test/FieldTest.java
@@ -0,0 +1,12 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+import org.junit.*;
+
+import static junit.framework.Assert.assertEquals;
+
+public class FieldTest {
+    @Test
+    public void shouldBeZero() {
+        assertEquals(0, 0);
+    }
+
+}
