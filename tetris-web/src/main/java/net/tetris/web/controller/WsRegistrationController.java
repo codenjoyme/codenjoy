3e20eda8bdb483b91012727be66b97e8641d955d
sergey <sergey.zelenin@gmail.com>
2013-04-10 17:05:02 +0300

    Added websockets client jsp

--- tetris-web/src/main/java/net/tetris/web/controller/WsRegistrationController.java
+++ tetris-web/src/main/java/net/tetris/web/controller/WsRegistrationController.java
@@ -6,7 +6,9 @@ import net.tetris.services.TetrisPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -72,7 +74,13 @@ public class WsRegistrationController {
         }
         playerService.addNewPlayer(player.getName(), request.getRemoteAddr(), TetrisPlayerService.WS_PROTOCOL);
 
-        return "redirect:/board/" + player.getName();
+//        return "redirect:/board/" + player.getName();
+        return "redirect:/wsregister/" + player.getName();
     }
 
+    @RequestMapping(value = "{playerName}", method = RequestMethod.GET)
+    public String board(@PathVariable("playerName") String playerName, HttpServletRequest request) {
+        request.setAttribute("user", playerName);
+        return "wsjsclient";
+    }
 }
---
+++ tetris-web/src/main/webapp/view/wsjsclient.jsp
@@ -0,0 +1,42 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;">
+    <title>Javascript Websocket client</title>
+    <script src="/resources/js/jquery-1.7.2.js"></script>
+    <link href="/resources/css/bootstrap.css" rel="stylesheet">
+    <link href="/resources/css/bootstrap.css" rel="stylesheet">
+    <link href="/resources/css/tetris.css" rel="stylesheet">
+</head>
+<body>
+<script>
+
+    var socket = new WebSocket("ws://${pageContext.request.serverName}:${pageContext.request.serverPort}/${pageContext.request.contextPath}ws?user=${requestScope["user"]}");
+    socket.onopen = function(){
+        $("#response").innerHTML = "Socket has been opened!";
+    }
+
+    socket.onmessage = function(msg){
+        $("#response").append(msg.data+"</p>");
+        socket.send("left=4, right=0, rotate=0, drop");
+    }
+
+</script>
+
+<div id="wrapper">
+
+    <div id="container">
+
+        <h1>WebSockets Client</h1>
+
+        <div id="response">
+
+        </div>
+
+    </div><!-- #container -->
+
+</div>
+
+
+</body>
+</html>
