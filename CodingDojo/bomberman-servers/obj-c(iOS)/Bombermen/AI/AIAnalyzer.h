bdd1ae286294a7059ee3175e5f245bf703613ab2
Hell_Ghost <dev.hellghost@gmail.com>
2013-09-15 23:35:45 +0300

    Реструктуризация файлов

---
+++ CodingDojo/bomberman-servers/obj-c(iOS)/Bombermen/AI/AIAnalyzer.h
@@ -0,0 +1,13 @@
+//
+//  AIAnalyzer.h
+//  Bombermen
+//
+//  Created by Hell_Ghost on 15.09.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AIAnalyzer : NSObject
+
+@end
---
+++ CodingDojo/bomberman-servers/obj-c(iOS)/Bombermen/AI/AIAnalyzer.m
@@ -0,0 +1,13 @@
+//
+//  AIAnalyzer.m
+//  Bombermen
+//
+//  Created by Hell_Ghost on 15.09.13.
+//
+//
+
+#import "AIAnalyzer.h"
+
+@implementation AIAnalyzer
+
+@end
