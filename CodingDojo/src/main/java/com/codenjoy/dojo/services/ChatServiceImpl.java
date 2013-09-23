4e973c611fc1c25d0ba13ca6dc0fed2cd3f92cac
Oleksandr Baglai <apofig@gmail.com>
2013-09-24 02:39:54 +0300

    Сделал инит сообщение чтобы ребята могли понять как пользоваться чатом

--- CodingDojo/src/main/java/com/codenjoy/dojo/services/ChatServiceImpl.java
+++ CodingDojo/src/main/java/com/codenjoy/dojo/services/ChatServiceImpl.java
@@ -22,7 +22,17 @@ public class ChatServiceImpl implements ChatService {
     public static int MAX_LENGTH = 200;
     public static int MESSAGES_COUNT = 160;
 
-    private List<String> messages = new LinkedList<String>();
+    List<String> messages = new LinkedList<String>();
+
+    public ChatServiceImpl() {
+        chat("Codenjoy", "Теперь во время игры можно общаться!");
+        chat("Codenjoy", "<span style=\"color:red\">Свои</span> <span style=\"color:green\">сообщения</span> <span style=\"color:blue\">можно</span> <span style=\"color:pink\">разукрасить</span>, если знаешь HTML/CSS.");
+        chat("Codenjoy", "Каждое твое сообщение не должно быть более чем 200 символов в длинну.");
+        chat("Codenjoy", "Иначе оно будет обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться");
+        chat("Codenjoy", "Так же не стоит флудить - это некрасиво по отношению к окружающим. Подряд можно послать только 5 сообщений, а потом ждать ответа.");
+        chat("Codenjoy", "6-е подряд сообщение - пропадает!");
+
+    }
 
     @Override
     public void chat(String playerName, String message) {
@@ -35,8 +45,12 @@ public class ChatServiceImpl implements ChatService {
                 messages.add(playerName + ", " + MAX_LENGTH_MESSAGE + "\n");
                 message = message.substring(0, MAX_LENGTH) + "...";
             }
-            messages.add(playerName + ": " + message + "\n");
+            add(playerName, message);
         }
+    }
+
+    private void add(String playerName, String message) {
+        messages.add(playerName + ": " + message + "\n");
     }
 
     private boolean lastFlood() {
--- CodingDojo/src/test/java/com/codenjoy/dojo/services/ChatServiceImplTest.java
+++ CodingDojo/src/test/java/com/codenjoy/dojo/services/ChatServiceImplTest.java
@@ -1,10 +1,10 @@
 package com.codenjoy.dojo.services;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 
 /**
  * User: sanja
@@ -13,48 +13,68 @@ import static junit.framework.Assert.assertTrue;
  */
 public class ChatServiceImplTest {
 
+    private ChatServiceImpl chat;
+
+    @Before
+    public void setup() {
+        chat = new ChatServiceImpl();
+        chat.messages.clear();
+    }
+
+    @Test
+    public void shouldInitMessage() {
+        chat = new ChatServiceImpl();
+        assertLog(
+                "Codenjoy, НЛО прилетело и украло ваше сообщение\n" +
+                "Codenjoy: Так же не стоит флудить - это некрасиво по отношению к окружающим. Подряд можно послать только 5 сообщений, а потом ждать ответа.\n" +
+                "Codenjoy: Иначе оно будет обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезаться обрезать...\n" +
+                "Codenjoy, НЛО прилетело и поело ваше длинное сообщение\n" +
+                "Codenjoy: Каждое твое сообщение не должно быть более чем 200 символов в длинну.\n" +
+                "Codenjoy: <span style=\"color:red\">Свои</span> <span style=\"color:green\">сообщения</span> <span style=\"color:blue\">можно</span> <span style=\"color:pink\">разукрасить</span>, если знаешь HTML/CSS.\n" +
+                "Codenjoy: Теперь во время игры можно общаться!\n");
+    }
+
     @Test
     public void shouldEscape() {
-        ChatServiceImpl chat = new ChatServiceImpl();
-
         chat.chat("apofig", "message1");
         chat.chat("apofig", "сообщение2");
         chat.chat("apofig", "☺3");
 
-        String log = chat.getChatLog().getBoard();
-        assertEquals(
-                "apofig: \\u263A3\\n" +
-                "apofig: \\u0441\\u043E\\u043E\\u0431\\u0449\\u0435\\u043D\\u0438\\u04352\\n" +
-                "apofig: message1\\n", log);
+        assertLog("apofig: ☺3\n" +
+                "apofig: сообщение2\n" +
+                "apofig: message1\n");
+    }
+
+    private void assertLog(String expected) {
+        String log = StringEscapeUtils.unescapeJava(chat.getChatLog().getBoard());
+        assertEquals(expected, log);
     }
 
     @Test
     public void shouldOnly100Messages() {
+        int old = ChatServiceImpl.MESSAGES_COUNT;
         ChatServiceImpl.MESSAGES_COUNT = 10;
-        ChatServiceImpl chat = new ChatServiceImpl();
 
-        for (int index = 1; index <= 13; index ++) {
+        for (int index = 1; index <= 13; index++) {
             chat.chat("apofig" + index, "message" + index);
         }
 
-        String log = chat.getChatLog().getBoard();
-        assertEquals(
-                "apofig13: message13\\n" +
-                "apofig12: message12\\n" +
-                "apofig11: message11\\n" +
-                "apofig10: message10\\n" +
-                "apofig9: message9\\n" +
-                "apofig8: message8\\n" +
-                "apofig7: message7\\n" +
-                "apofig6: message6\\n" +
-                "apofig5: message5\\n" +
-                "apofig4: message4\\n", log);
+        assertLog("apofig13: message13\n" +
+                "apofig12: message12\n" +
+                "apofig11: message11\n" +
+                "apofig10: message10\n" +
+                "apofig9: message9\n" +
+                "apofig8: message8\n" +
+                "apofig7: message7\n" +
+                "apofig6: message6\n" +
+                "apofig5: message5\n" +
+                "apofig4: message4\n");
+
+        ChatServiceImpl.MESSAGES_COUNT = old;
     }
 
     @Test
     public void shouldAntiFlood() {
-        ChatServiceImpl chat = new ChatServiceImpl();
-
         chat.chat("apofig", "1");
         chat.chat("apofig", "2");
         chat.chat("apofig", "3");
@@ -64,45 +84,40 @@ public class ChatServiceImplTest {
         chat.chat("apofig", "7");
         chat.chat("apofig", "8");
 
-        String log = chat.getChatLog().getBoard();
-        assertEquals(utf("apofig, " + ChatServiceImpl.FLOOD_MESSAGE) + "\\n" +
-                        "apofig: 6\\n" +
-                        "apofig: 5\\n" +
-                        "apofig: 4\\n" +
-                        "apofig: 3\\n" +
-                        "apofig: 2\\n" +
-                        "apofig: 1\\n", log);
+        assertLog("apofig, " + ChatServiceImpl.FLOOD_MESSAGE + "\n" +
+                "apofig: 6\n" +
+                "apofig: 5\n" +
+                "apofig: 4\n" +
+                "apofig: 3\n" +
+                "apofig: 2\n" +
+                "apofig: 1\n");
 
         chat.chat("zanefig", "1");
         chat.chat("apofig", "2");
 
-        log = chat.getChatLog().getBoard();
-        assertEquals("apofig: 2\\n" +
-                "zanefig: 1\\n" +
-                utf("apofig, " + ChatServiceImpl.FLOOD_MESSAGE) + "\\n" +
-                "apofig: 6\\n" +
-                "apofig: 5\\n" +
-                "apofig: 4\\n" +
-                "apofig: 3\\n" +
-                "apofig: 2\\n" +
-                "apofig: 1\\n", log);
-    }
-
-    private String utf(String string) {
-        return StringEscapeUtils.escapeJava(string);
+        assertLog("apofig: 2\n" +
+                "zanefig: 1\n" +
+                "apofig, " + ChatServiceImpl.FLOOD_MESSAGE + "\n" +
+                "apofig: 6\n" +
+                "apofig: 5\n" +
+                "apofig: 4\n" +
+                "apofig: 3\n" +
+                "apofig: 2\n" +
+                "apofig: 1\n");
     }
 
     @Test
     public void shouldCutLongMessage() {
+        int old = ChatServiceImpl.MAX_LENGTH;
         ChatServiceImpl.MAX_LENGTH = 20;
-        ChatServiceImpl chat = new ChatServiceImpl();
 
         chat.chat("apofig", "first message");
         chat.chat("apofig", "123456789012345678901234567890");
 
-        String log = chat.getChatLog().getBoard();
-        assertEquals("apofig: 12345678901234567890...\\n" +
-                utf("apofig, " + ChatServiceImpl.MAX_LENGTH_MESSAGE) + "\\n" +
-                "apofig: first message\\n", log);
+        assertLog("apofig: 12345678901234567890...\n" +
+                "apofig, " + ChatServiceImpl.MAX_LENGTH_MESSAGE + "\n" +
+                "apofig: first message\n");
+
+        ChatServiceImpl.MAX_LENGTH = old;
     }
 }
