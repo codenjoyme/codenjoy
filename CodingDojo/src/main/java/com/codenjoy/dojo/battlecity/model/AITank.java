a855b93e24c06b727bca175e022a9b7ad9d38e66
Oleksandr Baglai <apofig@gmail.com>
2013-12-14 15:43:30 +0200

    Добавил ума AI

--- CodingDojo/src/main/java/com/codenjoy/dojo/battlecity/model/AITank.java
+++ CodingDojo/src/main/java/com/codenjoy/dojo/battlecity/model/AITank.java
@@ -1,7 +1,8 @@
 package com.codenjoy.dojo.battlecity.model;
 
-import com.codenjoy.dojo.services.RandomDice;
+import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.Direction;
+import com.codenjoy.dojo.services.RandomDice;
 
 import java.util.Random;
 
@@ -12,19 +13,33 @@ import java.util.Random;
  */
 public class AITank extends Tank {
 
+    private final static Dice dice = new RandomDice();
+
     public AITank(int x, int y, Direction direction) {
-        super(x, y, direction, new RandomDice(), 1);
+        super(x, y, direction, dice, 1);
     }
 
     @Override
     public void move() {
-        if (new Random().nextBoolean()) {
+        if (new Random().nextInt(6) == 1) {
             act();
         } else {
-            direction = Direction.random();
+
+            int c = 0;
+            int x = 0;
+            int y = 0;
+            do {
+                x = direction.changeX(getX());
+                y = direction.inverted().changeY(getY());
+
+                if (field.isBarrier(x, y)) {
+                    direction = Direction.random();
+                }
+            } while (field.isBarrier(x, y) && c++ < 10);
+
             moving = true;
             super.move();
+            return;
         }
     }
-
 }
