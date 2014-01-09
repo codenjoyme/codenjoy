d65faa73f402d5c9a06a3e44a1a3d83685f2d342
Oleksandr Baglai <apofig@gmail.com>
2014-01-09 17:47:41 +0200

    Потестит парсер команд джойстика

---
+++ CodingDojo/src/test/java/com/codenjoy/dojo/services/PlayerCommandTest.java
@@ -0,0 +1,95 @@
+package com.codenjoy.dojo.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * User: sanja
+ * Date: 09.01.14
+ * Time: 16:21
+ */
+public class PlayerCommandTest {
+
+    private Joystick joystick;
+    private Player player;
+
+    @Before
+    public void setup() {
+        joystick = mock(Joystick.class);
+        player = mock(Player.class);
+    }
+
+    @Test
+    public void shouldLeftCommand() {
+        execute("left");
+
+        verify(joystick, only()).left();
+    }
+
+    private void execute(String command) {
+        new PlayerCommand(joystick, command, player).execute();
+    }
+
+    @Test
+    public void shouldRightCommand() {
+        execute("RIGHT");
+
+        verify(joystick, only()).right();
+    }
+
+    @Test
+    public void shouldUpCommand() {
+        execute("Up");
+
+        verify(joystick, only()).up();
+    }
+
+    @Test
+    public void shouldDownCommand() {
+        execute("dOwn");
+
+        verify(joystick, only()).down();
+    }
+
+    @Test
+    public void shouldActCommand() {
+        execute("act");
+
+        verify(joystick, only()).act();
+    }
+
+    @Test
+    public void shouldActPlusDirectionCommand() {
+        execute("act, left");
+
+        InOrder inOrder = inOrder(joystick);
+        inOrder.verify(joystick).act();
+        inOrder.verify(joystick).left();
+    }
+
+    @Test
+    public void shouldDirectionPlusActCommand() {
+        execute("right,act");
+
+        InOrder inOrder = inOrder(joystick);
+        inOrder.verify(joystick).right();
+        inOrder.verify(joystick).act();
+    }
+
+    @Test
+    public void shouldDirectionsPlusActCommand() {
+        execute("right,left,up,up, down  ,act,down");
+
+        InOrder inOrder = inOrder(joystick);
+        inOrder.verify(joystick).right();
+        inOrder.verify(joystick).left();
+        inOrder.verify(joystick, times(2)).up();
+        inOrder.verify(joystick).down();
+        inOrder.verify(joystick).act();
+        inOrder.verify(joystick).down();
+    }
+
+}
