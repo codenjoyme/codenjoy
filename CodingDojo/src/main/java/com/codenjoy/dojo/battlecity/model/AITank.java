1ccea0908a2e70f67d4af280a6dd9ee616a02714
Oleksandr Baglai <apofig@gmail.com>
2014-01-11 03:44:58 +0200

    Сделал меньше ботов танков и немного изменил их логику - теперь они стреляют реже

--- CodingDojo/src/main/java/com/codenjoy/dojo/battlecity/model/AITank.java
+++ CodingDojo/src/main/java/com/codenjoy/dojo/battlecity/model/AITank.java
@@ -4,8 +4,6 @@ import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.Direction;
 import com.codenjoy.dojo.services.RandomDice;
 
-import java.util.Random;
-
 /**
  * User: sanja
  * Date: 14.06.13
@@ -14,6 +12,7 @@ import java.util.Random;
 public class AITank extends Tank {
 
     private final static Dice dice = new RandomDice();
+    private int act;
 
     public AITank(int x, int y, Direction direction) {
         super(x, y, direction, dice, 1);
@@ -21,25 +20,24 @@ public class AITank extends Tank {
 
     @Override
     public void move() {
-        if (new Random().nextInt(6) == 1) {
+        if (act++ % 10 == 0) {
             act();
-        } else {
-
-            int c = 0;
-            int x = 0;
-            int y = 0;
-            do {
-                x = direction.changeX(getX());
-                y = direction.changeY(getY());
-
-                if (field.isBarrier(x, y)) {
-                    direction = Direction.random();
-                }
-            } while (field.isBarrier(x, y) && c++ < 10);
-
-            moving = true;
-            super.move();
-            return;
         }
+
+        int c = 0;
+        int x = 0;
+        int y = 0;
+        do {
+            x = direction.changeX(getX());
+            y = direction.changeY(getY());
+
+            if (field.isBarrier(x, y)) {
+                direction = Direction.random();
+            }
+        } while (field.isBarrier(x, y) && c++ < 10);
+
+        moving = true;
+
+        super.move();
     }
 }
