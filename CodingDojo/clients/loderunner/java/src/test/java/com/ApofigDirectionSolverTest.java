fca6b72f2810145d373a02a7015a7d48294d8fe1
Sanja <apofig@gmail.com>
2014-10-25 01:51:35 +0300

    Кой че проверил

--- CodingDojo/clients/loderunner/java/src/test/java/com/ApofigDirectionSolverTest.java
+++ CodingDojo/clients/loderunner/java/src/test/java/com/ApofigDirectionSolverTest.java
@@ -237,6 +237,12 @@ public class ApofigDirectionSolverTest {
                 "☼☼☼☼☼☼☼☼☼☼",
                 "[UP, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT]");
 
+        assertC("☼☼☼☼☼" +
+                "☼$H ☼" +
+                "☼ H ☼" +
+                "☼ Q►☼" +
+                "☼☼☼☼☼", "[]");
+
     }
 
     private void assertW(String boardString, String expected) {
