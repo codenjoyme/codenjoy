90a51368c0f7bc5ade83a0c8face43d803612e32
alexander.baglay <apofig@gmil.com>
2015-04-04 14:37:23 +0300

    Пофиксил хоткеи, а то не работало с формы регистрации на админку

--- CodingDojo/server/src/main/webapp/resources/js/hotkeys.js
+++ CodingDojo/server/src/main/webapp/resources/js/hotkeys.js
@@ -6,7 +6,7 @@ function initHotkeys(gameName, contextPath) {
         if (ev.ctrlKey && ev.altKey && ev.keyCode == 65) {
             adminKey = true;
         } else if (adminKey && ev.keyCode == 68) {
-            window.open(contextPath + 'admin31415?select' + (gameNameParam == ''?'':'&') + gameNameParam);
+            window.open(contextPath + 'admin31415' + (gameNameParam == ''?'':'?select&') + gameNameParam);
         } else if (adminKey && ev.keyCode == 82) {
             window.open(contextPath + 'register' + (gameNameParam == ''?'':'?') + gameNameParam);
         } else if (adminKey && ev.keyCode == 83) {
