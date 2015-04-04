e0e3ad4cf9959675ff597c90f0cfea5850d5b678
alexander.baglay <apofig@gmil.com>
2015-04-04 14:43:12 +0300

    В Шахматах пока только туду

--- CodingDojo/games/chess/src/main/java/com/codenjoy/dojo/chess/services/ChessGame.java
+++ CodingDojo/games/chess/src/main/java/com/codenjoy/dojo/chess/services/ChessGame.java
@@ -81,5 +81,14 @@ public class ChessGame implements GameType {
     @Override
     public void newAI(String aiName) {
         // TODO implement me
+//        try {
+//            WebSocketRunner.run(
+//                    WebSocketRunner.Host.LOCAL,
+//                    aiName,
+//                    new ApofigDirectionSolver(new RandomDice()),
+//                    new Board());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
