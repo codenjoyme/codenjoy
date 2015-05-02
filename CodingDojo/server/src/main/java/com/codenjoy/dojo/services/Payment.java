91898c3158c90e52ac2c12fd8ac801deab3374f0
alexander.baglay <apofig@gmil.com>
2015-05-02 15:23:32 +0300

    Добавил сервис оплаты

---
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/Payment.java
@@ -0,0 +1,76 @@
+package com.codenjoy.dojo.services;
+
+import com.codenjoy.dojo.services.jdbc.ObjectMapper;
+import com.codenjoy.dojo.services.jdbc.SqliteConnectionThreadPool;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+@Component
+public class Payment {
+
+    private SqliteConnectionThreadPool pool;
+
+    public Payment(String dbFile) {
+        pool = new SqliteConnectionThreadPool(dbFile,
+                "CREATE TABLE IF NOT EXISTS payments (" +
+                        "email varchar(255), " +
+                        "game_type varchar(255), " +
+                        "till int);");
+    }
+
+    void removeDatabase() {
+        pool.removeDatabase();
+    }
+
+    public boolean canPlay(final String email, final String gameType) {
+        return pool.select("SELECT till FROM payments WHERE email = ? AND game_type = ?;",
+                new Object[]{email, gameType},
+                new ObjectMapper<Boolean>() {
+                    @Override
+                    public Boolean mapFor(ResultSet resultSet) throws SQLException {
+                        if (resultSet.next()) {
+                            return resultSet.getLong("till") > Calendar.getInstance().getTime().getTime();
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+        );
+    }
+
+    public void buy(final String email, final String gameType, int days) {
+        final Calendar calendar = Calendar.getInstance();
+        long till = till(email, gameType);
+        if (till != 0) {
+            calendar.setTimeInMillis(till);
+        }
+        calendar.add(Calendar.DAY_OF_MONTH, days);
+
+        if (till == 0) {
+            pool.update("INSERT INTO payments (email, game_type, till) VALUES (?,?,?);",
+                    new Object[]{email, gameType, calendar.getTime().getTime()});
+        } else {
+            pool.update("UPDATE payments SET till = ? WHERE email = ? AND game_type = ?;",
+                    new Object[]{calendar.getTime().getTime(), email, gameType});
+        }
+    }
+
+    public long till(String email, String gameType) {
+        return pool.select("SELECT till FROM payments WHERE email = ? AND game_type = ?;",
+                new Object[]{email, gameType},
+                new ObjectMapper<Long>() {
+                    @Override
+                    public Long mapFor(ResultSet resultSet) throws SQLException {
+                        if (resultSet.next()) {
+                            return resultSet.getLong("till");
+                        } else {
+                            return 0L;
+                        }
+                    }
+                }
+        );
+    }
+}
---
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/services/PaymentTest.java
@@ -0,0 +1,71 @@
+package com.codenjoy.dojo.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+public class PaymentTest {
+
+    private static Payment service;
+
+    @Before
+    public void setup() {
+        service = new Payment("target/payment.db" + new Random().nextInt());
+    }
+
+    @After
+    public void tearDown() {
+        service.removeDatabase();
+    }
+
+    @Test
+    public void shouldCanPlay_whenBuy() throws InterruptedException {
+        assertFalse(service.canPlay("user", "game"));
+
+        service.buy("user", "game2", 100);
+
+        assertFalse(service.canPlay("user", "game"));
+
+        service.buy("user", "game", 100);
+
+        assertTrue(service.canPlay("user", "game"));
+    }
+
+    @Test
+    public void shouldTill_whenBuy() throws InterruptedException {
+        long now = Calendar.getInstance().getTime().getTime();
+        assertEquals(0, service.till("user", "game"));
+
+        service.buy("user", "game", 365);
+
+        assertEquals(365, delta(now, service.till("user", "game")));
+    }
+
+    private long delta(long now, long till) {
+        if (till == 0) {
+            return 0;
+        }
+        return (till - now)/1000/60/60/24;
+    }
+
+    @Test
+    public void shouldByuTwice() throws InterruptedException {
+        long now = Calendar.getInstance().getTime().getTime();
+
+        assertEquals(0, delta(now, service.till("user", "game")));
+
+        service.buy("user", "game", 100);
+
+        assertEquals(100, delta(now, service.till("user", "game")));
+
+        service.buy("user", "game", 170);
+
+        assertEquals(270, delta(now, service.till("user", "game")));
+    }
+}
