f90b6156afe133a18c2bc1f1113703c7feb67375
alexander.baglay <apofig@gmil.com>
2015-11-05 02:04:58 +0200

    Пофиксил дурацкую багу с лагами при сохранении игры

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/AutoSaver.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/AutoSaver.java
@@ -3,11 +3,18 @@ package com.codenjoy.dojo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 @Component
 public class AutoSaver implements Tickable {
 
     @Autowired
     private SaveService saveService;
+
+    @Autowired
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     private boolean justStart = true;
     private int count = 0;
@@ -21,7 +28,13 @@ public class AutoSaver implements Tickable {
             count++;
             int saveOn = 30;
             if (count%saveOn == (saveOn - 1)) {
-                saveService.saveAll();
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO это очень тугодумная операция, потому она в отдельном потоке
+                        saveService.saveAll();
+                    }
+                });
             }
         }
     }
