26c9f147d4907a0d3ede34183d9b40b63eb6d440
alexander.baglay <apofig@gmil.com>
2016-06-25 12:34:28 +0300

    Добавил воспомагательный класс lengthToXY.Map для быстрого оперирования текстовыми картами

--- CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/LengthToXY.java
+++ CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/LengthToXY.java
@@ -17,4 +17,43 @@ public class LengthToXY { // TODO есть точно такой же в com.cod
     public int getLength(int x, int y) {
         return (size - 1 - y) * size + x;
     }
+
+    public static class Map { // TODO test me
+        private final LengthToXY xy;
+        private char[] map;
+
+        public Map(String map) {
+            this.map = map.toCharArray();
+            xy = new LengthToXY(getSize());
+        }
+
+        public Map(int size) {
+            map = new char[size*size];
+            xy = new LengthToXY(getSize());
+        }
+
+        public int getSize() {
+            return (int) Math.sqrt(map.length);
+        }
+
+        public char getAt(int x, int y) {
+            int length = xy.getLength(x, y);
+            return map[length];
+        }
+
+        public char setAt(int x, int y, char ch) {
+            int length = xy.getLength(x, y);
+            char old = map[length];
+            map[length] = ch;
+            return old;
+        }
+
+        public boolean isOutOf(int x, int y) {
+            return new PointImpl(x, y).isOutOf(getSize());
+        }
+
+        public String getMap() {
+            return new String(map);
+        }
+    }
 }
