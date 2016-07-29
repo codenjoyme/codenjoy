e0409d8e3a89d274b04a728b0dab805ba5e8a769
alexander.baglay <apofig@gmil.com>
2016-07-30 00:06:51 +0300

    Отключил рекламу по умолчанию, донейт и джойстик

--- CodingDojo/server/src/main/webapp/resources/js/settings.js
+++ CodingDojo/server/src/main/webapp/resources/js/settings.js
@@ -20,13 +20,13 @@
  * #L%
  */
 var game = {};
-game.enableDonate = true;
-game.enableJoystick = true;
+game.enableDonate = false;
+game.enableJoystick = false;
 game.enableAlways = false;
 game.enablePlayerInfo = true;
 game.enableLeadersTable = true;
 game.enableChat = true;
 game.enableHotkeys = true;
-game.enableAdvertisement = true;
+game.enableAdvertisement = false;
 game.showBody = true;
 
