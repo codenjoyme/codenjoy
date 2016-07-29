dbd51efc613d97d23b6671600ebdd992ad95a751
alexander.baglay <apofig@gmil.com>
2016-07-29 17:30:00 +0300

    Release 1.0.7

--- engine/setup.bat
+++ engine/setup.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=engine-1.0.6.jar -Dsources=engine-1.0.6-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.6 -Dpackaging=jar
+mvn install:install-file -Dfile=engine-1.0.7.jar -Dsources=engine-1.0.7-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.7 -Dpackaging=jar
