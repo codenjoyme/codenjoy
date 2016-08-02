9af95dcfbd6094dc2ec699b340e278e4d92813f2
alexander.baglay <apofig@gmil.com>
2016-08-02 13:50:39 +0300

    Зарелизил версию 1.0.8

--- CodingDojo/games/engine/setup.bat
+++ CodingDojo/games/engine/setup.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=engine-1.0.7.jar -Dsources=engine-1.0.7-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.7 -Dpackaging=jar
+mvn install:install-file -Dfile=engine-1.0.8.jar -Dsources=engine-1.0.8-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.8 -Dpackaging=jar
