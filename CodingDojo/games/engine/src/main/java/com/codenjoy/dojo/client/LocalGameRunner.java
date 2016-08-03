122a73e5640a35ac0728813ed207727e41f9726c
alexander.baglay <apofig@gmil.com>
2016-08-03 22:29:09 +0300

    Сделал возможным поменять количество миллисекунд для LocalGameRunner

--- CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/client/LocalGameRunner.java
+++ CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/client/LocalGameRunner.java
@@ -27,6 +27,8 @@ import com.codenjoy.dojo.services.*;
 
 public class LocalGameRunner {
 
+    public static int TIMEOUT = 1000;
+
     public static void run(GameType gameType, Solver solver, AbstractBoard board) {
         Game game = gameType.newGame(new EventListener() {
             @Override
@@ -40,7 +42,7 @@ public class LocalGameRunner {
             String data = game.getBoardAsString();
             board.forString(data);
 
-            System.out.print(board.toString());
+            System.out.println(board.toString());
 
             String answer = solver.get(board);
 
@@ -49,7 +51,7 @@ public class LocalGameRunner {
             new PlayerCommand(game.getJoystick(), answer).execute();
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(TIMEOUT);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
