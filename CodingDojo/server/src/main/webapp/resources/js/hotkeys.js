002397e155fcfc741ae9714a1048ea6e38898260
alexander.baglay <apofig@gmil.com>
2016-10-13 19:05:00 +0300

    Добавил кнопку перехода на лидерборд страничку

--- CodingDojo/server/src/main/webapp/resources/js/hotkeys.js
+++ CodingDojo/server/src/main/webapp/resources/js/hotkeys.js
@@ -34,6 +34,8 @@ function initHotkeys(gameName, contextPath) {
             window.open(contextPath);
         } else if (adminKey && ev.keyCode == 74) { // ... + J
             game.enableJoystick = !game.enableJoystick;
+        } else if (adminKey && ev.keyCode == 66) { // ... + B
+            window.open(contextPath + 'board/game/' + gameName;
         } else {
             adminKey = false;
         }
