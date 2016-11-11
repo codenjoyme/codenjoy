c8e27bc4019efb82db64e04e386463f724ea0453
alexander.baglay <apofig@gmil.com>
2016-11-12 00:23:56 +0200

    Зарелизил версию v1.0.17

--- CodingDojo/builder/README.md
+++ CodingDojo/builder/README.md
@@ -7,7 +7,7 @@ Codenjoy builder
 Для подключения своей игры (game-name-engine) необходимо добавить зависимость
 ```
     <properties>
-        <codenjoy.version>1.0.13</codenjoy.version>
+        <codenjoy.version>1.0.17</codenjoy.version>
     </properties>
     ...
     <dependencies>
--- CodingDojo/games/engine/setup.bat
+++ CodingDojo/games/engine/setup.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=engine-1.0.14.jar -Dsources=engine-1.0.14-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.14 -Dpackaging=jar
+mvn install:install-file -Dfile=engine-1.0.17.jar -Dsources=engine-1.0.17-sources.jar -DpomFile=pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.17 -Dpackaging=jar
