58d13ed10d4fd4db327412eb54856bba9eba9316
alexander.baglay <apofig@gmil.com>
2017-02-26 11:10:54 +0200

    Создали скрипт для перезапуска сервера codenjoy.com

---
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/integration/RestartCodenjoyServer.java
@@ -0,0 +1,41 @@
+package com.codenjoy.dojo.integration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ * Created by indigo on 2017-02-26.
+ */
+public class RestartCodenjoyServer {
+
+    public static final String SERVER_URL = "http://uc2.nodecluster.net/";
+
+    public static void main(String[] args) {
+        HtmlUnitDriver driver = new HtmlUnitDriver(true);
+        driver.get(SERVER_URL + "jcp/");
+
+        driver.findElement(By.id("LoginForm_username")).sendKeys("login");
+        driver.findElement(By.id("LoginForm_password")).sendKeys("password");
+        driver.findElement(By.xpath("/html/body//form[@id='login-form']//input[@type='submit' and @value='Login']")).click();
+
+        if (driver.getCurrentUrl().contains("uc2.nodecluster.net/jcp/my/appserver")) {
+            System.out.println("Login success!");
+
+            System.out.println("Try to restart...");
+
+            driver.findElement(By.xpath("/html/body//form[@id='restart-form']//input[@type='submit' and @name='Restart']")).click();
+
+            System.out.println("Try to logout..");
+
+            driver.get(SERVER_URL + "/jcp/site/logout");
+
+            if (driver.getCurrentUrl().contains("uc2.nodecluster.net/jcp/")) {
+                System.out.println("Logout success..");
+            } else {
+                System.out.println("Logout failure!");
+            }
+        } else {
+            System.out.println("Login failure!");
+        }
+    }
+}
