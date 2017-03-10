d958e60fc8c34278ff5ba7c4aa8256fe6ec4fb35
alexander.baglay <apofig@gmil.com>
2017-03-10 12:14:40 +0200

    Помники для инсталяции approvals

---
+++ CodingDojo/games/kata/lib/setup.bat
@@ -0,0 +1,2 @@
+call mvn install:install-file -Dfile=ApprovalTests-012.jar -Dsources=ApprovalTests-012.jar -DpomFile=pom.xml -DgroupId=org.approvaltests -DartifactId=approvaltests -Dversion=0.12 -Dpackaging=jar
+pause >nul
