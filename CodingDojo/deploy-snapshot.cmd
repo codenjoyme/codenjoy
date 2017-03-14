be1579cb6f1fd0aef716b1f324a2a25a3659bb73
alexander.baglay <apofig@gmil.com>
2017-03-14 20:28:39 +0200

    Так удобнее деплоить

--- CodingDojo/deploy-snapshot.cmd
+++ CodingDojo/deploy-snapshot.cmd
@@ -1,2 +1,2 @@
-call mvn -DaltDeploymentRepository=snapshots::default::file:repo\snapshots clean deploy 1>deploy-snapshot.log 2>&1
+call mvn -DaltDeploymentRepository=snapshots::default::file:repo\snapshots clean deploy
 pause >nul
