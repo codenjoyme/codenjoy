f9e111cb558ede977b64d4e7acd0775796d014c8
alexander.baglay <apofig@gmil.com>
2017-03-15 18:11:51 +0200

    Все последующие изменения будут видны как v1.0.22 (ее пока не релизил)

--- CodingDojo/games/engine/setup.bat
+++ CodingDojo/games/engine/setup.bat
@@ -1,10 +1,10 @@
-call mvn install:install-file -Dfile=engine-1.0.21.jar -Dsources=engine-1.0.21-sources.jar -DpomFile=engine-1.0.21-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.21 -Dpackaging=jar
+call mvn install:install-file -Dfile=engine-1.0.22.jar -Dsources=engine-1.0.22-sources.jar -DpomFile=engine-1.0.22-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.22 -Dpackaging=jar
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
 echo [INFO] !!!!    Then press Enter to continue (and wait until the next step installation is complete)
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 pause >nul
-call mvn install:install-file -Dfile=games-1.0.21-pom.xml -DpomFile=games-1.0.21-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=1.0.21 -Dpackaging=pom
+call mvn install:install-file -Dfile=games-1.0.22-pom.xml -DpomFile=games-1.0.21-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=1.0.21 -Dpackaging=pom
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
 echo [INFO] !!!!    Then press Enter to exit
