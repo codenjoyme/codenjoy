56e904325553907049a19e1035c3d3870c29f274
alexander.baglay <apofig@gmil.com>
2017-04-03 20:08:45 +0200

    Обновил версию, а то бага какая-то

--- CodingDojo/games/engine/setup.bat
+++ CodingDojo/games/engine/setup.bat
@@ -1,10 +1,10 @@
-call mvn install:install-file -Dfile=engine-1.0.22-pre.jar -Dsources=engine-1.0.22-pre-sources.jar -DpomFile=engine-1.0.22-pre-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.22-pre -Dpackaging=jar
+call mvn install:install-file -Dfile=engine-1.0.22b.jar -Dsources=engine-1.0.22b-sources.jar -DpomFile=engine-1.0.22b-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.22b -Dpackaging=jar
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
 echo [INFO] !!!!    Then press Enter to continue (and wait until the next step installation is complete)
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 pause >nul
-call mvn install:install-file -Dfile=games-1.0.22-pre-pom.xml -DpomFile=games-1.0.22-pre-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=1.0.22-pre -Dpackaging=pom
+call mvn install:install-file -Dfile=games-1.0.22b-pom.xml -DpomFile=games-1.0.22-pre-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=1.0.22-pre -Dpackaging=pom
 echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
 echo [INFO] !!!!    Then press Enter to exit
--- CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
+++ CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
@@ -1,5 +1,5 @@
 def fromVersion = '1.0.22'
-def toVersion = '1.0.22-pre'
+def toVersion = '1.0.22b'
 
 // select root directories
 def base = '..\\..\\..\\..\\..\\..\\..\\..\\';
