435ba70c2112962337db79ce4076a4a8332ed63a
Oleksandr Baglai <apofig@gmail.com>
2017-09-01 21:36:47 +0300

    Refactoring in tests

---
+++ src/test/java/com/epam/dojo/expansion/model/AbstractSinglePlayersTest.java
@@ -0,0 +1,217 @@
+package com.epam.dojo.expansion.model;
+
+/*-
+ * #%L
+ * iCanCode - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 EPAM
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.EventListener;
+import com.codenjoy.dojo.services.Point;
+import com.codenjoy.dojo.services.QDirection;
+import com.codenjoy.dojo.utils.TestUtils;
+import com.epam.dojo.expansion.model.interfaces.ILevel;
+import com.epam.dojo.expansion.model.items.Hero;
+import com.epam.dojo.expansion.services.Events;
+import com.epam.dojo.expansion.services.Levels;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.*;
+
+import static com.codenjoy.dojo.services.PointImpl.pt;
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * User: sanja
+ * Date: 17.12.13
+ * Time: 4:47
+ */
+public abstract class AbstractSinglePlayersTest {
+
+    private Dice dice;
+    private List<EventListener> listeners;
+    private List<Single> singles;
+    private List<Expansion> gamesSingle;
+    private List<List<ILevel>> levelsSingle;
+
+    private List<ILevel> levelMultiple;
+    private Expansion gameMultiple;
+
+    @Before
+    public void setup() {
+        dice = mock(Dice.class);
+        listeners = new LinkedList<>();
+        singles = new LinkedList<>();
+        gamesSingle = new LinkedList<>();
+        levelsSingle = new LinkedList<>();
+    }
+
+    private void dice(int... ints) {
+        OngoingStubbing<Integer> when = when(dice.next(anyInt()));
+        for (int i : ints) {
+            when = when.thenReturn(i);
+        }
+    }
+
+    protected void givenFl(int count, String... boards) {
+        Levels.VIEW_SIZE = Levels.VIEW_SIZE_TESTING;
+        Deque<String> strings = new LinkedList<>(Arrays.asList(boards));
+
+        String multiple = strings.removeLast();
+        levelMultiple = createLevels(Arrays.asList(multiple));
+        gameMultiple = new Expansion(levelMultiple, dice, Expansion.MULTIPLE);
+
+        for (int i = 0; i < count; i++) {
+            List<ILevel> levels = createLevels(strings);
+            levelsSingle.add(levels);
+            Expansion expansion = new Expansion(levels, dice, Expansion.SINGLE);
+            gamesSingle.add(expansion);
+
+            EventListener listener = mock(EventListener.class);
+            listeners.add(listener);
+            Single game = new Single(expansion, gameMultiple, listener, null, null);
+            singles.add(game);
+            game.newGame();
+        }
+    }
+
+    protected void tickAll() {
+        for (Single single : singles) {
+            single.tick();
+        }
+    }
+
+    // делает удобным перемещение героя, что очень надо для этого легаси теста
+    @NotNull
+    private Hero getOnlyMovingJoystick(Single single, final int x, final int y) {
+        final Hero hero = (Hero) single.getJoystick();
+        final Point pt = pt(x, y);
+        final int INCREASE = 2;
+        final int MOVE = 1;
+        return new Hero() {
+            @Override
+            public void down() {
+                hero.increaseAndMove(
+                        new Forces(pt, INCREASE),
+                        new ForcesMoves(pt, MOVE, QDirection.DOWN)
+                );
+            }
+
+            @Override
+            public void up() {
+                hero.increaseAndMove(
+                        new Forces(pt, INCREASE),
+                        new ForcesMoves(pt, MOVE, QDirection.UP)
+                );
+            }
+
+            @Override
+            public void left() {
+                hero.increaseAndMove(
+                        new Forces(pt, INCREASE),
+                        new ForcesMoves(pt, MOVE, QDirection.LEFT)
+                );
+            }
+
+            @Override
+            public void right() {
+                hero.increaseAndMove(
+                        new Forces(pt, INCREASE),
+                        new ForcesMoves(pt, MOVE, QDirection.RIGHT)
+                );
+            }
+
+            @Override
+            public void act(int... p) {
+                hero.act(p);
+            }
+
+            @Override
+            public void message(String command) {
+                hero.message(command);
+            }
+
+            @Override
+            public void loadLevel(int level) {
+                hero.loadLevel(level);
+            }
+        };
+    }
+
+    protected Hero hero(int index, int x, int y) {
+        Single single = singles.get(index);
+        return getOnlyMovingJoystick(single, x, y);
+    }
+
+    private List<ILevel> createLevels(Collection<String> boards) {
+        List<ILevel> levels = new LinkedList<ILevel>();
+        for (String board : boards) {
+            ILevel level = new LevelImpl(board);
+            levels.add(level);
+        }
+        return levels;
+    }
+
+    private void assertL(int index, String expected) {
+        Single single = singles.get(index);
+        assertEquals(TestUtils.injectN(expected),
+                TestUtils.injectN(single.getPrinter().getBoardAsString(1, single.getPlayer()).getLayers().get(0)));
+    }
+
+    protected void assertE(int index, String expected) {
+        Single single = singles.get(index);
+        assertEquals(TestUtils.injectN(expected),
+                TestUtils.injectN(single.getPrinter().getBoardAsString(2, single.getPlayer()).getLayers().get(1)));
+    }
+
+    protected void assertF(int index, String expected) {
+        Single single = singles.get(index);
+        assertEquals(expected, single.getPrinter().getBoardAsString(2, single.getPlayer()).getForces().toString().replace('"', '\''));
+    }
+
+    protected EventListener verify(int index) {
+        EventListener listener = listeners.get(index);
+        return Mockito.verify(listener);
+    }
+
+    protected void reset(int index) {
+        EventListener listener = listeners.get(index);
+        Mockito.reset(listener);
+    }
+
+    protected void verifyNoMoreInteractions(int index) {
+        EventListener listener = listeners.get(index);
+        Mockito.verifyNoMoreInteractions(listener);
+    }
+
+    public static final int PLAYER1 = 0;
+    public static final int PLAYER2 = 1;
+    public static final int PLAYER3 = 2;
+    public static final int PLAYER4 = 3;
+    public static final int PLAYER5 = 4;
+}
---
+++ src/test/java/com/epam/dojo/expansion/model/Single4PelayersTest.java
@@ -0,0 +1,132 @@
+package com.epam.dojo.expansion.model;
+
+/*-
+ * #%L
+ * iCanCode - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 EPAM
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.epam.dojo.expansion.services.Events;
+import org.junit.Test;
+
+/**
+ * User: sanja
+ * Date: 17.12.13
+ * Time: 4:47
+ */
+public class Single4PelayersTest extends AbstractSinglePlayersTest {
+
+    @Test
+    public void shouldEveryHeroHasTheirOwnStartBase() {
+        // given
+        givenFl(4,
+                "╔═════┐" +
+                "║1.E..│" +
+                "║.....│" +
+                "║E...E│" +
+                "║.....│" +
+                "║..E..│" +
+                "└─────┘",
+                "╔═════┐" +
+                "║4.E.1│" +
+                "║.....│" +
+                "║E...E│" +
+                "║.....│" +
+                "║3.E.2│" +
+                "└─────┘");
+
+        // level 1 - single for everyone
+
+        assertE(PLAYER1,
+                "-------" +
+                "-♥-----" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------");
+
+        assertF(PLAYER1,
+                "[[1,5]=10]");
+
+        assertE(PLAYER2,
+                "-------" +
+                "-♥-----" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------");
+
+        assertF(PLAYER2,
+                "[[1,5]=10]");
+
+        // when
+        // hero1 goes to multiple level
+        hero(PLAYER1, 1, 5).right();
+        tickAll();
+
+        assertF(PLAYER1,
+                "[[1,5]=11," +
+                " [2,5]=1]");
+
+        hero(PLAYER1, 2, 5).right();
+        tickAll();
+
+        assertF(PLAYER1,
+                "[[1,5]=11," +
+                " [2,5]=2," +
+                " [3,5]=1]");
+
+        verify(PLAYER1).event(Events.WIN(0));
+        reset(PLAYER1);
+        verifyNoMoreInteractions(PLAYER2);
+
+        tickAll();
+
+        // then
+        // hero1 on their own start base
+        assertE(PLAYER1,
+                "-------" +
+                "-----♥-" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------");
+
+        assertF(PLAYER1,
+                "[[5,5]=10]");
+
+        // for hero2 nothing will be changed
+        assertE(PLAYER2,
+                "-------" +
+                "-♥-----" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------" +
+                "-------");
+
+        assertF(PLAYER2,
+                "[[1,5]=10]");
+
+    }
+}
