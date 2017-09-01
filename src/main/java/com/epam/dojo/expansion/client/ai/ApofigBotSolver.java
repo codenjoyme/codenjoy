4c70ca158ab14501aba1a7a406f08bfbc420fabd
Oleksandr Baglai <apofig@gmail.com>
2017-09-01 18:03:16 +0300

    Fix bot so they fill whole board

--- src/main/java/com/epam/dojo/expansion/client/ai/ApofigBotSolver.java
+++ src/main/java/com/epam/dojo/expansion/client/ai/ApofigBotSolver.java
@@ -27,6 +27,7 @@ import com.codenjoy.dojo.services.Direction;
 import com.codenjoy.dojo.services.QDirection;
 import com.codenjoy.dojo.services.Point;
 import com.epam.dojo.expansion.client.*;
+import com.epam.dojo.expansion.model.Elements;
 import com.epam.dojo.expansion.model.Forces;
 import com.epam.dojo.expansion.model.ForcesMoves;
 
@@ -46,33 +47,47 @@ public class ApofigBotSolver extends AbstractSolver {
     @Override
     public Command whatToDo(Board board) {
         if (!board.isMeAlive()) return doNothing();
-
+        Point point = null;
+        QDirection direction = null;
         List<Point> destination = board.getGold();
-        if (destination.isEmpty()) {
-            destination = board.getFreeSpaces();
-        }
-
-        Point faster = null;
-        int length = Integer.MAX_VALUE;
-        List<Direction> shortestWay = null;
-        for (Forces force : board.getMyForces()) {
-            Point from = force.getRegion();
-            List<Direction> way = board.getShortestWay(from, destination);
-            if (way.size() < length) {
-                length = way.size();
-                shortestWay = way;
-                faster = from;
+        if (!destination.isEmpty()) {
+            int length = Integer.MAX_VALUE;
+            List<Direction> shortestWay = null;
+            for (Forces force : board.getMyForces()) {
+                Point from = force.getRegion();
+                List<Direction> way = board.getShortestWay(from, destination);
+                if (way.size() < length) {
+                    length = way.size();
+                    shortestWay = way;
+                    point = from;
+                }
+            }
+            if (!shortestWay.isEmpty()) {
+                direction = QDirection.get(shortestWay.get(0));
             }
         }
-
-        if (shortestWay.isEmpty()) {
+        if (direction == null) {
+            List<Point> free = board.getFreeSpaces();
+            List<Forces> forces = board.getMyForces();
+            for (Forces force : forces) {
+                Point pt1 = force.getRegion();
+                for (QDirection d : QDirection.values()) {
+                    Point pt2 = d.change(pt1);
+                    if (free.contains(pt2)) {
+                        point = pt1;
+                        direction = d;
+                        break;
+                    }
+                }
+            }
+        }
+        if (direction == null) {
             return doNothing();
         }
-        QDirection nextStep = QDirection.get(shortestWay.get(0));
 
         return Command
-                .increase(new Forces(faster, 10))
-                .move(new ForcesMoves(faster, 5, nextStep))
+                .increase(new Forces(point, 10))
+                .move(new ForcesMoves(point, 5, direction))
                 .build();
     }
 
