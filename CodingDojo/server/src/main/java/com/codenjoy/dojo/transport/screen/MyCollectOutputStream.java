5563cec8a93d00a83cb23770757cfae81b369faf
Oleksandr Baglai <apofig@gmail.com>
2017-09-02 22:58:53 +0300

    Обманом ужимаю все что отправляется браузерам, чтобы не париться что я пихаю в json. Использую gzip

---
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/transport/screen/MyCollectOutputStream.java
@@ -0,0 +1,60 @@
+package com.codenjoy.dojo.transport.screen;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 - 2017 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by Oleksandr_Baglai on 2017-09-02.
+ */
+public class MyCollectOutputStream extends FilterOutputStream {
+
+    private StringBuffer buffer = new StringBuffer();
+
+    /**
+     * Creates an output stream filter built on top of the specified
+     * underlying output stream.
+     *
+     * @param out the underlying output stream to be assigned to
+     *            the field <tt>this.out</tt> for later use, or
+     *            <code>null</code> if this instance is to be
+     *            created without an underlying stream.
+     */
+    public MyCollectOutputStream(OutputStream out) {
+        super(out);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        buffer.append((char)b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.write(buffer.toString().getBytes());
+        super.close();
+    }
+}
