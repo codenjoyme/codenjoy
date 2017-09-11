dacdf633d79fb4ad50128ba19b79e2fa0c733fe4
Oleksandr Baglai <apofig@gmail.com>
2017-09-11 11:45:57 +0300

    Refactoring

--- src/main/java/com/epam/dojo/expansion/model/lobby/LobbyPlayerBoard.java
+++ src/main/java/com/epam/dojo/expansion/model/lobby/LobbyPlayerBoard.java
@@ -1,13 +1,40 @@
 package com.epam.dojo.expansion.model.lobby;
 
+/*-
+ * #%L
+ * expansion - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 - 2017 EPAM
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
 import com.codenjoy.dojo.services.Point;
-import com.epam.dojo.expansion.model.Player;
-import com.epam.dojo.expansion.model.PlayerBoard;
+import com.epam.dojo.expansion.model.*;
+import com.epam.dojo.expansion.model.levels.CellImpl;
 import com.epam.dojo.expansion.model.levels.Level;
 import com.epam.dojo.expansion.model.levels.LevelImpl;
 import com.epam.dojo.expansion.model.levels.Levels;
 import com.epam.dojo.expansion.model.levels.items.Hero;
+import com.epam.dojo.expansion.model.levels.items.HeroForces;
+import com.epam.dojo.expansion.model.levels.items.Start;
 import com.epam.dojo.expansion.services.SettingsWrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -16,7 +43,9 @@ import static com.codenjoy.dojo.services.PointImpl.pt;
 /**
  * Created by Oleksandr_Baglai on 2017-09-11.
  */
-public class LobbyPlayerBoard implements PlayerBoard {
+public class LobbyPlayerBoard implements PlayerBoard, Field {
+    public static final int BOARD_SIZE = 20;
+    public static final Point NULL_POINT = pt(-1, -1);
     private List<Player> waiting;
     private LevelImpl level;
 
@@ -27,12 +56,12 @@ public class LobbyPlayerBoard implements PlayerBoard {
 
     @Override
     public int getViewSize() {
-        return 20; // TODO надо это где-то взять
+        return BOARD_SIZE;
     }
 
     @Override
     public int size() {
-        return 20; // TODO надо это где-то взять
+        return BOARD_SIZE;
     }
 
     @Override
@@ -55,7 +84,7 @@ public class LobbyPlayerBoard implements PlayerBoard {
         Hero hero = new Hero() {
             @Override
             public Point getPosition() {
-                return pt(0, 0);
+                return NULL_POINT;
             }
 
             @Override
@@ -64,7 +93,7 @@ public class LobbyPlayerBoard implements PlayerBoard {
             }
         };
         player.setHero(hero);
-        hero.setField(new Lobby());
+        hero.setField(this);
     }
 
     @Override
@@ -95,5 +124,63 @@ public class LobbyPlayerBoard implements PlayerBoard {
     @Override
     public void tick() {
         // do nothing
+    }
+
+    @Override
+    public void increase(Hero hero, List<ForcesMoves> increase) {
+        // do nothing
+    }
+
+    @Override
+    public void move(Hero hero, List<ForcesMoves> movements) {
+        // do nothing
+    }
+
+    @Override
+    public Start getBaseOf(Hero hero) {
+        return getStart(hero.getPosition());
+    }
+
+    @Nullable
+    @Override
+    public Start getFreeBase() {
+        return getStart(NULL_POINT);
+    }
+
+    @NotNull
+    private Start getStart(Point pt) {
+        Start start = new Start(Elements.BASE1);
+        start.setCell(new CellImpl(pt));
+        return start;
+    }
+
+    @Override
+    public HeroForces startMoveForces(Hero item, int x, int y, int count) {
+        return HeroForces.EMPTY;
+    }
+
+    @Override
+    public void removeForces(Hero hero, int x, int y) {
+        // do nothing
+    }
+
+    @Override
+    public void reset() {
+        // do nothing
+    }
+
+    @Override
+    public void removeFromCell(Hero hero) {
+        // do nothing
+    }
+
+    @Override
+    public int totalRegions() {
+        return 1;
+    }
+
+    @Override
+    public int regionsCount(Hero hero) {
+        return 0;
     }
 }
