5df2e0b143b317b9592ed8c44eee59fb568917c1
Oleksandr Baglai <apofig@gmail.com>
2017-09-11 07:11:57 +0300

    +2 tests for lobby

--- src/test/java/com/epam/dojo/expansion/model/GameRunnerWithLobbyTest.java
+++ src/test/java/com/epam/dojo/expansion/model/GameRunnerWithLobbyTest.java
@@ -38,6 +38,47 @@ import static org.mockito.Mockito.mock;
  */
 public class GameRunnerWithLobbyTest extends AbstractGameRunnerTest {
 
+    public static final String LOBBY_LEVEL = "╔══════════════════┐\n" +
+            "║..................│\n" +
+            "║..................│\n" +
+            "║..................│\n" +
+            "║..B....BBB..BBB...│\n" +
+            "║..B....B.B..B.B...│\n" +
+            "║..B....B.B..BBB...│\n" +
+            "║..B....B.B..B.B...│\n" +
+            "║..BBB..BBB..BBB...│\n" +
+            "║..................│\n" +
+            "║..................│\n" +
+            "║........BBB..B.B..│\n" +
+            "║........B.B..B.B..│\n" +
+            "║........BBB...B...│\n" +
+            "║........B.B...B...│\n" +
+            "║........BBB...B...│\n" +
+            "║..................│\n" +
+            "║..................│\n" +
+            "║..................│\n" +
+            "└──────────────────┘\n";
+    public static final String LOBBY_FORCES = "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n" +
+            "--------------------\n";
+
     @NotNull
     protected PlayerLobby getLobby() {
         return new WaitForAllPlayerLobby();
@@ -54,60 +95,18 @@ public class GameRunnerWithLobbyTest extends AbstractGameRunnerTest {
         createNewGame();
         createNewGame();
 
-        String levelLobby =
-                "╔══════════════════┐\n" +
-                "║..................│\n" +
-                "║..................│\n" +
-                "║..................│\n" +
-                "║..B....BBB..BBB...│\n" +
-                "║..B....B.B..B.B...│\n" +
-                "║..B....B.B..BBB...│\n" +
-                "║..B....B.B..B.B...│\n" +
-                "║..BBB..BBB..BBB...│\n" +
-                "║..................│\n" +
-                "║..................│\n" +
-                "║........BBB..B.B..│\n" +
-                "║........B.B..B.B..│\n" +
-                "║........BBB...B...│\n" +
-                "║........B.B...B...│\n" +
-                "║........BBB...B...│\n" +
-                "║..................│\n" +
-                "║..................│\n" +
-                "║..................│\n" +
-                "└──────────────────┘\n";
-        String forcesLobby =
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n" +
-                "--------------------\n";
-        assertL(levelLobby, PLAYER1);
-        assertE(forcesLobby, PLAYER1);
-        assertL(levelLobby, PLAYER2);
-        assertE(forcesLobby, PLAYER2);
-        assertL(levelLobby, PLAYER3);
-        assertE(forcesLobby, PLAYER3);
-        assertL(levelLobby, PLAYER4);
-        assertE(forcesLobby, PLAYER4);
-        assertL(levelLobby, PLAYER5);
-        assertE(forcesLobby, PLAYER5);
-        assertL(levelLobby, PLAYER6);
-        assertE(forcesLobby, PLAYER6);
+        assertL(LOBBY_LEVEL, PLAYER1);
+        assertE(LOBBY_FORCES, PLAYER1);
+        assertL(LOBBY_LEVEL, PLAYER2);
+        assertE(LOBBY_FORCES, PLAYER2);
+        assertL(LOBBY_LEVEL, PLAYER3);
+        assertE(LOBBY_FORCES, PLAYER3);
+        assertL(LOBBY_LEVEL, PLAYER4);
+        assertE(LOBBY_FORCES, PLAYER4);
+        assertL(LOBBY_LEVEL, PLAYER5);
+        assertE(LOBBY_FORCES, PLAYER5);
+        assertL(LOBBY_LEVEL, PLAYER6);
+        assertE(LOBBY_FORCES, PLAYER6);
 
         // when
         levelOrFreeRoom(LEVEL1,  // LEVEL1
@@ -161,6 +160,93 @@ public class GameRunnerWithLobbyTest extends AbstractGameRunnerTest {
         assertE(forces2, PLAYER5);
         assertL(level2, PLAYER6);
         assertE(forces2, PLAYER6);
+    }
+
+    @Test
+    public void shouldNextTwoPlayersGoToLobby() {
+        shouldCreateSixPlayersInTwoDifferentRooms();
+
+        createNewGame();
+        createNewGame();
+
+        // first free room
+        tickAll();
+
+        String level2 =
+                "╔════┐\n" +
+                "║..1.│\n" +
+                "║4...│\n" +
+                "║...2│\n" +
+                "║.3..│\n" +
+                "└────┘\n";
+        String forces2 =
+                "------\n" +
+                "---♥--\n" +
+                "------\n" +
+                "----♦-\n" +
+                "------\n" +
+                "------\n";
+        assertL(level2, PLAYER5);
+        assertE(forces2, PLAYER5);
+        assertL(level2, PLAYER6);
+        assertE(forces2, PLAYER6);
+
+        assertL(LOBBY_LEVEL, PLAYER7);
+        assertE(LOBBY_FORCES, PLAYER7);
+        assertL(LOBBY_LEVEL, PLAYER8);
+        assertE(LOBBY_FORCES, PLAYER8);
+    }
+
+    @Test
+    public void shouldWhenOneUserShouldResetLevelThenGoToLobby() {
+        shouldCreateSixPlayersInTwoDifferentRooms();
+
+        game(PLAYER1).getJoystick().act(0); // player want to leave room
+        tickAll();
+
+        String level1 =
+                "╔════┐\n" +
+                "║1..2│\n" +
+                "║....│\n" +
+                "║....│\n" +
+                "║4..3│\n" +
+                "└────┘\n";
+        String forces1 =
+                "------\n" +
+                "----♦-\n" +
+                "------\n" +
+                "------\n" +
+                "-♠--♣-\n" +
+                "------\n";
+        assertL(level1, PLAYER2);
+        assertE(forces1, PLAYER2);
+        assertL(level1, PLAYER3);
+        assertE(forces1, PLAYER3);
+        assertL(level1, PLAYER4);
+        assertE(forces1, PLAYER4);
+
+        String level2 =
+                "╔════┐\n" +
+                "║..1.│\n" +
+                "║4...│\n" +
+                "║...2│\n" +
+                "║.3..│\n" +
+                "└────┘\n";
+        String forces2 =
+                "------\n" +
+                "---♥--\n" +
+                "------\n" +
+                "----♦-\n" +
+                "------\n" +
+                "------\n";
+        assertL(level2, PLAYER5);
+        assertE(forces2, PLAYER5);
+        assertL(level2, PLAYER6);
+        assertE(forces2, PLAYER6);
+
+
+        assertL(LOBBY_LEVEL, PLAYER1);
+        assertE(LOBBY_FORCES, PLAYER1);
     }
 
 }
