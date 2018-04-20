6469986b567ffa8d2865cb061a12315e26082f75
Svetlana Menyaylova <Svetlana_Menyaylova@epam.com>
2018-04-20 11:16:15 +0300

    VB net client created for Snake

---
+++ CodingDojo/games/snake/src/main/vbnet/Board.vb
@@ -0,0 +1,55 @@
+﻿Imports System
+Imports System.Text
+
+Friend Class Board
+
+    Public Property RawBoard As String
+
+    Public Property MapSize As Integer
+
+    Public Sub Parse(ByVal input As String)
+
+        If input.StartsWith("board=") Then
+            input = input.Substring(6)
+        End If
+
+        RawBoard = input.Replace("☼", "#").  ' wall
+        Replace("▲", "0").Replace("◄", "0").Replace("►", "0").Replace("▼", "0").  ' head
+        Replace("║", "o").Replace("═", "o").Replace("╙", "o").Replace("╘", "o").  ' body
+        Replace("╓", "o").Replace("╕", "o").
+        Replace("╗", "o").Replace("╝", "o").Replace("╔", "o").Replace("╚", "o").  ' body
+        Replace("☻", "X").  ' bad apple
+        Replace("☺", "$") ' good apple
+
+        With RawBoard
+
+        End With
+
+        Dim length As Integer = RawBoard.Length
+        MapSize = CType(Math.Sqrt(length), Integer)
+
+    End Sub
+
+    Public Function GetAt(ByVal x As Integer, ByVal y As Integer)
+
+        Return RawBoard(x + y * MapSize)
+
+    End Function
+
+    Public Function GetDisplay() As String
+
+        Dim sb As New StringBuilder()
+
+        For line As Integer = 0 To MapSize - 1
+            If line > 0 Then
+                sb.AppendLine()
+            End If
+            sb.Append("  ")
+            sb.Append(RawBoard.Substring(MapSize * line, MapSize))
+        Next
+
+        Return sb.ToString()
+
+    End Function
+
+End Class
---
+++ CodingDojo/games/snake/src/main/vbnet/MySnakeBot.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+
+Friend Class MySnakeBot
+
+    Public Property HeadlineText As String
+
+    Public Property DisplayText As String
+
+    Public Property CommandText As String
+
+    Public Function Process(ByVal input As String) As String
+
+        HeadlineText = ""
+        DisplayText = ""
+
+        Dim board As New Board()
+        board.Parse(input)
+        DisplayText = board.GetDisplay()
+
+        Dim random As New Random()
+
+        Dim move As Integer = random.Next(0, 3)
+
+        Select Case move
+            Case 0
+                CommandText = "UP"
+            Case 1
+                CommandText = "DOWN"
+            Case 2
+                CommandText = "LEFT"
+            Case 3
+                CommandText = "RIGHT"
+        End Select
+
+        Return CommandText
+
+    End Function
+
+
+End Class
