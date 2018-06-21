d1a150a14c176fd93899432df9322acaf81aa8fd
Nikita Zimin <nzeemin@gmail.com>
2018-06-21 20:21:40 +0300

    Lunolet game basics, WIP

--- CodingDojo/games/lunolet/src/main/java/com/codenjoy/dojo/lunolet/services/GameRunner.java
+++ CodingDojo/games/lunolet/src/main/java/com/codenjoy/dojo/lunolet/services/GameRunner.java
@@ -59,7 +59,7 @@ public class GameRunner extends AbstractGameType implements GameType {
 
     @Override
     public Parameter<Integer> getBoardSize() {
-        return v(0);
+        return v(20);
     }
 
     @Override
@@ -69,7 +69,7 @@ public class GameRunner extends AbstractGameType implements GameType {
 
     @Override
     public Enum[] getPlots() {
-        return new Enum[0];
+        return Elements.values();
     }
 
     @Override
