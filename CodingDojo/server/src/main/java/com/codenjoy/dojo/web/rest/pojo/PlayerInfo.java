96922c2e2b7ad11d826af37415a310f3d8259754
Daniil Nasretdinov <Daniil_Nasretdinov@epam.com>
2018-07-05 10:48:57 +0400

    добавил rest метод возвращающий список игроков и карту по указанной игре

---
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/web/rest/pojo/PlayerInfo.java
@@ -0,0 +1,72 @@
+package com.codenjoy.dojo.web.rest.pojo;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 - 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+public class PlayerInfo {
+  private String name;
+  private Integer score;
+  private int x;
+  private int y;
+
+  public PlayerInfo() {}
+
+  public PlayerInfo(String name, Integer score, int x, int y) {
+    this.name = name;
+    this.score = score;
+    this.x = x;
+    this.y = y;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+}
