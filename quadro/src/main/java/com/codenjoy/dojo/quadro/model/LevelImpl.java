272ef53276d64d67f8b2ebab7ff1bf95675f9c04
Oleh Shklyar <oleh.shklyar@gmail.com>
2018-07-14 18:02:11 +0300

    renaming

---
+++ quadro/src/main/java/com/codenjoy/dojo/quadro/model/LevelImpl.java
@@ -0,0 +1,88 @@
+package com.codenjoy.dojo.quadro.model;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.quadro.model.items.Gold;
+import com.codenjoy.dojo.quadro.model.items.Wall;
+import com.codenjoy.dojo.services.LengthToXY;
+import com.codenjoy.dojo.services.Point;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.codenjoy.dojo.quadro.model.Elements.GOLD;
+import static com.codenjoy.dojo.quadro.model.Elements.HERO;
+import static com.codenjoy.dojo.quadro.model.Elements.WALL;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Полезный утилитный класс для получения объектов на поле из текстового вида.
+ */
+public class LevelImpl implements Level {
+    private final LengthToXY xy;
+
+    private String map;
+
+    public LevelImpl(String map) {
+        this.map = map;
+        xy = new LengthToXY(getSize());
+    }
+
+    @Override
+    public int getSize() {
+        return (int) Math.sqrt(map.length());
+    }
+
+    @Override
+    public List<Hero> getHero() {
+        return pointsOf(HERO).stream()
+                .map(Hero::new)
+                .collect(toList());
+
+    }
+
+    @Override
+    public List<Gold> getGold() {
+        return pointsOf(GOLD).stream()
+                .map(Gold::new)
+                .collect(toList());
+    }
+
+    @Override
+    public List<Wall> getWalls() {
+        return pointsOf(WALL).stream()
+                .map(Wall::new)
+                .collect(toList());
+    }
+
+    private List<Point> pointsOf(Elements element) {
+        List<Point> result = new LinkedList<>();
+        for (int index = 0; index < map.length(); index++) {
+            if (map.charAt(index) == element.ch) {
+                result.add(xy.getXY(index));
+            }
+        }
+        return result;
+    }
+}
