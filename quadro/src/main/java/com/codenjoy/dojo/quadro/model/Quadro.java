3dc0e0c0758cf9395d18a2694d188b06f19e643e
alexander.baglay <codenjoyme@gmail.com>
2018-07-19 20:31:05 +0300

    Часть большого рефакторинга

--- quadro/src/main/java/com/codenjoy/dojo/quadro/model/Quadro.java
+++ quadro/src/main/java/com/codenjoy/dojo/quadro/model/Quadro.java
@@ -128,11 +128,12 @@ public class Quadro implements Field {
     }
 
     private void checkWin(Point pt, boolean color) {
-        if (getCount(DOWN, pt, color) >= CHIPS_LENGTH_TO_WIN
-                || (getCount(RIGHT, pt, color) + getCount(LEFT, pt, color)) >= CHIPS_LENGTH_TO_WIN
-                || (getCount(LEFT_DOWN, pt, color) + getCount(RIGHT_UP, pt, color)) >= CHIPS_LENGTH_TO_WIN
-                || (getCount(RIGHT_DOWN, pt, color) + getCount(LEFT_UP, pt, color)) >= CHIPS_LENGTH_TO_WIN)
-        {
+        boolean vertical = getCount(DOWN, pt, color) >= CHIPS_LENGTH_TO_WIN;
+        boolean diagonal1 = (getCount(RIGHT, pt, color) + getCount(LEFT, pt, color)) >= CHIPS_LENGTH_TO_WIN;
+        boolean diagonal2 = (getCount(LEFT_DOWN, pt, color) + getCount(RIGHT_UP, pt, color)) >= CHIPS_LENGTH_TO_WIN;
+        boolean horizontal = (getCount(RIGHT_DOWN, pt, color) + getCount(LEFT_UP, pt, color)) >= CHIPS_LENGTH_TO_WIN;
+
+        if (vertical || diagonal1 || diagonal2 || horizontal) {
             win(color);
         }
     }
