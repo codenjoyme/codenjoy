270630f0a483c7879aa8ab676ba28cd12f0b83ff
Oleksandr Baglai <apofig@gmail.com>
2018-08-08 19:57:38 +0300

    Исправил билд файл

--- CodingDojo/games/engine/setup.bat
+++ CodingDojo/games/engine/setup.bat
@@ -1,19 +1,23 @@
 IF "%JAVA_HOME%"=="" set JAVA_HOME=%CD%\..\..\jdk8
 IF "%M2_HOME%"=="" set M2_HOME=%CD%\..\..\maven
+IF "%CODENJOY_VERSION%"=="" set CODENJOY_VERSION=1.0.25
 
-call %M2_HOME%\bin\mvn install:install-file -Dfile=engine-1.0.25.jar -Dsources=engine-1.0.25-sources.jar -DpomFile=engine-1.0.25-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=1.0.25 -Dpackaging=jar
+call %M2_HOME%\bin\mvn install:install-file -Dfile=engine-%CODENJOY_VERSION%.jar -Dsources=engine-%CODENJOY_VERSION%-sources.jar -DpomFile=engine-%CODENJOY_VERSION%-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=%CODENJOY_VERSION% -Dpackaging=jar
 
 echo off
-echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
-echo [INFO] !!!!    Then press Enter to continue (and wait until the next step installation is complete)
-echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo     +--------------------------------------------------+
+echo     !      Check that maven prints BUILD SUCCESS       !
+echo     !      Then press Enter to continue (and wait      !
+echo     !   until the next step installation is complete)  !
+echo     +--------------------------------------------------+
+echo on
 pause >nul
 
 
-call mvn install:install-file -Dfile=games-1.0.25-pom.xml -DpomFile=games-1.0.24-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=1.0.24 -Dpackaging=pom
-echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo [INFO] !!!!    Check that maven prints BUILD SUCCESS
-echo [INFO] !!!!    Then press Enter to exit
-echo [INFO] !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+call mvn install:install-file -Dfile=games-%CODENJOY_VERSION%-pom.xml -DpomFile=games-%CODENJOY_VERSION%-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=%CODENJOY_VERSION% -Dpackaging=pom
+echo     +--------------------------------------------------+
+echo     !     Check that maven prints BUILD SUCCESS        !
+echo     !           Then press Enter to exit               !
+echo     +--------------------------------------------------+
+echo on
 pause >nul
