607ee59d0607d27d092753c91e48c12ab5c81aa7
alexander.baglay <codenjoyme@gmail.com>
2018-08-10 22:01:35 +0300

    Еще один тест для triple

--- CodingDojo/server/src/test/java/com/codenjoy/dojo/services/multiplayer/MultiplayerServiceImplTest.java
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/services/multiplayer/MultiplayerServiceImplTest.java
@@ -3,6 +3,7 @@ package com.codenjoy.dojo.services.multiplayer;
 import com.codenjoy.dojo.services.*;
 import com.codenjoy.dojo.services.printer.BoardReader;
 import com.codenjoy.dojo.services.printer.PrinterFactory;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,9 +22,10 @@ public class MultiplayerServiceImplTest {
     private PlayerGames playerGames;
     private PrinterFactory printerFactory;
 
-    private GameType single1;
-    private GameType multiple1;
-    private GameType tournament1;
+    private GameType single;
+    private GameType multiple;
+    private GameType tournament;
+    private GameType triple;
 
     private List<GameField> fields = new LinkedList<>();
 
@@ -33,9 +35,10 @@ public class MultiplayerServiceImplTest {
         multiplayer = new MultiplayerServiceImpl(playerGames);
         printerFactory = mock(PrinterFactory.class);
 
-        single1 = setupGameType("single1", MultiplayerType.SINGLE);
-        tournament1 = setupGameType("tournament1", MultiplayerType.TOURNAMENT);
-        multiple1 = setupGameType("multiple1", MultiplayerType.MULTIPLE);
+        single = setupGameType("single", MultiplayerType.SINGLE);
+        tournament = setupGameType("tournament", MultiplayerType.TOURNAMENT);
+        triple = setupGameType("triple", MultiplayerType.TRIPLE);
+        multiple = setupGameType("multiple", MultiplayerType.MULTIPLE);
     }
 
     private GameType setupGameType(String gameName, MultiplayerType type) {
@@ -60,9 +63,9 @@ public class MultiplayerServiceImplTest {
         Player player2 = new Player();
         Player player3 = new Player();
 
-        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(single1, player1, null);
-        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(single1, player2, null);
-        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(single1, player3, null);
+        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(single, player1, null);
+        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(single, player2, null);
+        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(single, player3, null);
 
         GameField field1 = playerGame1.getGame().getField();
         GameField field2 = playerGame2.getGame().getField();
@@ -81,11 +84,11 @@ public class MultiplayerServiceImplTest {
         Player player4 = new Player();
         Player player5 = new Player();
 
-        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(tournament1, player1, null);
-        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(tournament1, player2, null);
-        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(tournament1, player3, null);
-        PlayerGame playerGame4 = multiplayer.playerWantsToPlay(tournament1, player4, null);
-        PlayerGame playerGame5 = multiplayer.playerWantsToPlay(tournament1, player5, null);
+        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(tournament, player1, null);
+        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(tournament, player2, null);
+        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(tournament, player3, null);
+        PlayerGame playerGame4 = multiplayer.playerWantsToPlay(tournament, player4, null);
+        PlayerGame playerGame5 = multiplayer.playerWantsToPlay(tournament, player5, null);
 
         GameField field1 = playerGame1.getGame().getField();
         GameField field2 = playerGame2.getGame().getField();
@@ -96,9 +99,55 @@ public class MultiplayerServiceImplTest {
         assertSame(field1, field2);
         assertSame(field3, field4);
 
-        assertNotSame(field1, field3);
+        assertNotSame(field1, field3, field5);
+    }
+
+    public void assertSame(GameField... fields) {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields.length; j++) {
+                if (j == i) continue;
+                Assert.assertSame(String.format("[%s]==[%s]", i, j),
+                        fields[i], fields[j]);
+            }
+        }
+    }
+
+    public void assertNotSame(GameField... fields) {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields.length; j++) {
+                if (j == i) continue;
+                Assert.assertNotSame(String.format("[%s]!=[%s]", i, j),
+                        fields[i], fields[j]);
+            }
+        }
+    }
+
+    @Test
+    public void shouldThreePlayerOnBoard_whenTriple() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        Player player5 = new Player();
+
+        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(triple, player1, null);
+        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(triple, player2, null);
+        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(triple, player3, null);
+        PlayerGame playerGame4 = multiplayer.playerWantsToPlay(triple, player4, null);
+        PlayerGame playerGame5 = multiplayer.playerWantsToPlay(triple, player5, null);
+
+        GameField field1 = playerGame1.getGame().getField();
+        GameField field2 = playerGame2.getGame().getField();
+        GameField field3 = playerGame3.getGame().getField();
+        GameField field4 = playerGame4.getGame().getField();
+        GameField field5 = playerGame5.getGame().getField();
+
+        assertSame(field1, field2, field3);
+        assertSame(field4, field5);
+
+        assertNotSame(field1, field4);
+        assertNotSame(field2, field5);
         assertNotSame(field3, field5);
-        assertNotSame(field1, field5);
     }
 
     @Test
@@ -107,9 +156,9 @@ public class MultiplayerServiceImplTest {
         Player player2 = new Player();
         Player player3 = new Player();
 
-        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(multiple1, player1, null);
-        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(multiple1, player2, null);
-        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(multiple1, player3, null);
+        PlayerGame playerGame1 = multiplayer.playerWantsToPlay(multiple, player1, null);
+        PlayerGame playerGame2 = multiplayer.playerWantsToPlay(multiple, player2, null);
+        PlayerGame playerGame3 = multiplayer.playerWantsToPlay(multiple, player3, null);
 
         GameField field1 = playerGame1.getGame().getField();
         GameField field2 = playerGame2.getGame().getField();
