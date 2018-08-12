1504bf58670dd977c5326c7e278dc249ce2dc777
vick <vnikitchyn@gmail.com>
2018-08-12 15:52:38 +0300

    created Helper for getting maps from txt files created logic of WIN increase level TODO: Level has to be stored in db or class higher

---
+++ sokoban-text/src/main/java/com/codenjoy/dojo/sampletext/client/ai/ApofigSolver.java
@@ -0,0 +1,67 @@
+package com.codenjoy.dojo.sampletext.client.ai;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.client.AbstractTextBoard;
+import com.codenjoy.dojo.client.AbstractTextSolver;
+import com.codenjoy.dojo.client.WebSocketRunner;
+import com.codenjoy.dojo.sampletext.client.Board;
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.RandomDice;
+
+/**
+ * User: your name
+ * Это твой алгоритм AI для игры. Реализуй его на свое усмотрение.
+ * Обрати внимание на {@see YourSolverTest} - там приготовлен тестовый
+ * фреймворк для тебя.
+ */
+public class ApofigSolver extends AbstractTextSolver {
+
+    private Dice dice;
+    private AbstractTextBoard board;
+
+    public ApofigSolver(Dice dice) {
+        this.dice = dice;
+    }
+
+    @Override
+    public String getAnswer(String question) {
+        return "answer" + question.substring("question".length());
+    }
+
+    public static void main(String[] args) {
+//        LocalGameRunner.run(new GameRunner(),
+//                new ApofigSolver(new RandomDice()),
+//                new Board());
+        start(WebSocketRunner.DEFAULT_USER, WebSocketRunner.Host.LOCAL, new RandomDice());
+    }
+
+    public static void start(String name, WebSocketRunner.Host host, Dice dice) {
+        WebSocketRunner.run(host,
+                name,
+                null,
+                new ApofigSolver(dice),
+                new Board());
+    }
+}
---
+++ sokoban-text/src/main/java/com/codenjoy/dojo/sampletext/services/GameRunner.java
@@ -0,0 +1,125 @@
+package com.codenjoy.dojo.sampletext.services;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2016 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.client.WebSocketRunner;
+import com.codenjoy.dojo.sampletext.client.ai.ApofigSolver;
+import com.codenjoy.dojo.sampletext.model.*;
+import com.codenjoy.dojo.services.*;
+import com.codenjoy.dojo.services.multiplayer.GamePlayer;
+import com.codenjoy.dojo.services.multiplayer.MultiplayerType;
+import com.codenjoy.dojo.services.printer.BoardReader;
+import com.codenjoy.dojo.services.printer.PrinterFactory;
+import com.codenjoy.dojo.services.settings.Parameter;
+import org.json.JSONObject;
+
+import static com.codenjoy.dojo.services.settings.SimpleParameter.v;
+
+/**
+ * Генератор игор - реализация {@see GameType}
+ */
+public class GameRunner extends AbstractGameType implements GameType {
+
+    private final Level level;
+    private SampleText game;
+
+    public GameRunner() {
+        new Scores(0, settings);
+        level = new LevelImpl(
+                "question1=answer1",
+                "question2=answer2",
+                "question3=answer3",
+                "question4=answer4",
+                "question5=answer5",
+                "question6=answer6",
+                "question7=answer7",
+                "question8=answer8",
+                "question9=answer9",
+                "question10=answer10",
+                "question11=answer11",
+                "question12=answer12",
+                "question13=answer13",
+                "question14=answer14",
+                "question15=answer15",
+                "question16=answer16",
+                "question17=answer17",
+                "question18=answer18",
+                "question19=answer19",
+                "question20=answer20"
+        );
+    }
+
+    @Override
+    public SampleText createGame() {
+        return new SampleText(level, getDice());
+    }
+
+    @Override
+    public PlayerScores getPlayerScores(Object score) {
+        return new Scores((Integer)score, settings);
+    }
+
+    @Override
+    public Parameter<Integer> getBoardSize() {
+        return v(0);
+    }
+
+    @Override
+    public String name() {
+        return "sampletext";
+    }
+
+    @Override
+    public Enum[] getPlots() {
+        return new Enum[0];
+    }
+
+    @Override
+    public MultiplayerType getMultiplayerType() {
+        return MultiplayerType.SINGLE;
+    }
+
+    @Override
+    public GamePlayer createPlayer(EventListener listener, String save, String playerName) {
+        return new Player(listener);
+    }
+
+    @Override
+    public boolean newAI(String aiName) {
+        ApofigSolver.start(aiName, WebSocketRunner.Host.REMOTE_LOCAL, getDice());
+        return true;
+    }
+
+    @Override
+    public PrinterFactory getPrinterFactory() {
+        return PrinterFactory.get((BoardReader reader, Player player) -> {
+            JSONObject result = new JSONObject();
+
+            result.put("nextQuestion", player.getNextQuestion());
+            result.put("history", player.getHistory());
+
+            return result;
+        });
+    }
+}
