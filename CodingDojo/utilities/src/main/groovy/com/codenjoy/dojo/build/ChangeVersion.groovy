6cb0a001ab4f1755ceb493417593e4d8f125060d
alexander.baglay <codenjoyme@gmail.com>
2018-08-23 20:48:12 +0300

    Зарелизил новую версию 1.0.26

--- CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
+++ CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
@@ -1,5 +1,5 @@
-def fromVersion = '1.0.24'
-def toVersion = '1.0.25'
+def fromVersion = '1.0.25'
+def toVersion = '1.0.26'
 
 // select root directories
 def base = '..\\..\\..\\..\\..\\..\\..\\..\\';
