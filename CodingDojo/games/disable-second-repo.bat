5dd7375303ea33e52bc0f36715627f2479e2b33e
Oleksandr Baglai <apofig@gmail.com>
2018-08-23 22:46:39 +0300

    Исправил батники для переключения репозиториев

--- CodingDojo/games/disable-second-repo.bat
+++
@@ -1,9 +0,0 @@
-attrib -h .git
-ren .git .git_
-attrib +h .git_
-ren .gitignore .gitignore_
-cd..
-cd..
-attrib -h .git_
-ren .git_ .git
-attrib +h .git
---
+++ second-repo-disable.bat
@@ -0,0 +1,24 @@
+attrib -h .git_
+ren .git_ .git
+attrib +h .git
+ren .gitignore_ .gitignore
+
+cd CodingDojo/repo
+attrib -h .git
+ren .git .git_
+attrib +h .git_
+ren .gitignore .gitignore_
+cd ../..
+
+cd CodingDojo/builder
+attrib -h .git
+ren .git .git_
+attrib +h .git_
+ren .gitignore .gitignore_
+cd ../..
+
+cd CodingDojo/games
+attrib -h .git
+ren .git .git_
+attrib +h .git_
+ren .gitignore .gitignore_
