916abbd36df31555c45d2cd29013bb9a65f6e074
alexander.baglay <apofig@gmail.com>
2018-11-12 14:25:16 +0200

    Сделал те же тесты и на java/scala

--- CodingDojo/games/tetris/src/test/scala/com/codenjoy/dojo/tetris/client/BoardScalaTest.scala
+++ CodingDojo/games/tetris/src/test/scala/com/codenjoy/dojo/tetris/client/BoardScalaTest.scala
@@ -51,18 +51,35 @@ class BoardScalaTest {
       Array[String]("I", "O", "L", "Z"))
 
     assertEquals(Elements.NONE, board.getGlass.getAt(0, 0))
+    assertEquals(false, board.getGlass.isAt(0, 0, Elements.YELLOW))
+    assertEquals(true, board.getGlass.isAt(0, 0, Elements.NONE))
+    assertEquals(true, board.getGlass.isAt(0, 0, Elements.ORANGE, Elements.NONE))
     assertEquals(true, board.isFree(0, 0))
 
     assertEquals(Elements.YELLOW, board.getGlass.getAt(2, 0))
+    assertEquals(true, board.getGlass.isAt(2, 0, Elements.YELLOW))
+    assertEquals(false, board.getGlass.isAt(2, 0, Elements.NONE))
+    assertEquals(false, board.getGlass.isAt(2, 0, Elements.ORANGE, Elements.NONE))
     assertEquals(false, board.isFree(2, 0))
 
     assertEquals(Elements.GREEN, board.getGlass.getAt(2, 2))
+    assertEquals(false, board.getGlass.isAt(2, 2, Elements.YELLOW))
+    assertEquals(false, board.getGlass.isAt(2, 2, Elements.NONE))
+    assertEquals(false, board.getGlass.isAt(2, 2, Elements.ORANGE, Elements.NONE))
     assertEquals(false, board.isFree(2, 2))
 
     assertEquals(Elements.ORANGE, board.getGlass.getAt(3, 4))
+    assertEquals(false, board.getGlass.isAt(3, 4, Elements.YELLOW))
+    assertEquals(false, board.getGlass.isAt(3, 4, Elements.NONE))
+    assertEquals(true, board.getGlass.isAt(3, 4, Elements.ORANGE, Elements.NONE))
     assertEquals(false, board.isFree(3, 0))
 
     assertEquals("[[0,1], [1,1], [1,2], [2,2]]", board.getGlass.get(Elements.GREEN).toString)
+
+    assertEquals("[[2,4], [3,2], [3,3], [3,4]]", board.getGlass.get(Elements.ORANGE).toString)
+
+    assertEquals("[[0,1], [1,1], [1,2], [2,2], [2,4], [3,2], [3,3], [3,4]]", board.getGlass.get(Elements.GREEN, Elements.ORANGE).toString)
+
     assertEquals("[., L, ., L, L, ., I, ., .]", board.getGlass.getNear(pt(3, 4)).toString)
 
     assertEquals("[1,2]", board.getCurrentFigurePoint.toString)
