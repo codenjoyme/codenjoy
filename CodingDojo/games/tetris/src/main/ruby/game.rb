7d2f853548d1f3847658a12e5582b6e4219589f4
alexander.baglay <apofig@gmail.com>
2018-11-12 10:25:40 +0200

    Сделал клиента рабочим для тетриса

--- CodingDojo/games/tetris/src/main/ruby/game.rb
+++ CodingDojo/games/tetris/src/main/ruby/game.rb
@@ -25,85 +25,39 @@ require 'websocket-client-simple'
 
 
 # Check ARGS
-usage = %Q(\n\nPlease run 'ruby game.rb GAME_HOST USERNAME [BLAST_SIZE]'\n\nExample:\nruby game.rb 127.0.0.1:8080 root@localhost.local 4\n\n)
+usage = %Q(\n\nPlease run 'ruby game.rb GAME_HOST USERNAME CODE'\n\nExample:\nruby game.rb 127.0.0.1:8080 root@localhost.local\n\n)
 
 raise usage unless ARGV[0]
 raise usage unless ARGV[1]
-
-# Bomb blast size
-BLAST_SIZE = ARGV[2].to_i || 4
-
-
+raise usage unless ARGV[2]
 
 ##################################### ELEMENTS TYPES #########################################################
 
 ELEMENTS = Hash.new
 
-# This is your Bomberman
-ELEMENTS[:BOMBERMAN] = '☺'             # this is what he usually looks like
-ELEMENTS[:BOMB_BOMBERMAN] = '☻'        # this is if he is sitting on own bomb
-ELEMENTS[:DEAD_BOMBERMAN] = 'Ѡ'        # oops, your Bomberman is dead (don't worry, he will appear
-# somewhere in next move)
-# you're getting -200 for each death
+# This is glass content
+ELEMENTS[:BLUE] = 'I'
+ELEMENTS[:CYAN] = 'J'
+ELEMENTS[:ORANGE] = 'L'
+ELEMENTS[:YELLOW] = 'O'
+ELEMENTS[:GREEN] = 'S'
+ELEMENTS[:PURPLE] = 'T'
+ELEMENTS[:RED] = 'Z'
+ELEMENTS[:NONE] = '.'
 
-# this is other players Bombermans
-ELEMENTS[:OTHER_BOMBERMAN] = '♥'       # this is what other Bombermans looks like
-ELEMENTS[:OTHER_BOMB_BOMBERMAN] = '♠'  # this is if player just set the bomb
-ELEMENTS[:OTHER_DEAD_BOMBERMAN] = '♣'  # enemy corpse (it will disappear shortly, right on the next move)
-# if you've done it you'll get +1000
-
-# the bombs
-ELEMENTS[:BOMB_TIMER_5] = '5'          # after bomberman set the bomb, the timer starts (5 tacts)
-ELEMENTS[:BOMB_TIMER_4] = '4'          # this will blow up after 4 tacts
-ELEMENTS[:BOMB_TIMER_3] = '3'          # this after 3
-ELEMENTS[:BOMB_TIMER_2] = '2'          # two
-ELEMENTS[:BOMB_TIMER_1] = '1'          # one
-ELEMENTS[:BOOM] = '҉'                  # Boom! this is what is bomb does, everything that is destroyable
-# got destroyed
-
-# walls
-ELEMENTS[:WALL] = '☼'                 # indestructible wall - it will not fall from bomb
-ELEMENTS[:DESTROY_WALL] = '#'          # this wall could be blowed up
-
-ELEMENTS[:DESTROYED_WALL] = 'H'        # this is how broken wall looks like, it will dissapear on next move
-# if it's you did it - you'll get +10 points.
-
-# meatchoppers
-ELEMENTS[:MEAT_CHOPPER] = '&'          # this guys runs over the board randomly and gets in the way all the time
-# if it will touch bomberman - it will die
-# you'd better kill this piece of ... meat, you'll get +100 point for it
-ELEMENTS[:DEAD_MEAT_CHOPPER] = 'x'     # this is chopper corpse
-
-# a void
-ELEMENTS[:NONE] = ' '                 # this is the only place where you can move your Bomberman
-
-# List of barriers
-BARRIERS = [
-    ELEMENTS[:BOMB_BOMBERMAN],
-    ELEMENTS[:BOMB_TIMER_1],
-    ELEMENTS[:BOMB_TIMER_2],
-    ELEMENTS[:BOMB_TIMER_3],
-    ELEMENTS[:BOMB_TIMER_4],
-    ELEMENTS[:BOMB_TIMER_5],
-    ELEMENTS[:OTHER_BOMBERMAN],
-    ELEMENTS[:OTHET_BOMB_BOMBERMAN],
-    ELEMENTS[:WALL],
-    ELEMENTS[:DESTROY_WALL],
-]
-
-# List of bombs
-BOMBS = [
-    ELEMENTS[:BOMB_BOMBERMAN],
-    ELEMENTS[:BOMB_TIMER_1],
-    ELEMENTS[:BOMB_TIMER_2],
-    ELEMENTS[:BOMB_TIMER_3],
-    ELEMENTS[:BOMB_TIMER_4],
-    ELEMENTS[:BOMB_TIMER_5],
-    ELEMENTS[:OTHET_BOMB_BOMBERMAN],
+# List of figures
+FIGURES = [
+    ELEMENTS[:BLUE],
+    ELEMENTS[:CYAN],
+    ELEMENTS[:ORANGE],
+    ELEMENTS[:YELLOW],
+    ELEMENTS[:GREEN],
+    ELEMENTS[:PURPLE],
+    ELEMENTS[:RED],
+    ELEMENTS[:NONE],
 ]
 
 ##################################### END OF ELEMENTS TYPES #########################################################
-
 
 # Return list of indexes of char +char+ in string +s+ ("STR".index returns only first char/string appear)
 #
@@ -160,55 +114,55 @@ class Point
   end
 end
 
-# Bomberman class
-class Bomberman
-
-  # Initialize
-  # @param [Game] game game object
-  def initialize(game)
-    @game = game
-  end
-
-  # Is Bomberman alive?!
-  # @return [Boolean] +true+ if my bomberman alive
-  def dead?
-    board.index(ELEMENTS[:DEAD_BOMBERMAN]) != nil
-  end
-
-  # Can I move in specified direction?
-  #
-  # @param [String] direction string of direction - 'UP', 'DOWN', 'LEFT', 'RIGHT'
-  # @return [Boolean] true/false
-  def can_move?(direction)
-    point = position
-    !@game.is_barrier_at?(point.send(direction.downcase))
-  end
-
-  # Return current position of Bomberman on field
-  # @return [Point] position of bomberman
-  def position
-    pos = @game.board.index(ELEMENTS[:BOMBERMAN])
-    pos = @game.board.index(ELEMENTS[:DEAD_BOMBERMAN]) unless pos
-    pos = @game.board.index(ELEMENTS[:BOMB_BOMBERMAN]) unless pos
-
-    @game.pos_to_coords(pos)
-  end
-
-  # What will be position of bomberman after move in specified direction
-  #
-  # @param [String] direction - 'UP', 'DOWN', 'LEFT', 'RIGHT'
-  # @return [Point] position after move
-  def position_after_move(direction)
-    point = position
-    point.send(direction.downcase)
-  end
-end
+# # Bomberman class
+# class Bomberman
+#
+#   # Initialize
+#   # @param [Game] game game object
+#   def initialize(game)
+#     @game = game
+#   end
+#
+#   # Is Bomberman alive?!
+#   # @return [Boolean] +true+ if my bomberman alive
+#   def dead?
+#     board.index(ELEMENTS[:DEAD_BOMBERMAN]) != nil
+#   end
+#
+#   # Can I move in specified direction?
+#   #
+#   # @param [String] direction string of direction - 'UP', 'DOWN', 'LEFT', 'RIGHT'
+#   # @return [Boolean] true/false
+#   def can_move?(direction)
+#     point = position
+#     !@game.is_barrier_at?(point.send(direction.downcase))
+#   end
+#
+#   # Return current position of Bomberman on field
+#   # @return [Point] position of bomberman
+#   def position
+#     pos = @game.board.index(ELEMENTS[:BOMBERMAN])
+#     pos = @game.board.index(ELEMENTS[:DEAD_BOMBERMAN]) unless pos
+#     pos = @game.board.index(ELEMENTS[:BOMB_BOMBERMAN]) unless pos
+#
+#     @game.pos_to_coords(pos)
+#   end
+#
+#   # What will be position of bomberman after move in specified direction
+#   #
+#   # @param [String] direction - 'UP', 'DOWN', 'LEFT', 'RIGHT'
+#   # @return [Point] position after move
+#   def position_after_move(direction)
+#     point = position
+#     point.send(direction.downcase)
+#   end
+# end
 
 
 # Game class
 class Game
   attr_accessor :board
-  attr_reader :bomberman
+  # attr_reader :bomberman
 
   # Returns board size
   # @return [Integer] board size
@@ -216,29 +170,29 @@ class Game
     Math.sqrt(board.length).to_i
   end
 
-  # Retruns bomberman position
-  # @return [Bomberman] returns bomberman object
-  def bomberman
-    @bomberman ||= Bomberman.new(self)
-  end
+  # # Retruns bomberman position
+  # # @return [Bomberman] returns bomberman object
+  # def bomberman
+  #   @bomberman ||= Bomberman.new(self)
+  # end
 
-  # Returns array of other bomberman positions
+  # # Returns array of other bomberman positions
+  # #
+  # # @return [Array[Point]] array of other bomberman`s positions
+  # def get_other_bombermans
+  #   res = []
+  #   bombers = []
+  #   bombers += indexes(board, ELEMENTS[:OTHER_BOMBERMAN])
+  #   bombers += indexes(board, ELEMENTS[:OTHER_DEAD_BOMBERMAN])
+  #   bombers += indexes(board, ELEMENTS[:OTHER_BOMB_BOMBERMAN])
   #
-  # @return [Array[Point]] array of other bomberman`s positions
-  def get_other_bombermans
-    res = []
-    bombers = []
-    bombers += indexes(board, ELEMENTS[:OTHER_BOMBERMAN])
-    bombers += indexes(board, ELEMENTS[:OTHER_DEAD_BOMBERMAN])
-    bombers += indexes(board, ELEMENTS[:OTHER_BOMB_BOMBERMAN])
-
-    # BOMBERS
-    bombers.each do |pos|
-      res << pos_to_coords(pos)
-    end
-
-    res
-  end
+  #   # BOMBERS
+  #   bombers.each do |pos|
+  #     res << pos_to_coords(pos)
+  #   end
+  #
+  #   res
+  # end
 
   # Get object at position
   #
@@ -295,146 +249,104 @@ class Game
     res ? res.size : 0
   end
 
-  # Check if barrier (elements of +BARRIERS+ array) at position
+  # Check if figures (elements of +FIGURES+ array) at position
   #
   # @param [Point] point position
   # @return [Boolean] true if barrier at
-  def is_barrier_at?(point)
+  def is_busy_at?(point)
     element = board[coords_to_pos(point)]
-    BARRIERS.include? element
+    FIGURES.include? element
   end
 
-  # List of the barriers on the field
+  # List of busy spaces in the glass
   #
   # @return [Array[Point]] list of barriers on the filed
-  def get_barriers
+  def get_figures
     res = []
     pos = 0
     board.chars.each do |ch|
-      res << pos_to_coords(pos) if BARRIERS.include? ch
+      res << pos_to_coords(pos) if FIGURES.include? ch
       pos += 1
     end
 
     res
   end
 
-  # Return list of meat choppers on field
-  #
-  # @return [Array[Point]] array of meat choppers positions
-  def get_meat_choppers
-    res = []
-    pos = 0
-    board.chars.each do |ch|
-      res << pos_to_coords(pos) if ch == ELEMENTS[:MEAT_CHOPPER]
-      pos += 1
-    end
-
-    res
-  end
-
-  # Return list of walls on field
+  # Return list of free spaces in the glass
   #
   # @return [Array[Point]] array of walls positions
-  def get_walls
+  def get_free_space
     res = []
     pos = 0
     board.chars.each do |ch|
-      res << pos_to_coords(pos) if ch == ELEMENTS[:WALL]
+      res << pos_to_coords(pos) if ch == ELEMENTS[:NONE]
       pos += 1
     end
 
     res
   end
 
-  # Return list of destroyable walls on field
+  # # Return list of positions where blast will be in next few tacts
+  # #
+  # # @return [Array[Point]] array of positions where blast will be in next few tacts
+  # def get_future_blasts
+  #   res = []
   #
-  # @return [Array[Point]] array of destroyable walls positions
-  def get_destroy_walls
-    res = []
-    pos = 0
-    board.chars.each do |ch|
-      res << pos_to_coords(pos) if ch == ELEMENTS[:WALL]
-      pos += 1
-    end
-
-    res
-  end
-
-  # Return list of bombs on field
+  #   get_bombs.each do |bomb|
+  #     directions = {:up => true, :down => true, :left => true, :right => true}
+  #     res << bomb
   #
-  # @return [Array[Point]] array of bombs positions
-  def get_bombs
-    res = []
-    pos = 0
-    board.chars.each do |ch|
-      res << pos_to_coords(pos) if BOMBS.include? ch
-      pos += 1
-    end
-
-    res
-  end
-
-  # Return list of positions where blast will be in next few tacts
+  #     (1..4).each do |sh|
+  #       # x + N
+  #       x = Point.new(bomb.x + sh, bomb.y)
+  #       if is_barrier_at?(x)
+  #         directions[:right] = false
+  #       elsif !is_barrier_at?(x) && directions[:right]
+  #         res << x
+  #       end
   #
-  # @return [Array[Point]] array of positions where blast will be in next few tacts
-  def get_future_blasts
-    res = []
-
-    get_bombs.each do |bomb|
-      directions = {:up => true, :down => true, :left => true, :right => true}
-      res << bomb
-
-      (1..BLAST_SIZE).each do |sh|
-        # x + N
-        x = Point.new(bomb.x + sh, bomb.y)
-        if is_barrier_at?(x)
-          directions[:right] = false
-        elsif !is_barrier_at?(x) && directions[:right]
-          res << x
-        end
-
-        # x - N
-        x = Point.new(bomb.x - sh, bomb.y)
-        if is_barrier_at?(x)
-          directions[:left] = false
-        elsif !is_barrier_at?(x) && directions[:left]
-          res << x
-        end
-
-        # y + N
-        x = Point.new(bomb.x, bomb.y + sh)
-        if is_barrier_at?(x)
-          directions[:down] = false
-        elsif !is_barrier_at?(x) && directions[:down]
-          res << x
-        end
-
-        # y - N
-        x = Point.new(bomb.x, bomb.y - sh)
-        if is_barrier_at?(x)
-          directions[:up] = false
-        elsif !is_barrier_at?(x) && directions[:up]
-          res << x
-        end
-
-      end
-    end
-
-    res
-  end
-
-  # Is danger at position?
+  #       # x - N
+  #       x = Point.new(bomb.x - sh, bomb.y)
+  #       if is_barrier_at?(x)
+  #         directions[:left] = false
+  #       elsif !is_barrier_at?(x) && directions[:left]
+  #         res << x
+  #       end
   #
-  # @param [Point] point position
-  # @return [Boolean] true if danger at position
-  def danger_at?(point)
-    dangers = []
+  #       # y + N
+  #       x = Point.new(bomb.x, bomb.y + sh)
+  #       if is_barrier_at?(x)
+  #         directions[:down] = false
+  #       elsif !is_barrier_at?(x) && directions[:down]
+  #         res << x
+  #       end
+  #
+  #       # y - N
+  #       x = Point.new(bomb.x, bomb.y - sh)
+  #       if is_barrier_at?(x)
+  #         directions[:up] = false
+  #       elsif !is_barrier_at?(x) && directions[:up]
+  #         res << x
+  #       end
+  #
+  #     end
+  #   end
+  #
+  #   res
+  # end
 
-    dangers += get_meat_choppers
-    dangers += get_future_blasts
-
-    dangers.include?(point)
-  end
+  # # Is danger at position?
+  # #
+  # # @param [Point] point position
+  # # @return [Boolean] true if danger at position
+  # def danger_at?(point)
+  #   dangers = []
+  #
+  #   dangers += get_meat_choppers
+  #   dangers += get_future_blasts
+  #
+  #   dangers.include?(point)
+  # end
 
   # How far specified element from position (strait direction)
   # Return +board_size+ if wall in specified direction
@@ -487,10 +399,8 @@ class Game
 end
 
 
-
 # WebSocket object to connect to Codenjoy server
-ws = WebSocket::Client::Simple.connect "ws://#{ARGV[0]}/codenjoy-contest/ws?user=#{ARGV[1]}"
-
+ws = WebSocket::Client::Simple.connect "ws://#{ARGV[0]}/codenjoy-contest/ws?user=#{ARGV[1]}&code=#{ARGV[2]}"
 
 # Default direction
 direction = 'DOWN'
@@ -505,9 +415,8 @@ ws.on :message do |msg|
     board = $1.force_encoding('UTF-8')
     game.board = board
 
-    # Bomberman object
-    bomber = game.bomberman
-
+    # # Bomberman object
+    # bomber = game.bomberman
 
     ############################################################################################################
     #
@@ -521,27 +430,29 @@ ws.on :message do |msg|
 
 
     # Place bomb if wall nearby
-    act = game.count_near(bomber.position, ELEMENTS[:DESTROY_WALL]) > 0
+    # act = game.count_near(bomber.position, ELEMENTS[:DESTROY_WALL]) > 0
+
+    direction = 'DOWN'
 
     # Change direction if bomberman can't move in specified direction
-    if direction.empty? || !bomber.can_move?(direction)
-      ['DOWN', 'UP', 'LEFT', 'RIGHT'].each do |dir|
-        if bomber.can_move?(dir)
-          direction = dir
-          break
-        end
-      end
-    end
+    # if direction.empty? || !bomber.can_move?(direction)
+    #   ['DOWN', 'UP', 'LEFT', 'RIGHT'].each do |dir|
+    #     if bomber.can_move?(dir)
+    #       direction = dir
+    #       break
+    #     end
+    #   end
+    # end
 
     # Don't move in to danger places
-    if game.danger_at?(bomber.position_after_move(direction))
-      ['DOWN', 'UP', 'RIGHT', 'LEFT'].each do |dir|
-        if !game.danger_at?(bomber.position_after_move(dir)) && bomber.can_move?(dir)
-          direction = dir
-          break
-        end
-      end
-    end
+    # if game.danger_at?(bomber.position_after_move(direction))
+    #   ['DOWN', 'UP', 'RIGHT', 'LEFT'].each do |dir|
+    #     if !game.danger_at?(bomber.position_after_move(dir)) && bomber.can_move?(dir)
+    #       direction = dir
+    #       break
+    #     end
+    #   end
+    # end
 
     ############################################################################################################
     #
@@ -552,7 +463,8 @@ ws.on :message do |msg|
 
 
     # Send Direction and Place bomb (ACT) actions to server
-    ws.send "#{act ? 'ACT' : ''} #{direction.to_s.upcase}"
+    # ws.send "#{act ? 'ACT' : ''} #{direction.to_s.upcase}"
+    ws.send "#{direction.to_s.upcase}"
   rescue Exception => e
     puts e.message
     puts e.backtrace
