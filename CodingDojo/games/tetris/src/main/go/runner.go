f12089f5ad54b3968ee803e065d24a19d65eace2
alexander.baglay <apofig@gmail.com>
2018-11-14 12:40:00 +0200

    Убрали код профайлера из GO

--- CodingDojo/games/tetris/src/main/go/runner.go
+++ CodingDojo/games/tetris/src/main/go/runner.go
@@ -22,20 +22,14 @@
 package main
 
 import (
+	"./network"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"runtime/pprof"
 	"strings"
-	"time"
-
-	"./network"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-var memprofile = flag.String("memprofile", "", "write memory profile to this file")
 var urlStr = flag.String("url", "ws://127.0.0.1:8080/codenjoy-contest/ws?user=apofig@gmail.com&code=20010765231070354251", "server url")
 
 func main() {
@@ -48,43 +42,10 @@ func main() {
 		network.NewClient(id, url)
 	}
 
-	if *memprofile != "" {
-		go func() {
-			for {
-				time.Sleep(time.Second * 2)
-				f, err := os.Create(*memprofile)
-				if err != nil {
-					log.Fatal(err)
-				}
-				pprof.WriteHeapProfile(f)
-				f.Close()
-			}
-		}()
-	}
-
-	if *cpuprofile != "" {
-		go func() {
-			for {
-				f, err := os.Create(*cpuprofile)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				fmt.Println("CPU profile start")
-				pprof.StartCPUProfile(f)
-				time.Sleep(time.Second * 30)
-				pprof.StopCPUProfile()
-				fmt.Println("CPU profile done")
-				f.Close()
-			}
-		}()
-	}
-
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 	<-interrupt
 	log.Println("exiting")
 	// To cleanly close a connection, a client should send a close
 	// frame and wait for the server to close the connection.
-
 }
