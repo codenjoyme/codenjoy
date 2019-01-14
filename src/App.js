1ab58ce4c46cb2cdabe297b50bac41913ead32f0
Pavel Mashchenko <pmashchenko@gmail.com>
2019-01-14 13:18:58 +0200

    adding uui epam main css and fonts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import 'css/style/uui-all.css';
+import 'css/fonts/font-awesome/css/font-awesome.css';
 
 // proj
 import store, { history, persistor } from './store';
