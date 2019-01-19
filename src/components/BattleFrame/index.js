d09db172d7def4b7dff8af66cb425c6ea4ad9595
Oleh Yanivskyy <oleh.yanivskyy@gmail.com>
2019-01-19 13:34:17 +0200

    Adjust board container styles

--- src/components/BattleFrame/index.js
+++ src/components/BattleFrame/index.js
@@ -1,22 +1,24 @@
 // vendor
 import React, { Component } from 'react';
-import Iframe from 'react-iframe';
+
+import Styles from './styles.module.css';
 
 class BattleFrameHandler extends Component {
     render() {
         const { participant: { email, server } = {} } = this.props;
 
         return email && server ? (
-            <Iframe
-                url={ `http://${server}/codenjoy-contest/board/player/${email}?only=true` }
-                width='400px'
-                height='400px'
-                //className={ }
-                id='myId'
-                display='initial'
-                position='relative'
-                allowFullScreen
-            />
+            <div className={ Styles.ratioWrapper }>
+                <div className={ Styles.ratioSquare }>
+                    <iframe
+                        className={ Styles.ratioInner }
+                        src={
+                            'http://www.youtube.com/embed/xDMP3i36naA' ||
+                            `http://${server}/codenjoy-contest/board/player/${email}?only=true`
+                        }
+                    />
+                </div>
+            </div>
         ) : null;
     }
 }
