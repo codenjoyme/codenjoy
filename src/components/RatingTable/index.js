d98566dd03ed63a545c6b5a06ca73255fd09e27f
Oleh Yanivskyy <oleh.yanivskyy@gmail.com>
2019-01-24 10:28:12 +0200

    Add spinner loading to rating table

--- src/components/RatingTable/index.js
+++ src/components/RatingTable/index.js
@@ -157,7 +157,13 @@ class RatingTableHandler extends Component {
             </div>
         ) : (
             <div>
-                Інформація про рейтинг відсутня. Спробуйте обрати інший день
+                <div className={ Styles.spinner }>
+                    <div className={ Styles.rect1 } />
+                    <div className={ Styles.rect2 } />
+                    <div className={ Styles.rect3 } />
+                    <div className={ Styles.rect4 } />
+                    <div className={ Styles.rect5 } />
+                </div>
             </div>
         );
     }
