d2f37c0461708a02da7d06637af0afab409fb5ef
alexander.baglay <apofig@gmail.com>
2019-01-28 01:03:32 +0200

    Добавил описание элементам

--- CodingDojo/games/snake-battle/src/main/java/com/codenjoy/dojo/snakebattle/model/Elements.java
+++ CodingDojo/games/snake-battle/src/main/java/com/codenjoy/dojo/snakebattle/model/Elements.java
@@ -31,33 +31,35 @@ import com.codenjoy.dojo.services.printer.CharElements;
  */
 public enum Elements implements CharElements {
 
-    NONE(' '),       // например это пустое место, куда можно перейти герою
-    WALL('☼'),       // а это стенка, через которую я хочу чтобы проходить нельзя было
-    START_FLOOR('#'),       // место старта змей
-    OTHER('?'), // TODO а это что за чудо?
+    NONE(' '),         // пустое место
+    WALL('☼'),         // а это стенка
+    START_FLOOR('#'),  // место старта змей
+    OTHER('?'),        // этого ты никогда не увидишь :)
 
-    APPLE('○'),
-    STONE('●'),
-    FLYING_PILL('©'),
-    FURY_PILL('®'),
-    GOLD('$'),
+    APPLE('○'),        // яблоки надо кушать от них становишься длинее
+    STONE('●'),        // а это кушать не стоит - от этого укорачиваешься
+    FLYING_PILL('©'),  // таблетка полета - дает суперсилы
+    FURY_PILL('®'),    // таблетка ярости - дает суперсилы
+    GOLD('$'),         // золото - просто очки
 
-    // игрок
+    // голова твоей змеи в разных состояниях и напрвлениях
     HEAD_DOWN('▼'),
     HEAD_LEFT('◄'),
     HEAD_RIGHT('►'),
     HEAD_UP('▲'),
-    HEAD_DEAD('☻'),
-    HEAD_EVIL('♥'),
-    HEAD_FLY('♠'),
-    HEAD_SLEEP('&'),
+    HEAD_DEAD('☻'),    // этот раунд ты проиграл
+    HEAD_EVIL('♥'),    // скушали таблетку ярости
+    HEAD_FLY('♠'),     // скушали таблетку полета
+    HEAD_SLEEP('&'),   // змейка ожидает начала раунда
 
+    // хвост твоей змейки
     TAIL_END_DOWN('╙'),
     TAIL_END_LEFT('╘'),
     TAIL_END_UP('╓'),
     TAIL_END_RIGHT('╕'),
     TAIL_INACTIVE('~'),
 
+    // туловище твоей змейки
     BODY_HORIZONTAL('═'),
     BODY_VERTICAL('║'),
     BODY_LEFT_DOWN('╗'),
@@ -65,22 +67,24 @@ public enum Elements implements CharElements {
     BODY_RIGHT_DOWN('╔'),
     BODY_RIGHT_UP('╚'),
 
-    // противник
+    // змейки противников
     ENEMY_HEAD_DOWN('˅'),
     ENEMY_HEAD_LEFT('<'),
     ENEMY_HEAD_RIGHT('>'),
     ENEMY_HEAD_UP('˄'),
-    ENEMY_HEAD_DEAD('☺'),
-    ENEMY_HEAD_EVIL('♣'),
-    ENEMY_HEAD_FLY('♦'),
-    ENEMY_HEAD_SLEEP('ø'),
+    ENEMY_HEAD_DEAD('☺'),   // этот раунд противник проиграл
+    ENEMY_HEAD_EVIL('♣'),   // противник скушал таблетку ярости
+    ENEMY_HEAD_FLY('♦'),    // противник скушал таблетку полета
+    ENEMY_HEAD_SLEEP('ø'),  // змейка ожидает начала раунда
 
+    // хвосты змеек противников
     ENEMY_TAIL_END_DOWN('¤'),
     ENEMY_TAIL_END_LEFT('×'),
     ENEMY_TAIL_END_UP('æ'),
     ENEMY_TAIL_END_RIGHT('ö'),
     ENEMY_TAIL_INACTIVE('*' ),
 
+    // туловище змеек противников
     ENEMY_BODY_HORIZONTAL('─'),
     ENEMY_BODY_VERTICAL('│'),
     ENEMY_BODY_LEFT_DOWN('┐'),
