a26a74dbb5fa543f23ee382bac67115d54e798a3
Volodymyr Vreshch <vreshch@users.noreply.github.com>
2019-01-28 21:51:52 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-HOST="root@jschallenge.vreshch.com"
+# HOST="root@jschallenge.vreshch.com"
 
-echo 'JS challenge: deploy the project via ssh';
+# echo 'JS challenge: deploy the project via ssh';
 
-curl "http://178.238.230.131:1234/run?password=${CI_DEPLOY_PWD}&command=server.rebuild";
+# curl "http://178.238.230.131:1234/run?password=${CI_DEPLOY_PWD}&command=server.rebuild";
