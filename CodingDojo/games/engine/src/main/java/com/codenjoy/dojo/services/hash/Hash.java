83d4948987b7fed46753d0cba9c6736d268e143d
alexander.baglay <apofig@gmail.com>
2019-02-05 23:30:22 +0200

    Антирефакторинг

--- CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/hash/Hash.java
+++ CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/hash/Hash.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.services.hash;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -28,8 +28,10 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang.StringUtils.reverse;
 
 public class Hash {
 
@@ -38,32 +40,24 @@ public class Hash {
     }
 
     public static String getId(String email, String soul) {
-        if (StringUtils.isEmpty(email)) {
-            return email;
-        }
-
-        String encoded = xor(email, soul);
-        encoded = shuffle(encoded, shuffleSoul(soul, true));
-        return encode(encoded);
+        return (StringUtils.isEmpty(email)) ? email : encode(shuffle(xor(email, soul), shuffleSoul(soul), true));
     }
 
-    private static String shuffleSoul(String soul, boolean inverted) {
-        soul = encode(md5(soul) + md5(soul + "qwe"));
-        return inverted ? StringUtils.reverse(soul) : soul;
+    public static String getEmail(String id, String soul) {
+        return (StringUtils.isEmpty(id)) ? id : xor(shuffle(decode(id), shuffleSoul(soul), false), soul);
+    }
+
+    // TODO сделать метод получения хеша с испольованием соли
+    public static String getCode(String email, String password) {
+        return "" + Math.abs(email.hashCode()) + Math.abs(password.hashCode());
+    }
+
+    private static String shuffleSoul(String soul) {
+        return encode(md5(soul) + md5(soul + "qwe"));
     }
 
     private static String encode(String encoded) {
         return ZBase32Encoder.encode(encoded.getBytes());
-    }
-
-    public static String getEmail(String id, String soul) {
-        if (StringUtils.isEmpty(id)) {
-            return id;
-        }
-
-        String data = decode(id);
-        data = shuffle(data, shuffleSoul(soul, false));
-        return xor(data, soul);
     }
 
     private static String decode(String id) {
@@ -73,26 +67,22 @@ public class Hash {
     private static String xor(String email, String soul) {
         String h1 = md5(soul);
         String h2 = h1 + md5(h1);
-
         AtomicInteger i = new AtomicInteger();
-        return email.chars()
-                .map(ch -> ch ^ h1.codePointAt(i.addAndGet(h2.codePointAt(i.get() % h1.length())) % h1.length()))
-                .collect(StringBuilder::new,
-                        StringBuilder::appendCodePoint,
-                        StringBuilder::append)
-                .toString();
+        return toString(getChars(email).map(ch -> (char)(ch ^ h1.codePointAt(i.addAndGet(h2.codePointAt(i.get() % h1.length())) % h1.length()))));
     }
 
-    private static String shuffle(String input, String soul) {
-        List<Character> l = input.chars()
-                .mapToObj(c -> new Character((char) c))
-                .collect(toList());
+    private static String shuffle(String input, String soul, boolean inverted) {
+        List<Character> l = getChars(input).collect(toList());
+        (inverted ? reverse(soul) : soul).chars().forEach(c -> swap((c >>> 12) ^ c >> input.length(), c ^ (c >>> 3), l));
+        return toString(l.stream());
+    }
 
-        soul.chars().forEach(c -> {
-            swap((c >>> 12) ^ c >> input.length(), c ^ (c >>> 3), l);
-        });
+    private static Stream<Character> getChars(String input) {
+        return input.chars().mapToObj(c -> (char) c);
+    }
 
-        return l.stream().collect(StringBuilder::new,
+    private static String toString(Stream<Character> stream) {
+        return stream.collect(StringBuilder::new,
                 StringBuilder::appendCodePoint,
                 StringBuilder::append)
                 .toString();
@@ -102,11 +92,6 @@ public class Hash {
         m = m % l.size();
         n = n % l.size();
         l.set(m, l.set(n, l.get(m)));
-    }
-
-    // TODO сделать метод получения хеша с испольованием соли
-    public static String getCode(String email, String password) {
-        return "" + Math.abs(email.hashCode()) + Math.abs(password.hashCode());
     }
 
     public static void main(String[] args) {
