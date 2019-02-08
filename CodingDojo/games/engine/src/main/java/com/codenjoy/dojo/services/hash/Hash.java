5063ee03005f7b8d1b0a2d672331f71f588b9e30
alexander.baglay <apofig@gmail.com>
2019-02-08 08:48:58 +0200

    Вернул алгоритм на место

--- CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/hash/Hash.java
+++ CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/hash/Hash.java
@@ -26,12 +26,7 @@ package com.codenjoy.dojo.services.hash;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static org.apache.commons.lang.StringUtils.reverse;
 
 public class Hash {
 
@@ -40,58 +35,44 @@ public class Hash {
     }
 
     public static String getId(String email, String soul) {
-        return (StringUtils.isEmpty(email)) ? email : encode(shuffle(xor(email, soul), shuffleSoul(soul), true));
+        if (StringUtils.isEmpty(email)) {
+            return email;
+        }
+
+        String encoded = xor(email, soul);
+        return ZBase32Encoder.encode(encoded.getBytes());
     }
 
     public static String getEmail(String id, String soul) {
-        return (StringUtils.isEmpty(id)) ? id : xor(shuffle(decode(id), shuffleSoul(soul), false), soul);
+        if (StringUtils.isEmpty(id)) {
+            return id;
+        }
+
+        String data = new String(ZBase32Encoder.decode(id));
+        return xor(data, soul);
+    }
+
+    private static String xor(String email, String soul) {
+        String hash = md5(soul);
+
+        AtomicInteger index = new AtomicInteger();
+        return email.chars()
+                .map(ch -> {
+                    int i = index.getAndIncrement();
+                    if (i >= hash.length()) {
+                        i = i % hash.length();
+                    }
+                    return ch ^ hash.codePointAt(i);
+                })
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
     }
 
     // TODO сделать метод получения хеша с испольованием соли
     public static String getCode(String email, String password) {
         return "" + Math.abs(email.hashCode()) + Math.abs(password.hashCode());
-    }
-
-    private static String shuffleSoul(String soul) {
-        return encode(md5(soul) + md5(soul + "qwe"));
-    }
-
-    private static String encode(String encoded) {
-        return ZBase32Encoder.encode(encoded.getBytes());
-    }
-
-    private static String decode(String id) {
-        return new String(ZBase32Encoder.decode(id));
-    }
-
-    private static String xor(String email, String soul) {
-        String h1 = md5(soul);
-        String h2 = h1 + md5(h1);
-        AtomicInteger i = new AtomicInteger();
-        return toString(getChars(email).map(ch -> (char)(ch ^ h1.codePointAt(i.addAndGet(h2.codePointAt(i.get() % h1.length())) % h1.length()))));
-    }
-
-    private static String shuffle(String input, String soul, boolean inverted) {
-        List<Character> l = getChars(input).collect(toList());
-        (inverted ? reverse(soul) : soul).chars().forEach(c -> swap((c >>> 12) ^ c >> input.length(), c ^ (c >>> 3), l));
-        return toString(l.stream());
-    }
-
-    private static Stream<Character> getChars(String input) {
-        return input.chars().mapToObj(c -> (char) c);
-    }
-
-    private static String toString(Stream<Character> stream) {
-        return stream.collect(StringBuilder::new,
-                StringBuilder::appendCodePoint,
-                StringBuilder::append)
-                .toString();
-    }
-
-    private static void swap(int n, int m, List<Character> l) {
-        m = m % l.size();
-        n = n % l.size();
-        l.set(m, l.set(n, l.get(m)));
     }
 
     public static void main(String[] args) {
--- CodingDojo/games/engine/src/test/java/com/codenjoy/dojo/services/hash/HashTest.java
+++ CodingDojo/games/engine/src/test/java/com/codenjoy/dojo/services/hash/HashTest.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.services.hash;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -24,11 +24,6 @@ package com.codenjoy.dojo.services.hash;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 
 
@@ -36,154 +31,32 @@ public class HashTest {
 
     @Test
     public void testEncodeEmailToIdAndBack() {
-        assertHashes("soul",
-                    "apofig@gmail.com\n" +
-                    "apofig@gmail.comd\n" +
-                    "qwertyuiop[]asdfghjkl;'zxvbnm,./!@#$%^&*()_+~`1234567890-=QWERTYUIOP{}ASDFGHJKL:\"||ZXCVBNM<>?\\/\n" +
-                    "1\n" +
-                    "12\n" +
-                    "123\n" +
-                    "1234\n" +
-                    "12345\n" +
-                    "123456\n" +
-                    "1234567\n" +
-                    "12345678\n" +
-                    "123456789\n" +
-                    "1234567890\n" +
-                    "12345678901\n" +
-                    "123456789012\n" +
-                    "1234567890123\n" +
-                    "12345678901234\n" +
-                    "123456789012345\n" +
-                    "1234567890123456\n" +
-                    "afia7o8g48w74gf4\n" +
-                    "I&X#F&I!#C#KXF*OC\n" +
-                    "GI&F&IDo7diXCCYkl8\n" +
-                    "O&*Do&do&de6o&do7D\n" +
-                    "O&*d6IO&^Do7DO7e^o&\n" +
-                    "I&io&cO&*c6\n" +
-                    "(PG&opD&^IO^5sdio&^dOC\n" +
-                    "O78F^&O6Fo&fr6O8\n" +
-                    "1\n" +
-                    "11\n" +
-                    "111\n" +
-                    "1111\n" +
-                    "11111\n" +
-                    "111111\n" +
-                    "1111111\n" +
-                    "11111111\n" +
-                    "111111111\n" +
-                    "1111111111\n" +
-                    "11111111111\n" +
-                    "111111111111\n" +
-                    "1111111111111\n" +
-                    "11111111111111\n" +
-                    "111111111111111\n" +
-                    "1111111111111111\n" +
-                    "11111111111111111\n" +
-                    "111111111111111111\n" +
-                    "1111111111111111111\n" +
-                    "11111111111111111111\n" +
-                    "111111111111111111111\n" +
-                    "1111111111111111111111\n" +
-                    "11111111111111111111111\n" +
-                    "111111111111111111111111\n" +
-                    "1111111111111111111111111\n" +
-                    "11111111111111111111111111\n" +
-                    "111111111111111111111111111\n" +
-                    "o87f67D^o&r6FP&*F",
+        assertHash("apofig@gmail.com", "soul", "k3dfwbacm74feznamcgthbk6bh");
+        assertHash("apofig@gmail.comd", "soul", "k3dfwbacm74feznamcgthbk6b7ky");
+        assertHash("apofig@gmail.com", "G(^D@F(@&If2d", "yemy1byxyf4ygzkzmy8b1y16ba");
 
-                    "bymya1ygkhffoidtbrnogzn4by\n" +
-                    "brniqiykmjmio1yemtaocdyeycmy\n" +
-                    "ytqog44zyakrexnhbbgwsfmqyhei1naqkw8bnfo6ncdwq8yiyffyg71fejyd6fe5jfggc81ey3miqwo8yrffrfacktfispbsq3tuk1mcxthuc8earbo8kj3nqf9z4k1hehqw6433r3zzq6bmmjpih5a6\n" +
-                    "yh\n" +
-                    "kodo\n" +
-                    "ktkoq\n" +
-                    "kieieba\n" +
-                    "ktkoqint\n" +
-                    "yikfkb4wkr\n" +
-                    "kwdykwkwkodo\n" +
-                    "ktkycbkty7ki1\n" +
-                    "mfkfninimodokbo\n" +
-                    "ktqfebagkfciknef\n" +
-                    "kiciniy8ytqycbkwbr\n" +
-                    "bfciebntkodoeiefy3qy\n" +
-                    "kwnyqsery3kfnbejktkia\n" +
-                    "ytkycsejmonykik1y7kiniy\n" +
-                    "bfjfkikomodykwkwyhnfebn3\n" +
-                    "ktkfqik3y7ey1begkijfnbyrmo\n" +
-                    "kayfkwogkhbonzyjbhyoeoobkr\n" +
-                    "8ao86fkxxia8wxt8n3hrym1ge3yy\n" +
-                    "mtxwym48xetohjk1qiauanmqriqow\n" +
-                    "kynraoyqnwoohondkihon7eybfqoh\n" +
-                    "yo7wakebmat819m6ehyiri44eb7g4ta\n" +
-                    "b7dowod9yp8zhwn8me\n" +
-                    "byy1noaqep4zcw548hythpuopy7iataiqamo\n" +
-                    "87ei1fnzxrifcfm6m3engr4orc\n" +
-                    "yh\n" +
-                    "khdo\n" +
-                    "k7moq\n" +
-                    "k7kfqba\n" +
-                    "k7moqwnw\n" +
-                    "yjmiqb4oko\n" +
-                    "khyyrinzkydo\n" +
-                    "k7eyyy1wy7miy\n" +
-                    "kbmiewnzkodoryy\n" +
-                    "k7kfybayktefqnyn\n" +
-                    "k7efeia8ytkyyy1oby\n" +
-                    "bbefybnwkhdoqianybky\n" +
-                    "khnyqwy8ybmieyoekbmie\n" +
-                    "ytmoywyekodori4zy7miewy\n" +
-                    "bbmiqi4wkoyyrinoyhnfqb4o\n" +
-                    "kbmiyi4oy7kyoyoyk7mieby8ko\n" +
-                    "k7moqy1wyhbyoi4oyoyfywnwktmo\n" +
-                    "yobfqi4oyjkfyi4wyydieyy8khrfy\n" +
-                    "kbkfqi4wybmoqnyykydiewyrkhbyrwy\n" +
-                    "yhnfqinzkbkyriyekybfqynwy7eyyi4o\n" +
-                    "y7mooinoyjkfywnoytkfqyy8bbmiqy1wyy\n" +
-                    "kbkfqinoyeyyeiarkbkyqiaey7kyrwnzbyyy\n" +
-                    "kbmiqbayktkyriyrybkyob4okhbfqwnobynyq\n" +
-                    "kybfyi4wyhdieb4zkbmiqnnwyybyywnwbynyeia\n" +
-                    "kynyqwnzyjmiyiyyytkfqi4wkybyyi4wbydoob4z\n" +
-                    "yhnyoynokyyyriyeytkfqi4zkybfei4wk7kfyia8yh\n" +
-                    "yhnfqwnoyeyfyinzkodiqinzkteyownzyoryqi4wyeyy\n" +
-                    "kr5f1bndyb4t6enze74u61akypxy");
+        assertHash("qwertyuiop[]asdfghjkl;'zxvbnm,./!@#$%^&*()_+~`1234567890-=QWERTYUIOP{}ASDFGHJKL:\"||ZXCVBNM<>?\\/",
+                "soul",
+                "e3yiyratefyiwz1jpr6fnfkiytmi6sapmy8tguy7npmy4snkjhqbc7oseiygcro3dreg411qyayfyyadyteygdypy3rfo3jwqy4dk4unx77dn81fqio8k95iff7n49kanjfw4xdcq7t8ek3ebbqowq1q");
 
-        assertHash("1", "F@#*O2gt", "ya");
+        assertHash("1", "soul", "ya");
+        assertHash("12", "soul", "yany");
+        assertHash("123", "soul", "yanyc");
+        assertHash("1234", "soul", "yanycie");
+        assertHash("12345", "soul", "yanyciko");
+        assertHash("123456", "soul", "yanycikoba");
+        assertHash("1234567", "soul", "yanycikobabo");
+        assertHash("12345678", "soul", "yanycikobabos");
+        assertHash("123456789", "soul", "yanycikobabosny");
+        assertHash("1234567890", "soul", "yanycikobabosnyj");
+
+        assertHash("1", "F@#*O2gt", "kh");
+
         assertHash("", "soul", "");
         assertHash("", "H*OCv2ck", "");
 
         assertHash(null, "soul", null);
         assertHash(null, "F#*O&GF", null);
-    }
-
-    private void assertHashes(String soul, String expectedEmails, String expectedIds) {
-        List<String> actualIds = getIds(soul, Arrays.asList(expectedEmails.split("\n")));
-        List<String> actualEmails = getEmails(soul, actualIds);
-
-        assertEquals(expectedEmails, String.join("\n", actualEmails));
-        assertEquals(expectedIds, String.join("\n", actualIds));
-    }
-
-    @Test
-    public void test() {
-        long time = Calendar.getInstance().getTimeInMillis();
-        for (int i = 0; i < 10_000; i++) {
-            assertHash("apofig@gmail.com", "G(^D@F(@&If2d", "bomy6dkaywyygsy8bwboqj1eba");
-        }
-        System.out.println(Calendar.getInstance().getTimeInMillis() - time);
-    }
-
-    private List<String> getIds(String soul, List<String> emails) {
-        return emails.stream()
-                .map(email -> Hash.getId(email, soul))
-                .collect(toList());
-    }
-
-    private List<String> getEmails(String soul, List<String> ids) {
-        return ids.stream()
-                .map(id -> Hash.getEmail(id, soul))
-                .collect(toList());
     }
 
     private void assertHash(String email, String soul, String expectedHash) {
