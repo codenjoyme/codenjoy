a02ac2a8d8987995fd984f0eded0b29385b892f0
alexander.baglay <apofig@gmail.com>
2019-02-17 00:16:19 +0200

    Теперь IP сервера берется из ENV и вставляется в nginx конфиг

--- CodingDojo/portable/linux-docker-compose/config.sh
+++ CodingDojo/portable/linux-docker-compose/config.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+eval_echo() {
+    to_run=$1
+    echo "[94m"
+    echo $to_run
+    echo "[0m"
+
+    eval $to_run
+}
+
 echo "[93m"
 for entry in $(cat ./.env)
 do
@@ -10,3 +19,6 @@ do
   fi
 done
 echo "[0m"
+
+eval_echo "sed -i 's/\(server_name \).*\$/\1$SERVER_IP;/' ./config/nginx/domain.conf"
+cat ./config/nginx/domain.conf | grep 'server_name '
