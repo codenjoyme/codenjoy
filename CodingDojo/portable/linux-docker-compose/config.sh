33c96219bce89b9a06a0b1b592ec57d3c0b3253e
alexander.baglay <apofig@gmail.com>
2019-02-16 20:51:50 +0200

    Начал понемногу выделять информацию в конфиги

--- CodingDojo/portable/linux-docker-compose/config.sh
+++ CodingDojo/portable/linux-docker-compose/config.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
-BUILD_SERVER=true
-BUILD_BALANCER=true
-GIT_REPO=https://github.com/codenjoyme/codenjoy.git
-REVISION=master
-CODENJOY_CONTEXT=codenjoy-contest
-GAME=snakebattle
-GAME_PROJECT=snake-battle
-
-BASIC_AUTH_LOGIN=codenjoy
-BASIC_AUTH_PASSWORD=secureBasicAuthPassword
 
 echo "[93m"
-echo "BUILD_SERVER=$BUILD_SERVER"
-echo "BUILD_BALANCER=$BUILD_BALANCER"
-echo "GIT_REPO=$GIT_REPO"
-echo "REVISION=$REVISION"
-echo "GAME=$GAME"
-echo "GAME_PROJECT=$GAME_PROJECT"
-echo "BASIC_AUTH_LOGIN=$BASIC_AUTH_LOGIN"
-echo "BASIC_AUTH_PASSWORD=$BASIC_AUTH_PASSWORD"
+for entry in $(cat ./.env)
+do
+  if [[ ! $entry == \#* ]]
+  then
+    export $entry
+    echo "$entry"
+  fi
+done
 echo "[0m"
+
