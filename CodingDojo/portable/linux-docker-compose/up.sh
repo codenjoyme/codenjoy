eac1cd98f8cce5322f1c7067b8a4226e188c8603
alexander.baglay <apofig@gmail.com>
2019-02-16 23:33:45 +0200

    Порефакторил скрипт и улучшил читабельность

--- CodingDojo/portable/linux-docker-compose/up.sh
+++ CodingDojo/portable/linux-docker-compose/up.sh
@@ -4,17 +4,23 @@ if [ "x$CONFIG" = "x" ]; then
     . config.sh ;
 fi
 
-docker-compose down
+eval_echo() {
+    to_run=$1
+    echo "[94m"
+    echo $to_run
+    echo "[0m"
+
+    eval $to_run
+}
+
+eval_echo "docker-compose down"
 
 if [ "x$OPEN_PORTS" = "xtrue" ]; then
     echo "[93mWARNING: All applications ports are exposed out of containers[0m"
-
-    docker-compose -f docker-compose.yml -f open-ports.yml up -d codenjoy_db
-    sleep 10
-    docker-compose -f docker-compose.yml -f open-ports.yml up -d
-else
-    docker-compose up -d codenjoy_db
-    sleep 10
-    docker-compose up -d
+    open="-f open-ports.yml"
 fi
+
+eval_echo "docker-compose -f docker-compose.yml $open up -d codenjoy_db"
+sleep 10
+eval_echo "docker-compose -f docker-compose.yml $open up -d"
 
