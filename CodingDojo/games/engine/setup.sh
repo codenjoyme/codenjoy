b6ba4b81231d0a13765c9105931e3b87896c1bbb
alexander.baglay <apofig@gmail.com>
2019-03-01 09:23:48 +0200

    Протестил и исправил скрипт

--- CodingDojo/games/engine/setup.sh
+++ CodingDojo/games/engine/setup.sh
@@ -6,24 +6,24 @@ fi
 
 mvn install:install-file -Dfile=engine-$CODENJOY_VERSION.jar -Dsources=engine-$CODENJOY_VERSION-sources.jar -DpomFile=engine-$CODENJOY_VERSION-pom.xml -DgroupId=com.codenjoy -DartifactId=engine -Dversion=$CODENJOY_VERSION -Dpackaging=jar
 
-echo "[94m"
+echo "[93m"
 echo "       +--------------------------------------------------+"
 echo "       !           Check that BUILD was SUCCESS           !"
 echo "       !      Then press Enter to continue (and wait      !"
 echo "       !   until the next step installation is complete)  !"
 echo "       +--------------------------------------------------+"
 echo "[0m"
-if [ "x$DEBUG" = "x" ]; then
-    pause >nul
+if [ "x$DEBUG" = "xtrue" ]; then
+    read
 fi
 
 mvn install:install-file -Dfile=games-$CODENJOY_VERSION-pom.xml -DpomFile=games-$CODENJOY_VERSION-pom.xml -DgroupId=com.codenjoy -DartifactId=games -Dversion=$CODENJOY_VERSION -Dpackaging=pom
-echo "[94m"
+echo "[93m"
 echo "       +--------------------------------------------------+"
 echo "       !         Check that BUILD was SUCCESS             !"
 echo "       !           Then press Enter to exit               !"
 echo "       +--------------------------------------------------+"
 echo "[0m"
-if [ "x$DEBUG" = "x" ]; then
-    pause >nul
+if [ "x$DEBUG" = "xtrue" ]; then
+    read
 fi
