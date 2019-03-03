564b1a2ff0f5cae068b8782bcd7db4771ff4c065
alexander.baglay <apofig@gmail.com>
2019-03-03 20:25:05 +0200

    Скрипт для быстрого получения логов в многоконфиговом пространстве

---
+++ CodingDojo/portable/linux-docker-compose/logs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "x$CONFIG" = "x" ]; then
+    . config.sh ;
+fi
+
+service=$1
+
+eval_echo() {
+    to_run=$1
+    echo "[94m"
+    echo $to_run
+    echo "[0m"
+
+    eval $to_run
+}
+
+if [ "x$PGADMIN" = "xtrue" ]; then
+    pgadmin="-f pgadmin.yml"
+fi
+
+if [ "x$BALANCER" = "xtrue" ]; then
+    balancer="-f balancer.yml"
+fi
+
+if [ "x$CODENJOY" = "xtrue" ]; then
+    codenjoy="-f codenjoy.yml"
+fi
+
+eval_echo "docker-compose -f docker-compose.yml $balancer $codenjoy $pgadmin logs $service"
