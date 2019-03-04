2594099ba80071f6a490fc00ae1f3168a3ce1351
alexander.baglay <apofig@gmail.com>
2019-03-04 12:43:54 +0200

    Более гуманный скрипт удаления всех скриптов

--- CodingDojo/portable/linux-docker-compose/all-remove.sh
+++ CodingDojo/portable/linux-docker-compose/all-remove.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-cd ./applications
-bash force-remove-all-images.sh
-cd ..
+# cd ./applications
+# bash force-remove-all-images.sh
+# cd ..
+
+docker-compose -f docker-compose.yml -f codenjoy.yml -f wordpress.yml down
 
 rm -rf ./applications
 rm -rf ./config
