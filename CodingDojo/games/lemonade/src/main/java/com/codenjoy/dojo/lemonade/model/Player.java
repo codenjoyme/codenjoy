3f19767eb055272bd06c8629375fad374cbaceec
Nikita Zimin <nzeemin@gmail.com>
2019-03-12 21:41:25 +0300

    lemonade game stub copied from sample-text

---
+++ CodingDojo/games/lemonade/src/main/java/com/codenjoy/dojo/lemonade/model/Player.java
@@ -0,0 +1,119 @@
+package com.codenjoy.dojo.lemonade.model;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.lemonade.services.Events;
+import com.codenjoy.dojo.services.EventListener;
+import com.codenjoy.dojo.services.Point;
+import com.codenjoy.dojo.services.multiplayer.GamePlayer;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Класс игрока. Тут кроме героя может подсчитываться очки.
+ * Тут же ивенты передабтся лиснеру фреймворка.
+ */
+public class Player extends GamePlayer<Hero, Field> {
+
+    private Field field;
+    private List<QuestionAnswer> history;
+    private int questionIndex;
+    Hero hero;
+
+    public Player(EventListener listener) {
+        super(listener);
+        history = new LinkedList<>();
+    }
+
+    public void clearScore() {
+        if (history != null) {
+            history.clear();
+        }
+        questionIndex = 0;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void newHero(Field field) {
+        hero = new Hero();
+        this.field = field;
+        hero.init(field);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return hero != null && hero.isAlive();
+    }
+
+    public String getNextQuestion() { // TODO test me
+        if (field.isLastQuestion(questionIndex)) {
+            return "You win!";
+        }
+        return field.getQuestion(questionIndex);
+    }
+
+    public List<QuestionAnswer> getHistory() {
+        List<QuestionAnswer> result = new LinkedList<>();
+        result.addAll(history);
+        return result;
+    }
+
+    public void checkAnswer() {
+        hero.tick();
+
+        String answer = hero.popAnswer();
+        if (answer != null && !field.isLastQuestion(questionIndex)) {
+            String question = field.getQuestion(questionIndex);
+            String validAnswer = field.getAnswer(questionIndex);
+            if (validAnswer.equals(answer)) {
+                logSuccess(question, answer);
+                event(Events.WIN);
+                questionIndex++;
+            } else {
+                logFailure(question, answer);
+                event(Events.LOOSE);
+                questionIndex = 0;
+            }
+        }
+    }
+
+    private void logSuccess(String question, String answer) {
+        log(question, answer, true);
+    }
+
+    private void logFailure(String question, String answer) {
+        log(question, answer, false);
+    }
+
+    private void log(String question, String answer, boolean valid) {
+        QuestionAnswer qa = new QuestionAnswer(question, answer);
+        qa.setValid(valid);
+        history.add(qa);
+    }
+}
---
+++ CodingDojo/games/lemonade/src/main/java/com/codenjoy/dojo/lemonade/services/GameRunner.java
@@ -0,0 +1,131 @@
+package com.codenjoy.dojo.lemonade.services;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import com.codenjoy.dojo.client.ClientBoard;
+import com.codenjoy.dojo.client.Solver;
+import com.codenjoy.dojo.lemonade.client.Board;
+import com.codenjoy.dojo.lemonade.client.ai.AISolver;
+import com.codenjoy.dojo.lemonade.model.*;
+import com.codenjoy.dojo.services.*;
+import com.codenjoy.dojo.services.multiplayer.GamePlayer;
+import com.codenjoy.dojo.services.multiplayer.MultiplayerType;
+import com.codenjoy.dojo.services.printer.BoardReader;
+import com.codenjoy.dojo.services.printer.PrinterFactory;
+import com.codenjoy.dojo.services.settings.Parameter;
+import org.json.JSONObject;
+
+import static com.codenjoy.dojo.services.settings.SimpleParameter.v;
+
+/**
+ * Генератор игор - реализация {@see GameType}
+ */
+public class GameRunner extends AbstractGameType implements GameType {
+
+    private final Level level;
+    private Lemonade game;
+
+    public GameRunner() {
+        new Scores(0, settings);
+        level = new LevelImpl(
+                "question1=answer1",
+                "question2=answer2",
+                "question3=answer3",
+                "question4=answer4",
+                "question5=answer5",
+                "question6=answer6",
+                "question7=answer7",
+                "question8=answer8",
+                "question9=answer9",
+                "question10=answer10",
+                "question11=answer11",
+                "question12=answer12",
+                "question13=answer13",
+                "question14=answer14",
+                "question15=answer15",
+                "question16=answer16",
+                "question17=answer17",
+                "question18=answer18",
+                "question19=answer19",
+                "question20=answer20"
+        );
+    }
+
+    @Override
+    public Lemonade createGame(int levelNumber) {
+        return new Lemonade(level, getDice());
+    }
+
+    @Override
+    public PlayerScores getPlayerScores(Object score) {
+        return new Scores((Integer)score, settings);
+    }
+
+    @Override
+    public Parameter<Integer> getBoardSize() {
+        return v(0);
+    }
+
+    @Override
+    public String name() {
+        return "lemonade";
+    }
+
+    @Override
+    public Enum[] getPlots() {
+        return new Enum[0];
+    }
+
+    @Override
+    public Class<? extends Solver> getAI() {
+        return AISolver.class;
+    }
+
+    @Override
+    public Class<? extends ClientBoard> getBoard() {
+        return Board.class;
+    }
+
+    @Override
+    public MultiplayerType getMultiplayerType() {
+        return MultiplayerType.SINGLE;
+    }
+
+    @Override
+    public GamePlayer createPlayer(EventListener listener, String playerName) {
+        return new Player(listener);
+    }
+
+    @Override
+    public PrinterFactory getPrinterFactory() {
+        return PrinterFactory.get((BoardReader reader, Player player) -> {
+            JSONObject result = new JSONObject();
+
+            result.put("nextQuestion", player.getNextQuestion());
+            result.put("history", player.getHistory());
+
+            return result;
+        });
+    }
+}
