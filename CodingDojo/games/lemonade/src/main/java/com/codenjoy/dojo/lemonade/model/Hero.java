4154ad3878624ecad5b18efdf8ce99c577b98702
Nikita Zimin <nzeemin@gmail.com>
2019-03-16 18:32:07 +0300

    lemonade: put Simulator in Hero.

--- CodingDojo/games/lemonade/src/main/java/com/codenjoy/dojo/lemonade/model/Hero.java
+++ CodingDojo/games/lemonade/src/main/java/com/codenjoy/dojo/lemonade/model/Hero.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.lemonade.model;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -28,6 +28,8 @@ import com.codenjoy.dojo.services.joystick.MessageJoystick;
 import com.codenjoy.dojo.services.multiplayer.PlayerHero;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Это реализация героя. Обрати внимание, что он имплементит {@see Joystick}, а значит может быть управляем фреймворком
@@ -36,21 +38,38 @@ import java.util.Arrays;
  */
 public class Hero extends PlayerHero<Field> implements MessageJoystick {
 
+    private Simulator simulator;
     private boolean alive;
     private String answer;
 
     public Hero() {
+        simulator = new Simulator((int) System.currentTimeMillis());
         alive = true;
     }
 
     @Override
     public void init(Field field) {
+        simulator.reset();
+
         this.field = field;
     }
 
     @Override
-    public void message(String answer) {
-        this.answer = answer;
+    public void message(String s) {
+        this.answer = s;
+
+        String command = s.toLowerCase();
+
+        Pattern patternGo = Pattern.compile(
+                "go\\s*(-?[\\d]+)[,\\s]\\s*(-?[\\d]+)[,\\s]\\s*(-?[\\d]+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = patternGo.matcher(command);
+        if (matcher.matches()) {
+            int lemonadeToMake = Integer.parseInt(matcher.group(1));
+            int signsToMake = Integer.parseInt(matcher.group(2));
+            int lemonadePriceCents = Integer.parseInt(matcher.group(3));
+            simulate(lemonadeToMake, signsToMake, lemonadePriceCents);
+            return;
+        }
     }
 
     @Override
@@ -66,5 +85,10 @@ public class Hero extends PlayerHero<Field> implements MessageJoystick {
         String answer = this.answer;
         this.answer = null;
         return answer;
+    }
+
+    private void simulate(int lemonadeToMake, int signsToMake, int lemonadePriceCents) {
+        simulator.step(lemonadeToMake, signsToMake, lemonadePriceCents);
+        //TODO
     }
 }
