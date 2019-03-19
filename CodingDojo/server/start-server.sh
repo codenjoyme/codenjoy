9a7e484105b20a9817ed56aee3ce852f79974c08
Igor Petrov <Igor_Petrov@epam.com>
2019-03-19 12:27:17 +0400

    actualize root readme along with server running script, remove redundant codenjoy-contest.properties file

--- CodingDojo/server/start-server.sh
+++ CodingDojo/server/start-server.sh
@@ -1,1 +1,1 @@
-mvn -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true -Dspring-boot.run.profiles=sqlite,trace,debug clean spring-boot:run -DallGames  # -Dcontext=another-context # -Psnake,bomberman,sample
+mvn -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true -Dspring-boot.run.profiles=sqlite,trace clean spring-boot:run -DallGames  # -Dcontext=another-context # -Psnake,bomberman,sample
