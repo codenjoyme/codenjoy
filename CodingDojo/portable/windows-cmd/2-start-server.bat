4921ababf0876cea053256ca3ce7e33b7e86ab8a
Igor Petrov <Igor_Petrov@epam.com>
2019-03-22 10:22:44 +0400

    adapt batch scripts to spring-boot server application

--- CodingDojo/portable/windows-cmd/2-start-server.bat
+++ CodingDojo/portable/windows-cmd/2-start-server.bat
@@ -15,8 +15,8 @@ IF "%DEBUG%"=="true" (
 
 call explorer http://127.0.0.1:8080/%CONTEXT%
 
-cd %JETTY_HOME%
-%JAVA_HOME%\bin\java -jar start.jar
+cd %APP_HOME%
+%JAVA_HOME%\bin\java -jar server.jar
 
 echo Press any key to exit
 pause >nul
