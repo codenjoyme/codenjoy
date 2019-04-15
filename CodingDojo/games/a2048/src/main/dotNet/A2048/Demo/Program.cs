725d5b3ea7fb6d6471a30e630d7391175862aea6
alexander.baglay <apofig@gmail.com>
2019-04-16 02:55:58 +0300

    С# клиент для 2048

---
+++ CodingDojo/games/a2048/src/main/dotNet/A2048/Demo/Program.cs
@@ -0,0 +1,51 @@
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+using System;
+using System.Threading;
+
+namespace Demo
+{
+    class Program
+    {
+        // codenjoy.com:80  // to use for codenjoy.com server
+        // 127.0.0.1:8080               // to use for localhost server
+        static string ServerUrl = "http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789";
+        
+		// you can get this code after registration on the server with your email
+		// http://server-ip:8080/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth - 3, Console.LargestWindowHeight - 3);
+
+            // creating custom bomberman's Ai client
+
+
+            var bomber = new YourSolver(ServerUrl);
+
+            // starting thread with playing bomberman
+            Thread thread = new Thread(bomber.Play);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
