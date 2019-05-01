ace0f419f68c3b495f7c34742486fff080303102
Shulyatev Vladimir <Vladimir_Shulyatev@epam.com>
2019-05-01 21:24:35 +0300

    First version of two clients - .NET and JS.

---
+++ CodingDojo/games/lemonade/src/main/clients/csharp/LemonadeClient/LemonadeClient/ClientWrapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace LemonadeClient
+{
+	public class ClientWrapper
+	{
+		private WebSocket client = null;
+		private MyLemonadeBot bot = null;
+
+		public ClientWrapper(string url, MyLemonadeBot bot)
+		{
+			client = new WebSocket(url);
+
+			client.OnOpen += Client_OnOpen;
+			client.OnClose += Client_OnClose;
+			client.OnMessage += Client_OnMessage;
+			client.OnError += Client_OnError;
+
+			this.bot = bot;
+
+			client.Connect();
+		}
+
+		private void Client_OnError(object sender, ErrorEventArgs args)
+		{
+			LogMessage($"WebSocket Error: {args.Exception}\r\n");
+		}
+
+		private void Client_OnMessage(object sender, MessageEventArgs args)
+		{
+			if (!args.IsText)
+				return;
+
+			bot.Received(args.Data);
+
+			LogMessage($"Current state: {bot.CurrentState}\r\n");
+			string command = bot.Process();
+			LogMessage($"Sending: {command}\r\n");
+			client.Send(command);
+		}
+
+		private void Client_OnClose(object sender, CloseEventArgs closeEventArgs)
+		{
+			LogMessage("WebSocket Closed\r\n");
+		}
+
+		private void Client_OnOpen(object sender, EventArgs eventArgs)
+		{
+			LogMessage("WebSocket Opened\r\n");
+		}
+
+		private void LogMessage(string text)
+		{
+			Console.WriteLine(text);
+		}
+	}
+}
