d913604c461ad0baa6f4493f8aa1b0cdf8aeda70
Nikita Zimin <nzeemin@gmail.com>
2019-05-04 20:51:43 +0300

    lemonade: Improving tests and Java client.

--- CodingDojo/games/lemonade/src/test/java/com/codenjoy/dojo/lemonade/client/BoardTest.java
+++ CodingDojo/games/lemonade/src/test/java/com/codenjoy/dojo/lemonade/client/BoardTest.java
@@ -22,6 +22,7 @@ package com.codenjoy.dojo.lemonade.client;
  * #L%
  */
 
+import com.codenjoy.dojo.lemonade.model.WeatherForecast;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -35,7 +36,7 @@ public class BoardTest {
                     "            lemonadeSold : 2," +
                     "            lemonadePrice : 0.34," +
                     "            income : 0.00," +
-                    "            lemonadeMade : 3," +
+                    "            lemonadeMade : 4," +
                     "            signsMade : 3," +
                     "            expenses : 4.3," +
                     "            profit : -0.45," +
@@ -64,8 +65,22 @@ public class BoardTest {
     @Test
     public void parseBoard() {
         Board parsedBoard = (Board) new Board().forString(input);
+        Assert.assertEquals(3, parsedBoard.getDay());
         Assert.assertEquals(2, parsedBoard.getHistory().size());
-        Assert.assertNotNull(parsedBoard.getMessages());
+        Assert.assertEquals(1, parsedBoard.getHistory().get(0).getDay());
+        Assert.assertEquals(4, parsedBoard.getHistory().get(0).getLemonadeMade());
+        Assert.assertEquals(3, parsedBoard.getHistory().get(0).getSignsMade());
+        Assert.assertEquals(0.34, parsedBoard.getHistory().get(0).getLemonadePrice(), 0.001);
+        Assert.assertEquals(2, parsedBoard.getHistory().get(0).getLemonadeSold());
+        Assert.assertEquals(4.3, parsedBoard.getHistory().get(0).getExpenses(), 0.001);
+        Assert.assertEquals(0.0, parsedBoard.getHistory().get(0).getIncome(), 0.001);
+        Assert.assertEquals(-0.45, parsedBoard.getHistory().get(0).getProfit(), 0.001);
+        Assert.assertEquals(2.55, parsedBoard.getHistory().get(0).getAssetsAfter(), 0.001);
+        Assert.assertEquals(2, parsedBoard.getHistory().get(1).getDay());
         Assert.assertTrue(parsedBoard.isGameOver());
+        Assert.assertEquals("my test message", parsedBoard.getMessages());
+        Assert.assertEquals(1.55, parsedBoard.getAssets(), 0.001);
+        Assert.assertEquals(0.02, parsedBoard.getLemonadeCost(), 0.001);
+        Assert.assertEquals(WeatherForecast.SUNNY, parsedBoard.getWeatherForecast());
     }
 }
