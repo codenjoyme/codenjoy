7436affb466beba6c08b23236036b8eaa47ef8dd
psop <>
2019-06-03 16:39:55 +0600

    fix springboard and line changer

--- excitebike/src/main/java/com/codenjoy/dojo/excitebike/model/items/Springboard.java
+++ excitebike/src/main/java/com/codenjoy/dojo/excitebike/model/items/Springboard.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.excitebike.model.items;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -29,17 +29,20 @@ import com.codenjoy.dojo.services.PointImpl;
 import com.codenjoy.dojo.services.State;
 
 public class Springboard extends PointImpl implements State<Elements, Player> {
+    private SpringboardElement currentElement;
 
-    public Springboard(int x, int y) {
+    public Springboard(int x, int y, SpringboardElement currentElement) {
         super(x, y);
+        this.currentElement = currentElement;
     }
 
-    public Springboard(Point point) {
+    public Springboard(Point point, SpringboardElement currentElement) {
         super(point);
+        this.currentElement = currentElement;
     }
 
     @Override
     public Elements state(Player player, Object... objects) {
-        return null;
+        return currentElement.getElement();
     }
 }
--- excitebike/src/main/java/com/codenjoy/dojo/excitebike/model/items/SpringboardElement.java
+++ excitebike/src/main/java/com/codenjoy/dojo/excitebike/model/items/SpringboardElement.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.excitebike.model.items;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,30 +23,31 @@ package com.codenjoy.dojo.excitebike.model.items;
  */
 
 import com.codenjoy.dojo.excitebike.model.Elements;
-import com.codenjoy.dojo.excitebike.model.Player;
-import com.codenjoy.dojo.services.Point;
-import com.codenjoy.dojo.services.PointImpl;
-import com.codenjoy.dojo.services.State;
 
-public class LineChanger extends PointImpl implements State<Elements, Player> {
-    private boolean upper;
+public enum SpringboardElement {
+    DARK(Elements.SPRINGBOARD_DARK),
+    LIGHT(Elements.SPRINGBOARD_LIGHT),
+    LEFT_UP(Elements.SPRINGBOARD_LEFT_UP),
+    LEFT_DOWN(Elements.SPRINGBOARD_LEFT_DOWN),
+    RIGHT_UP(Elements.SPRINGBOARD_RIGHT_UP),
+    RIGHT_DOWN(Elements.SPRINGBOARD_RIGHT_DOWN);
 
-    public LineChanger(int x, int y, boolean upper) {
-        super(x, y);
-        this.upper = upper;
+    final Elements element;
+
+    SpringboardElement(Elements element) {
+        this.element = element;
     }
 
-    public LineChanger(Point point, boolean upper) {
-        super(point);
-        this.upper = upper;
+    public Elements getElement() {
+        return element;
     }
 
-    public boolean isUpper() {
-        return upper;
-    }
-
-    @Override
-    public Elements state(Player player, Object... objects) {
-        return isUpper() ? Elements.LINE_CHANGER_UP : Elements.LINE_CHANGER_DOWN;
+    public static SpringboardElement valueOf(Elements element) {
+        for (SpringboardElement el : SpringboardElement.values()) {
+            if (el.getElement() == element) {
+                return el;
+            }
+        }
+        throw new IllegalArgumentException("No such element for " + element);
     }
 }
