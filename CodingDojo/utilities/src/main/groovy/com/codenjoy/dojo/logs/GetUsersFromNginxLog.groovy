6c475f9d5443968747a191656d2c9fa7db7dc42e
Oleksandr Baglai <apofig@gmail.com>
2019-07-09 10:19:22 +0300

    [utils][nginx-log] Теперь умею доставать связку user id + time

--- CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/logs/GetUsersFromNginxLog.groovy
+++ CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/logs/GetUsersFromNginxLog.groovy
@@ -2,6 +2,8 @@ package main.groovy.com.codenjoy.dojo.logs
 
 import org.json.JSONObject
 
+import java.util.regex.Matcher
+
 def base = 'D:\\Work\\2019-07-09\\EPAM\\Парсил логи LearningWeek чтобы достать юзеров\\nginx';
 
 // get all files
@@ -12,18 +14,29 @@ new File(base).eachFile { file ->
 
 // filter and parse lines
 def logs = []
+def actions = [:]
+Matcher mather = "subject" =~ /regex/
 files.each {
     println it
     def text = it.text
     text.split('\n').each { line ->
         def json = new JSONObject(line)
+        def log = json.getString('log')
         if (!'stderr'.equals(json.getString('stream'))
                 && json.has('log')
-                && !json.getString('log').contains('ws?user=0&code=000000000000')
-                && json.getString('log').contains('GET /codenjoy-contest/ws?user='))
+                && !log.contains('ws?user=0&code=000000000000')
+                && log.contains('GET /codenjoy-contest/ws?user='))
         {
             logs << json
-            println json.toString()
+            def time = json.getString('time')
+
+            def prefix = '/ws?user='
+            def suffix = '\u0026code='
+            def playerId = log.substring(
+                    log.indexOf(prefix) + prefix.length(),
+                    log.indexOf(suffix))
+
+            println time + ' > ' + playerId
         }
     }
 }
