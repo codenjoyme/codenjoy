4bb3cbca86b52a0025b7f15f7306af313e6771bb
alexander.baglay <apofig@gmail.com>
2019-09-05 02:45:56 +0300

    [icancode] Вернул назад все варианты игрушек + пофиксил багу с лендингами

--- CodingDojo/server/src/main/webapp/resources/js/admin.js
+++ CodingDojo/server/src/main/webapp/resources/js/admin.js
@@ -32,7 +32,7 @@ function defaultRegistrationSettings() {
         showData4: true,
         defaultGame: null,
         gameTypes: {
-            icancode: ['JavaScript', 'Befunge']
+            icancode: [['JavaScript', 'Java', 'eKids', 'Befunge']]
         }
     };
 }
