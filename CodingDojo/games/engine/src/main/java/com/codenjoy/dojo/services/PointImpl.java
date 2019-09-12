69e4e754907ed73689f829c2c3b426d72f570d89
Oleksandr Baglai <apofig@gmail.com>
2019-09-12 21:56:08 +0300

    [engine] Иногда, когда hero, наследующий в конечном счете от point, не имеет своих координат (они у него -1,-1) то нужно иметь возможность получать доступ к классическим equals/hashcode

--- CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/PointImpl.java
+++ CodingDojo/games/engine/src/main/java/com/codenjoy/dojo/services/PointImpl.java
@@ -99,6 +99,14 @@ public class PointImpl implements Point, Comparable<Point> {
         return x*1000 + y;
     }
 
+    public int parentHashCode() {
+        return super.hashCode();
+    }
+
+    public boolean parentEquals(Object o) {
+        return super.equals(o);
+    }
+
     @Override
     public String toString() {
         return String.format("[%s,%s]", x, y);
