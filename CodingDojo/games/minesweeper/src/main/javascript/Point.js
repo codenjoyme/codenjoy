cdea0462a6c3a45cf9ac7a017203e13fe7ca35fb
vladimir.afanasiev <Vladimir.Afanasiev@wolterskluwer.com>
2020-02-20 13:56:34 +0300

    JS client implemented
    # Conflicts:
    #	CodingDojo/games/minesweeper/src/main/javascript/Api.js
    #	CodingDojo/games/minesweeper/src/main/javascript/Board.js
    #	CodingDojo/games/minesweeper/src/main/javascript/BrowserNodeStub.js
    #	CodingDojo/games/minesweeper/src/main/javascript/BrowserRuner.js
    #	CodingDojo/games/minesweeper/src/main/javascript/Configuration.js
    #	CodingDojo/games/minesweeper/src/main/javascript/Direction.js
    #	CodingDojo/games/minesweeper/src/main/javascript/Element.js
    #	CodingDojo/games/minesweeper/src/main/javascript/NodeJsRuner.js
    #	CodingDojo/games/minesweeper/src/main/javascript/Point.js
    #	CodingDojo/games/minesweeper/src/main/javascript/README.md
    #	CodingDojo/games/minesweeper/src/main/javascript/Solver.js
    #	CodingDojo/games/minesweeper/src/main/javascript/run-client.html

---
+++ CodingDojo/games/minesweeper/src/main/javascript/Point.js
@@ -0,0 +1,81 @@
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 - 2020 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+var Point = function (x, y) {
+    /**
+     * type {Point}
+     */
+    return {
+        equals: function (o) {
+            return o.getX() == x && o.getY() == y;
+        },
+
+        toString: function () {
+            return '[' + x + ',' + y + ']';
+        },
+
+        isOutOf: function (boardSize) {
+            return x >= boardSize || y >= boardSize || x < 0 || y < 0;
+        },
+
+        getX: function () {
+            return x;
+        },
+
+        getY: function () {
+            return y;
+        },
+
+        shiftLeft: function (delta = 1) {
+            return new Point(x - delta, y);
+        },
+
+        shiftRight: function (delta = 1) {
+            return new Point(x + delta, y);
+        },
+
+        shiftTop: function (delta = 1) {
+            return new Point(x, y + delta);
+        },
+
+        shiftBottom: function (delta = 1) {
+            return new Point(x, y - delta);
+        },
+
+        change(direction) {
+            switch (direction) {
+                case Direction.LEFT:
+                    return this.shiftLeft();
+                case Direction.RIGHT:
+                    return this.shiftRight();
+                case Direction.UP:
+                    return this.shiftTop();
+                case Direction.DOWN:
+                    return this.shiftBottom();
+                default:
+                    throw new InvalidOperationException(`Can not change point, using "${direction}" direction`);
+            }
+        }
+    }
+};
+
+
+if (module) module.exports = Point;
