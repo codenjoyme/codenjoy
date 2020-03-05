5089b6d6baf3742d47fcb1e02c322bec7fd261e0
Oleksandr Baglai <apofig@gmail.com>
2020-03-05 19:13:59 +0200

    [balancer] Включаем балансер в работу - выделил константы, отладил процесс сборки

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/web/rest/RestRegistrationController.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/web/rest/RestRegistrationController.java
@@ -31,9 +31,7 @@ import com.codenjoy.dojo.web.rest.pojo.PlayerInfo;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
