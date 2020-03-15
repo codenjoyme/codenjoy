deae408b116bf211769dd500606dc99ccc6f5741
Oleksandr Baglai <apofig@gmail.com>
2020-03-15 23:27:47 +0200

    [server][test] Сделал через TestRunner

--- CodingDojo/server/src/test/java/com/codenjoy/dojo/SmartAssert.java
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/SmartAssert.java
@@ -1,49 +1,71 @@
-package com.codenjoy.dojo.web.rest;
+package com.codenjoy.dojo;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.ComparisonFailure;
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
 
 
+import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.fail;
 
-public class SmartAssert {
+public class SmartAssert extends Runner {
+
+    public static final int STACK_TRACE_COUNT = 10;
+    public static final int REAL_ASSERT_DEPTH = 4;
+    private Class test;
     
-    @Test
-    public void test() {
-        q1();
-        q2();
-        assertEquals("z\n3", "3\nz");
+    // TODO тут надо решить со статикой
+    private static List<Failure> failures = new LinkedList<>();
+
+    public SmartAssert(Class test) {
+        super();
+        this.test = test;
     }
 
-    private void q2() {
-        assertEquals("z\n2", "2\nz");
+    @Override
+    public Description getDescription() {
+        return Description .createTestDescription(test, "This is SmartAssertRunner");
     }
 
-    private void q1() {
-        q3();
+    @Override
+    public void run(RunNotifier notifier) {
+        System.out.println("running the tests from SmartAssert: " + test);
+        try {
+            Object testObject = test.newInstance();
+            for (Method method : test.getMethods()) {
+                if (!method.isAnnotationPresent(Test.class)) continue;
+                
+                notifier.fireTestStarted(description(method));
+                
+                method.invoke(testObject);
+                checkResult();
+                
+                notifier.fireTestFinished(description(method));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void q3() {
-        assertEquals("z", "1");
+    private Description description(Method method) {
+        return Description.createTestDescription(test, method.getName());
     }
 
-    @After
-    public void checkAsserts() {
-        checkResult();
-    }
-    
     private static class Failure {
         private String message;
         private List<StackTraceElement> where;
 
+
         private Failure(String expected, String actual) {
             this.message = new ComparisonFailure("", expected, actual).getMessage();
-            where = getLines(4, 10);
+            where = getLines(REAL_ASSERT_DEPTH, STACK_TRACE_COUNT);
         }
 
         private List<StackTraceElement> getLines(int depth, int count) {
@@ -62,9 +84,10 @@ public class SmartAssert {
                 }
             }};
         }
-
+        
         @Override
         public String toString() {
+            // TODO почему-то тут idea не полхватывает expected: but was:  
             return "org.junit.ComparisonFailure: " + message + "\n"
                     + where.stream()
                         .map(s -> "\t" + s + "\n")
@@ -72,9 +95,7 @@ public class SmartAssert {
         }
     }
 
-    private List<Failure> failures = new LinkedList<>();
-
-    private void assertEquals(Object expected, Object actual) {
+    public static void assertEquals(Object expected, Object actual) {
         try {
             Assert.assertEquals(expected, actual);
         } catch (AssertionError e) {
---
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/web/rest/SmartAssertTest.java
@@ -0,0 +1,29 @@
+package com.codenjoy.dojo.web.rest;
+
+import com.codenjoy.dojo.SmartAssert;
+import static com.codenjoy.dojo.SmartAssert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SmartAssert.class)
+public class SmartAssertTest {
+
+    @Test
+    public void test() {
+        q1();
+        q2();
+        assertEquals("z\n3", "3\nz");
+    }
+
+    private void q2() {
+        assertEquals("z\n2", "2\nz");
+    }
+
+    private void q1() {
+        q3();
+    }
+
+    private void q3() {
+        assertEquals("z", "1");
+    }
+}
