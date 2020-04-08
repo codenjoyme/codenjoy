0557a95c7f2dd47c8a76a07444a92e0a7c0a9e19
Oleksandr Baglai <apofig@gmail.com>
2020-04-08 13:22:21 +0300

    [icancode] Исправил код последнего уровня

--- CodingDojo/games/icancode/src/main/java/com/codenjoy/dojo/icancode/services/levels/LevelZ.java
+++ CodingDojo/games/icancode/src/main/java/com/codenjoy/dojo/icancode/services/levels/LevelZ.java
@@ -17,58 +17,58 @@ public class LevelZ implements Level {
     @Override
     public String winCode() {
         // TODO закончить этот код
-        return "function program(robot) {" +
-                "    var scanner = robot.getScanner();" +
-                "    " +
-                "    var dest = scanner.getGold();" +
-                "    dest = dest.concat(scanner.getExit());" +
-                "    var minLength = 1000;" +
-                "    var minIndex = -1;" +
-                "    for (var index in dest) {" +
-                "        var path = scanner.getShortestWay(dest[index]);" +
-                "        if (path.length < minLength) {" +
-                "            minIndex = index;" +
-                "            minLength = path.length;" +
-                "        }" +
-                "    }" +
-                "    " +
-                "    if (minIndex == -1) {" +
-                "        return;" +
-                "    }" +
-                "    var path = scanner.getShortestWay(dest[minIndex]);" +
-                "    if (path.length === 1) {" +
-                "        return;" +
-                "    }" +
-                "    var to = path[1];" +
-                "    var from = scanner.getMe()" +
-                "" +
-                "    robot.goOverHole = function(direction) {" +
-                "        if (scanner.at(direction) != \"HOLE\") {" +
-                "            robot.go(direction);" +
-                "        } else {" +
-                "            if (direction == \"DOWN\") { // TODO crutch :)" +
-                "                var afterHole = new Point(from.getX(), from.getY() + 2);" +
-                "                if (scanner.at(afterHole) == \"WALL\") {" +
-                "                    robot.go(\"RIGHT\");" +
-                "                    return;" +
-                "                }" +
-                "            }" +
-                "            robot.jump(direction);" +
-                "        }" +
-                "    };" +
-                "    " +
-                "    var dx = to.getX() - from.getX(); " +
-                "    var dy = to.getY() - from.getY(); " +
-                "    if (dx > 0) {" +
-                "        robot.goOverHole(\"RIGHT\");" +
-                "    } else if (dx < 0) {" +
-                "        robot.goOverHole(\"LEFT\");" +
-                "    } else if (dy > 0) {" +
-                "        robot.goOverHole(\"DOWN\");" +
-                "    } else if (dy < 0) {" +
-                "        robot.goOverHole(\"UP\");" +
-                "    }" +
-                "}";
+        return "function program(robot) {\n" +
+                "    var scanner = robot.getScanner();\n" +
+                "    \n" +
+                "    var dest = scanner.getGold();\n" +
+                "    dest = dest.concat(scanner.getExit());\n" +
+                "    var minLength = 1000;\n" +
+                "    var minIndex = -1;\n" +
+                "    for (var index in dest) {\n" +
+                "        var path = scanner.getShortestWay(dest[index]);\n" +
+                "        if (path.length < minLength) {\n" +
+                "            minIndex = index;\n" +
+                "            minLength = path.length;\n" +
+                "        }\n" +
+                "    }\n" +
+                "    \n" +
+                "    if (minIndex == -1) {\n" +
+                "        return;\n" +
+                "    }\n" +
+                "    var path = scanner.getShortestWay(dest[minIndex]);\n" +
+                "    if (path.length === 1) {\n" +
+                "        return;\n" +
+                "    }\n" +
+                "    var to = path[1];\n" +
+                "    var from = scanner.getMe()\n" +
+                "\n" +
+                "    robot.goOverHole = function(direction) {\n" +
+                "        if (scanner.at(direction) != \"HOLE\") {\n" +
+                "            robot.go(direction);\n" +
+                "        } else {\n" +
+                "            if (direction == \"DOWN\") { // TODO crutch :)\n" +
+                "                var afterHole = new Point(from.getX(), from.getY() + 2);\n" +
+                "                if (scanner.at(afterHole) == \"WALL\") {\n" +
+                "                    robot.go(\"RIGHT\");\n" +
+                "                    return;\n" +
+                "                }\n" +
+                "            }\n" +
+                "            robot.jump(direction);\n" +
+                "        }\n" +
+                "    };\n" +
+                "    \n" +
+                "    var dx = to.getX() - from.getX(); \n" +
+                "    var dy = to.getY() - from.getY(); \n" +
+                "    if (dx > 0) {\n" +
+                "        robot.goOverHole(\"RIGHT\");\n" +
+                "    } else if (dx < 0) {\n" +
+                "        robot.goOverHole(\"LEFT\");\n" +
+                "    } else if (dy > 0) {\n" +
+                "        robot.goOverHole(\"DOWN\");\n" +
+                "    } else if (dy < 0) {\n" +
+                "        robot.goOverHole(\"UP\");\n" +
+                "    }\n" +
+                "}\n";
     }
 
     @Override
