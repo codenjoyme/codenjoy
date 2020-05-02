22a520dae43124559ab3d638ec00d3c6f7f34705
Oleksandr Baglai <apofig@gmail.com>
2020-05-02 06:44:22 +0300

    [server][engine][classloader] Немного играю в оптимизацию. Не самое лучшее решение, но работает

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/JarResourceHttpRequestHandler.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/JarResourceHttpRequestHandler.java
@@ -29,8 +29,11 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class JarResourceHttpRequestHandler extends ResourceHttpRequestHandler {
@@ -55,13 +58,15 @@ public class JarResourceHttpRequestHandler extends ResourceHttpRequestHandler {
                     String left = split[0].substring(PREFIX.length());
                     String right = split[1];
                     String middle = right.substring(0, right.indexOf(resourcePath));
+
                     File directory = new File(left);
                     List<File> jars = Arrays.asList(directory.listFiles((dir, name) -> name.endsWith("." + JAR)));
+
+                    Collections.sort(jars,
+                            (jar1, jar2) -> compare(jar1.getPath(), jar2.getPath(), resourcePath));
+
                     for (File jar : jars) {
-                        URL url = new URL(PREFIX + jar.getPath().replace('\\', '/') + middle);
-                        FileUrlResource resource = new FileUrlResource(url);
-                        // TODO тут есть возможность оптимизнуть - что если заглянуть сразу в мой jar глядя на запрос и на название jar и там и там фигурирует имя игры
-                        Resource result = super.getResource(resourcePath, resource);
+                        Resource result = getResource(resourcePath, middle, jar);
                         if (result != null) {
                             return result;
                         }
@@ -70,6 +75,32 @@ public class JarResourceHttpRequestHandler extends ResourceHttpRequestHandler {
 
                 return super.getResource(resourcePath, location);
             }
+
+            private Resource getResource(String resourcePath, String middle, File jar) throws IOException {
+                URL url = new URL(PREFIX + jar.getPath().replace('\\', '/') + middle);
+                FileUrlResource resource = new FileUrlResource(url);
+                return super.getResource(resourcePath, resource);
+            }
         }));
+    }
+
+    private int compare(String left1, String left2, String right) {
+        // разбираем все на токены из слов
+        String regex = "[.\\/-_]";
+        String[] splitL1 = left1.split(regex);
+        String[] splitL2 = left2.split(regex);
+        String[] splitR = right.split(regex);
+
+        // и сравниваем какие пары между собой глубже совпадают
+        return Integer.compare(overlap(splitL2, splitR), overlap(splitL1, splitR));
+    }
+
+    private int overlap(String[] left, String[] right) {
+        // тут хитро, узнаем насколько уменьшилось количество элементов, если мы все запакуем в Set (и устраним дубликаты)
+        return left.length + right.length -
+                new HashSet<String>(){{
+                    addAll(Arrays.asList(left));
+                    addAll(Arrays.asList(right));
+                }}.size();
     }
 }
