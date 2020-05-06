b4b1d179af928f812d115ed6f291ccda67ff5e19
Oleksandr Baglai <apofig@gmail.com>
2020-05-06 21:00:35 +0300

    [build][server][balancer] Скрипты для билда и запуска балансера

--- CodingDojo/start-server.bat
+++ CodingDojo/start-server.bat
@@ -4,7 +4,6 @@ java -jar %ROOT%\server\target\codenjoy-contest.war ^
         --MAVEN_OPTS=-Xmx1024m ^
         --spring.profiles.active=sqlite,debug ^
         --context=/codenjoy-contest ^
-        --server.port=8080 ^
-        --plugins.resources=./external/a2048-engine.jar!/resources/
+        --server.port=8080
  
 pause >nul
