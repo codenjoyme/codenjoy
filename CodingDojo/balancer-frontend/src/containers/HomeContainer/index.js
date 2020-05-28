505739495838218a036116ed5573e720e7bca467
Oleksandr Baglai <apofig@gmail.com>
2020-05-28 05:23:55 +0300

    [balancer][frontend] Исправил описание лендоса

--- CodingDojo/balancer-frontend/src/containers/HomeContainer/index.js
+++ CodingDojo/balancer-frontend/src/containers/HomeContainer/index.js
@@ -17,6 +17,9 @@ import Styles from './styles.module.css';
 
 const startDate = process.env.REACT_APP_EVENT_START_DATE;
 const endDate = process.env.REACT_APP_EVENT_END_DATE;
+const dayTimeStart = process.env.REACT_APP_EVENT_START_TIME;
+const dayTimeEnd = process.env.REACT_APP_EVENT_FINAL_TIME;
+const finalistsCount = process.env.REACT_APP_EVENT_FINALISTS_COUNT;
 const registerEndDate = process.env.REACT_APP_EVENT_REGISTER_END_DATE;
 
 const HomeContainer = () => (
@@ -51,7 +54,13 @@ const HomeContainer = () => (
                     </div>
                     <div className={ Styles.stepTitle }>СТВОРИ</div>
                     <div className={ Styles.stepDescription }>
-                        Створи свого Бота
+                        <div>
+                            Завантаж проект гри
+                        </div>
+                        <br />
+                        <div>
+                            Cтвори логіку для переміщення свого Бота
+                        </div>
                     </div>
                 </div>
                 <div className={ Styles.guideStep }>
@@ -60,7 +69,17 @@ const HomeContainer = () => (
                     </div>
                     <div className={ Styles.stepTitle }>ЗМАГАЙСЯ</div>
                     <div className={ Styles.stepDescription }>
-                        Змагайся з іншими учасниками
+                        <div>
+                            Змагайся з іншими учасниками
+                        </div>
+                        <br />
+                        <div>
+                            Вдосконалюй свого Бота кожен день
+                        </div>
+                        <br />
+                        <div>
+                            Більше модифікацій - більше шансів!
+                        </div>
                     </div>
                 </div>
                 <div className={ Styles.guideStep }>
@@ -69,30 +88,38 @@ const HomeContainer = () => (
                     </div>
                     <div className={ Styles.stepTitle }>ВИГРАВАЙ</div>
                     <div className={ Styles.stepDescription }>
-                        <div>Вигравай!</div>
                         <div>
                             Посідай одне з трьох призових місць і отримай:
                         </div>
-                        <br />
-                        <div>1 місце - ігрова консоль PlayStation 4 Pro 1TB</div>
-                        <div>2 місце - ігрова консоль Sega Mega Drive Mini</div>
-                        <div>3 місце - настільна Hobby World Fallout</div>
+                        <br/>
+                        <ol>
+                            <li>місце - ігрова консоль PlayStation 4 Pro 1TB</li>
+                            <li>місце - ігрова консоль Sega Mega Drive Mini</li>
+                            <li>місце - настільна Hobby World Fallout</li>
+                        </ol>
                     </div>
                 </div>
             </div>
             <div className='title'>Правила гри</div>
             <p>
-                Завантаж проект гри, створи логіку для переміщення свого Бота,
-                отримуй найбільшу кількість балів, щоб увійти до числа
-                фіналістів з 9.00 години { startDate }  до 19.00 години { registerEndDate } року (кожного дня
-                визначаються 10 Фіналістів). Бери участь у Суперфіналі { endDate },
-                займай одне з трьох призових місць і отримай драйвовий
-                подарунок!
+                Завантажуйте проект гри. Створіть логіку для переміщення свого Бота.
+                Отримуйте найбільшу кількість балів, щоб увійти до числа
+                Фіналістів.
             </p>
             <p>
-                Детальні правила і проект гри для створення Бота знайдеш{ ' ' }
+                Конкурс триватиме з { dayTimeStart } години { startDate }&nbsp;
+                до { dayTimeEnd } години { registerEndDate } (за виключенням вихідних).
+                Кожного дня визначаються { finalistsCount } Фіналістів.
+            </p>
+            <p>
+                Беріть участь у Фіналі { endDate }. Займайте одне з трьох призових
+                місць і отримайте драйвовий подарунок!
+            </p>
+            <p>
+                Детальні правила і проект гри для створення Бота шукайте
+                &nbsp;
                 <Link to={ book.rules }>
-                    тут <img src={ Rules } alt='rules' />
+                    за посиланням <img src={ Icon } alt='Правила Конкурсу' />
                 </Link>
             </p>
         </div>
