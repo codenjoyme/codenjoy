ba717ec83a421b63678638e8e0cd666d3798690f
Oleksandr Baglai <apofig@gmail.com>
2020-06-16 20:41:30 +0300

    [balancer][finalists][script] Скрипт модернизируется в то, что нужно

--- CodingDojo/balancer/src/main/java/com/codenjoy/dojo/FinalistsSorter.java
+++ CodingDojo/balancer/src/main/java/com/codenjoy/dojo/FinalistsSorter.java
@@ -1,35 +1,79 @@
 package com.codenjoy.dojo;
 
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Stream;
 
-import static java.util.Comparator.comparingInt;
+import static java.util.Comparator.comparing;
 
 public class FinalistsSorter {
-    public static void main(String[] args) {
-        JSONArray object = new JSONArray("[{\"id\":\"66ulvrge91942grmxua1\",\"name\":\"Stiven Pupkin\",\"score\":133,\"day\":null,\"time\":\"2020-06-15T19:00:05.001+0300\",\"server\":\"botchallenge.cloud.epam.com\",\"winner\":false},\n" +
-                "{\"id\":\"ipipsyrfsxtodwrvqpoa\",\"name\":\"Eva Pupkina\",\"score\":700,\"day\":null,\"time\":\"2020-06-15T19:00:05.001+0300\",\"server\":\"botchallenge.cloud.epam.com\",\"winner\":false}]");
 
-        JSONArray sorted = new JSONArray();
+    public static void main(String[] args) {
+        String day = "day2";
+        JSONArray finalists = load(day + "/finalists.txt");
+        JSONArray users = load(day + "/users.txt");
+        JSONArray todayScores = load(day + "/todayScores.txt");
+
+        // --------
+
+        Map<String, JSONObject> usersMap = new HashMap<>();
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject json = users.getJSONObject(i);
+            usersMap.put(json.getString("id"), json);
+        }
+
+        // --------
+
+        String today = todayScores.getJSONObject(0).getString("time").split("T")[0];
+
+
+
+
+        // ----------
+
         List<JSONObject> list = new ArrayList<>();
-        for (int i = 0; i < object.length(); i++) {
-            JSONObject json = object.getJSONObject(i);
-            json.remove("day");
+        for (int i = 0; i < finalists.length(); i++) {
+            JSONObject json = finalists.getJSONObject(i);
             json.remove("time");
             json.remove("server");
+
+            JSONObject user = usersMap.get(json.getString("id"));
+            copy(user, json, "fullName");
+            copy(user, json, "email");
+            copy(user, json, "phone");
+            copy(user, json, "city");
+            copy(user, json, "skills");
+
             list.add(json);
         }
 
-        Collections.sort(list, comparingInt((JSONObject a) -> a.getInt("score")).reversed());
+        Collections.sort(list, comparing((JSONObject o) -> getString(o)).reversed());
 
-        for (int i = 0; i < object.length(); i++) {
-            sorted.put(list.get(i));
+        list.forEach(it -> System.out.println("\n" + it.toString()));
+    }
+
+    @SneakyThrows
+    private static JSONArray load(String fileName) {
+        File file = new File("battle/" + fileName);
+        StringBuilder result = new StringBuilder();
+        try (Stream linesStream = Files.lines(file.toPath())) {
+            linesStream.forEach(line -> result.append(line));
         }
+        return new JSONArray(result.toString());
+    }
 
-        System.out.println(sorted.toString());
+    private static void copy(JSONObject from, JSONObject to, String key) {
+        to.put(key, from.getString(key));
+    }
+
+    private static String getString(JSONObject o) {
+        String score = String.valueOf(o.getInt("score"));
+        return o.getString("day") + "_" + StringUtils.leftPad(score, 10, "0");
     }
 }
