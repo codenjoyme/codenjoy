3cf030dd0f953d4be56c7c1533dcecf695a9d36c
Oleksandr_Baglai <apofig@gmail.com>
2020-08-18 15:27:35 +0300

    [battlecity] Вот что значит что-то проверить, потом забыть удалить, и наконец-то не поревьювать себя и закоммитить. Какая-то левая строчка попала в репозиторий и из за нее скорее всего battlecity не работает на проде.

--- CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/services/GameRunner.java
+++ CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/services/GameRunner.java
@@ -58,8 +58,6 @@ public class GameRunner extends AbstractGameType implements GameType {
 
     @Override
     public GameField createGame(int levelNumber) {
-        Parameter<Object> parameter = settings.getParameter("name");
-
         return new Battlecity(level.size(),
                 getDice(),
                 level.getConstructions(),
