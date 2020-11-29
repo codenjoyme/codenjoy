dae2fa6a36578b7c8c00dbfebbc30480b04ee9d9
Oleksandr_Baglai <apofig@gmail.com>
2020-11-29 18:36:24 +0200

    [football] Используем новый метод - устранили дублирование

--- CodingDojo/games/football/src/main/java/com/codenjoy/dojo/football/model/LevelImpl.java
+++ CodingDojo/games/football/src/main/java/com/codenjoy/dojo/football/model/LevelImpl.java
@@ -27,10 +27,11 @@ import com.codenjoy.dojo.football.model.elements.Goal;
 import com.codenjoy.dojo.football.model.elements.Hero;
 import com.codenjoy.dojo.football.model.elements.Wall;
 import com.codenjoy.dojo.services.LengthToXY;
-import com.codenjoy.dojo.services.Point;
+import com.codenjoy.dojo.utils.LevelUtils;
 
-import java.util.LinkedList;
 import java.util.List;
+
+import static com.codenjoy.dojo.football.model.Elements.*;
 
 public class LevelImpl implements Level {
 
@@ -50,106 +51,47 @@ public class LevelImpl implements Level {
 
     @Override
     public List<Hero> getHero() {
-        List<Hero> result = new LinkedList<>();
-
-        for (Point pt : getPointsOf(Elements.HERO)) {
-            result.add(new Hero(pt));
-        }
-        
-        for (Point pt : getPointsOf(Elements.HERO_W_BALL)) {
-            result.add(new Hero(pt));
-        }
-        
-        for (Point pt : getPointsOf(Elements.TEAM_MEMBER)) {
-            result.add(new Hero(pt));
-        }
-        
-        for (Point pt : getPointsOf(Elements.TEAM_MEMBER_W_BALL)) {
-            result.add(new Hero(pt));
-        }
-        
-        for (Point pt : getPointsOf(Elements.ENEMY)) {
-            result.add(new Hero(pt));
-        }
-        
-        for (Point pt : getPointsOf(Elements.ENEMY_W_BALL)) {
-            result.add(new Hero(pt));
-        }
-
-        return result;
+        return LevelUtils.getObjects(xy, map,
+                (pt, el) -> new Hero(pt),
+                HERO,
+                HERO_W_BALL,
+                TEAM_MEMBER,
+                TEAM_MEMBER_W_BALL,
+                ENEMY,
+                ENEMY_W_BALL);
     }
 
     @Override
     public List<Wall> getWalls() {
-        List<Wall> result = new LinkedList<>();
-
-        for (Point pt : getPointsOf(Elements.WALL)) {
-            result.add(new Wall(pt));
-        }
-
-        return result;
-    }
-
-    private List<Point> getPointsOf(Elements element) {
-        List<Point> result = new LinkedList<>();
-        for (int index = 0; index < map.length(); index++) {
-            if (map.charAt(index) == element.ch) {
-                result.add(xy.getXY(index));
-            }
-        }
-        return result;
-    }
-    
-    private List<Point> getPointsOf(Elements... elements) {
-        List<Point> result = new LinkedList<>();
-        for (int index = 0; index < map.length(); index++) {
-            for (Elements element : elements) {
-                if (map.charAt(index) == element.ch) {
-                    result.add(xy.getXY(index));
-                    break;
-                }
-            }
-        }
-        return result;
+        return LevelUtils.getObjects(xy, map,
+                (pt, el) -> new Wall(pt),
+                WALL);
     }
 
     @Override
     public List<Ball> getBalls() {
-        List<Ball> result = new LinkedList<>();
-
-        for (Point pt : getPointsOf(Elements.BALL, 
-                                    Elements.STOPPED_BALL, 
-                                    Elements.HERO_W_BALL, 
-                                    Elements.TEAM_MEMBER_W_BALL,
-                                    Elements.ENEMY_W_BALL, 
-                                    Elements.HITED_GOAL,
-                                    Elements.HITED_MY_GOAL
-                                    )) {
-            result.add(new Ball(pt));
-        }
-        
-        return result;
+        return LevelUtils.getObjects(xy, map,
+                (pt, el) -> new Ball(pt),
+                BALL,
+                STOPPED_BALL,
+                HERO_W_BALL,
+                TEAM_MEMBER_W_BALL,
+                ENEMY_W_BALL,
+                HITED_GOAL,
+                HITED_MY_GOAL);
     }
 
     @Override
     public List<Goal> getTopGoals() {
-        List<Goal> result = new LinkedList<>();
-
-        for (Point pt : getPointsOf(Elements.TOP_GOAL)) {
-            result.add(new Goal(pt, Elements.TOP_GOAL));
-        }
-
-        return result;
+        return LevelUtils.getObjects(xy, map,
+                Goal::new,
+                TOP_GOAL);
     }
 
     @Override
     public List<Goal> getBottomGoals() {
-        List<Goal> result = new LinkedList<>();
-
-        for (Point pt : getPointsOf(Elements.BOTTOM_GOAL)) {
-            result.add(new Goal(pt, Elements.BOTTOM_GOAL));
-        }
-
-        return result;
+        return LevelUtils.getObjects(xy, map,
+                Goal::new,
+                BOTTOM_GOAL);
     }
 }
