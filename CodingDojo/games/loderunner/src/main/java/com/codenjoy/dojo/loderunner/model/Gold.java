cf87913298eeebc5084674347d430e8ecbbd7e8e
Oleksandr_Baglai <apofig@gmail.com>
2020-11-29 19:27:46 +0200

    [loderunner] Используем новый метод - устранили дублирование

--- CodingDojo/games/loderunner/src/main/java/com/codenjoy/dojo/loderunner/model/Gold.java
+++ CodingDojo/games/loderunner/src/main/java/com/codenjoy/dojo/loderunner/model/Gold.java
@@ -33,10 +33,6 @@ public class Gold extends PointImpl implements State<Elements, Player> {
         super(point);
     }
 
-    public Gold(int x, int y) {
-        super(x, y);
-    }
-
     @Override
     public Elements state(Player player, Object... alsoAtPoint) {
         return Elements.GOLD;
