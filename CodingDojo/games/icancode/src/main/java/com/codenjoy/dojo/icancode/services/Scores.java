8d55c3db5886ca32e0b602dfa65ccd046da6b655
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2020-12-03 20:48:07 +0200

    [icancode] добавил заработок очков за выход на финальном уровне

--- CodingDojo/games/icancode/src/main/java/com/codenjoy/dojo/icancode/services/Scores.java
+++ CodingDojo/games/icancode/src/main/java/com/codenjoy/dojo/icancode/services/Scores.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.icancode.services;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -58,9 +58,7 @@ public class Scores implements PlayerScores {
         Events.Type eventsType = events.getType();
         switch (eventsType) {
             case WIN:
-                if (!events.isMultiple()) {
-                    score += settings.winScore();
-                }
+                score += settings.winScore();
                 score += settings.goldScore() * events.getGoldCount();
                 break;
             case LOOSE:
