545b796a29c862c81c6840276b3cb289c691906e
Oleksandr_Baglai <apofig@gmail.com>
2020-12-24 19:12:27 +0200

    [git] Bash скрипт для пуша всех репозиториев однгой командой

---
+++ CodingDojo/git-push-submodules.sh
@@ -0,0 +1,37 @@
+unset password
+prompt="Enter Password:"
+while IFS= read -p "$prompt" -r -s -n 1 char
+do
+    if [[ $char == $'\0' ]]
+    then
+        break
+    fi
+    prompt='*'
+    password+="$char"
+done
+
+cd ./games/excitebike
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-excitebike.git master 
+cd ./../..
+
+cd ./games/japanese
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-japanese.git master 
+cd ./../..
+
+cd ./games/selfdefense
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-selfdefense.git master 
+cd ./../..
+
+cd ./portable/linux-docker
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-portable-linux-lite.git master 
+cd ./../..
+
+cd ./portable/linux-docker-compose
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-portable-linux.git master 
+cd ./../..
+
+cd ./portable/windows-cmd
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy-portable-windows.git master 
+cd ./../..
+
+git push https://codenjoyme:$password@github.com/codenjoyme/codenjoy.git master 
