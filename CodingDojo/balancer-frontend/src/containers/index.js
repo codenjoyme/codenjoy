f02b0f41a4270862603ce780b4c07e36470b738c
Oleksandr_Baglai <apofig@gmail.com>
2021-02-16 14:45:06 +0200

    [balancer][frontend] Try to switch import by game

--- CodingDojo/balancer-frontend/src/containers/index.js
+++ CodingDojo/balancer-frontend/src/containers/index.js
@@ -1,13 +1,17 @@
 import BoardContainer from './BoardContainer';
 import LoginContainer from './LoginContainer';
 import RegisterContainer from './RegisterContainer';
-import RulesContainer from './RulesContainer/icancode';
 import HomeContainer from './HomeContainer';
 import PrivacyPolicyContainer from './PrivacyPolicyContainer';
 import PrivacyRulesContainer from './PrivacyRulesContainer';
 import UnavailableContainer from './UnavailableContainer';
 import ForgotPasswordContainer from './ForgotPasswordContainer';
 import RegisterConfirmContainer from './RegisterConfirmContainer';
+
+import RulesContainerICanCode from './RulesContainer/icancode';
+import RulesContainerBomberman from './RulesContainer/bomberman';
+
+const RulesContainer = RulesContainerICanCode;
 
 export {
     BoardContainer,
