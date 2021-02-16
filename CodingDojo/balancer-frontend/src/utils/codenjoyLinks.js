a3891f5138f82af9409707fe52a645d81cdba302
Oleksandr_Baglai <apofig@gmail.com>
2021-02-16 17:10:00 +0200

    [balancer][frontend] Fixed client links

--- CodingDojo/balancer-frontend/src/utils/codenjoyLinks.js
+++ CodingDojo/balancer-frontend/src/utils/codenjoyLinks.js
@@ -2,6 +2,8 @@ const protocol = process.env.REACT_APP_IS_SECURE
     ? process.env.REACT_APP_IS_SECURE
     : 'http';
 
+const game = process.env.REACT_APP_GAME;
+
 export const getGameConnectionString = (server, code, email) =>
     `${protocol}://${server}/codenjoy-contest/board/player/${email}?code=${code}`;
 
@@ -9,7 +11,7 @@ export const getIFrameLink = (server, id) =>
     `${protocol}://${server}/codenjoy-contest/board/player/${id}?only=true`;
 
 export const getJsClient = server =>
-    `${protocol}://${server}/codenjoy-contest/resources/user/icancode-servers.zip`;
+    `${protocol}://${server}/codenjoy-contest/resources/user/${game}-servers.zip`;
 
 export const getJavaClient = server =>
-    `${protocol}://${server}/codenjoy-contest/resources/user/icancode-servers.zip`;
+    `${protocol}://${server}/codenjoy-contest/resources/user/${game}-servers.zip`;
