690852f85100ed8a0e025bddd24abde65f0fa4af
Oleksandr_Baglai <apofig@gmail.com>
2021-02-16 13:04:02 +0200

    [balancer][frontend] Remove dead code

--- CodingDojo/balancer-frontend/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# HOST="root@codenjoy.com"
-
-# echo 'JS challenge: deploy the project via ssh';
-
-# curl "http://123.123.123.123:1234/run?password=${CI_DEPLOY_PWD}&command=server.rebuild";
