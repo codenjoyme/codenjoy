805e7283d120c1448c0e80f98e475f7eafa63d3b
Valentin Chizhov <valentin.chizhov@gmail.com>
2021-02-19 01:02:02 +0300

    Renaming: ICanCodePerksTest.java -> FirePerkTest.java

--- CodingDojo/games/icancode/src/test/java/com/codenjoy/dojo/icancode/model/FirePerkTest.java
+++ CodingDojo/games/icancode/src/test/java/com/codenjoy/dojo/icancode/model/FirePerkTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ICanCodePerksTest extends AbstractGameTest {
+public class FirePerkTest extends AbstractGameTest {
 
 
     @BeforeClass
