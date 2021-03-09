923d722cbeddcefbf9f7662a469228fddf62413e
Oleksandr_Baglai <apofig@gmail.com>
2021-03-09 02:49:13 +0200

    [server][dao] Added javadocs.

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/jdbc/CrudPrimaryKeyConnectionThreadPool.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/jdbc/CrudPrimaryKeyConnectionThreadPool.java
@@ -14,6 +14,10 @@ public abstract class CrudPrimaryKeyConnectionThreadPool extends CrudConnectionT
                 rs -> rs.next() ? rs.getInt(1) : null);
     }
 
+    /**
+     * @return запрос который выдает для конкретной колонки {@param column}
+     *         конкретной таблицы {@param table} последнюю сгенеренную айдишку
+     */
     abstract String getLastInsertedIdQuery(String table, String column);
 
     void createDB(String query) {
@@ -21,8 +25,15 @@ public abstract class CrudPrimaryKeyConnectionThreadPool extends CrudConnectionT
         update(query);
     }
 
+    /**
+     * @return Название типа айдишки чтобы был аутоинкремент
+     */
     abstract String getPkDirective();
 
+    /**
+     * @return запрос который очищает для конкретной колонки {@param column}
+     *         конкретной таблицы {@param table} генератор айдишек
+     */
     abstract String clearLastInsertedIdQuery(String table, String column);
 
     public void clearLastInsertedId(String table, String column) {
