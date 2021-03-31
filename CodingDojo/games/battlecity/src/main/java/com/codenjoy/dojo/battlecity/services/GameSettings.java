35802f37a4065fb0deca37b1afddd274f84ded83
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2021-03-31 10:05:23 +0300

    [battlecity] Settings order

--- CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/services/GameSettings.java
+++ CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/services/GameSettings.java
@@ -46,24 +46,30 @@ public class GameSettings extends SettingsImpl implements SettingsReader<GameSet
 
         MULTIPLE("[Game] Is multiple or disposable"),
         PLAYERS_PER_ROOM("[Game] Players per room for disposable"),
-        KILL_YOUR_TANK_PENALTY("[Score] Kill your tank penalty"),
-        KILL_OTHER_HERO_TANK_SCORE("[Score] Kill other hero tank score"),
-        KILL_OTHER_AI_TANK_SCORE("[Score] Kill other AI tank score"),
+
+        AI_TICKS_PER_SHOOT("[Game] Ticks until the next AI Tank shoot"),
+        TANK_TICKS_PER_SHOOT("[Game] Ticks until the next Tank shoot"),
+        SLIPPERINESS("[Game] Value of tank sliding on ice"),
+        PENALTY_WALKING_ON_WATER("[Game] Penalty time when walking on water"),
+        SHOW_MY_TANK_UNDER_TREE("[Game] Show my tank under tree"),
+
         SPAWN_AI_PRIZE("[Prize] Count spawn for AI Tank with prize"),
         KILL_HITS_AI_PRIZE("[Prize] Hits to kill AI Tank with prize"),
         PRIZE_ON_FIELD("[Prize] The period of prize validity on the field after the appearance"),
         PRIZE_WORKING("[Prize] Working time of the prize after catch up"),
-        AI_TICKS_PER_SHOOT("[Game] Ticks until the next AI Tank shoot"),
-        TANK_TICKS_PER_SHOOT("[Game] Ticks until the next Tank shoot"),
-        SLIPPERINESS("[Game] Value of tank sliding on ice"),
         AI_PRIZE_LIMIT("[Prize] The total number of prize tanks and prizes on the board"),
-        PENALTY_WALKING_ON_WATER("[Game] Penalty time when walking on water"),
-        LEVEL_MAP("[Level] map"),
+
         CHANCE_IMMORTALITY("[Chance] Prize immortality"),
         CHANCE_BREAKING_WALLS("[Chance] Prize breaking walls"),
         CHANCE_WALKING_ON_WATER("[Chance] Prize walking on water"),
         CHANCE_VISIBILITY("[Chance] Prize visibility"),
-        CHANCE_NO_SLIDING("[Chance] Prize no sliding");
+        CHANCE_NO_SLIDING("[Chance] Prize no sliding"),
+
+        KILL_YOUR_TANK_PENALTY("[Score] Kill your tank penalty"),
+        KILL_OTHER_HERO_TANK_SCORE("[Score] Kill other hero tank score"),
+        KILL_OTHER_AI_TANK_SCORE("[Score] Kill other AI tank score"),
+
+        LEVEL_MAP("[Level] map");
 
         private String key;
 
@@ -83,20 +89,6 @@ public class GameSettings extends SettingsImpl implements SettingsReader<GameSet
     }
 
     public GameSettings() {
-        integer(KILL_YOUR_TANK_PENALTY, 0);
-        integer(KILL_OTHER_HERO_TANK_SCORE, 50);
-        integer(KILL_OTHER_AI_TANK_SCORE, 25);
-
-        integer(SPAWN_AI_PRIZE, 4);
-        integer(KILL_HITS_AI_PRIZE, 3);
-        integer(PRIZE_ON_FIELD, 50);
-        integer(PRIZE_WORKING, 30);
-        integer(AI_TICKS_PER_SHOOT, 10);
-        integer(TANK_TICKS_PER_SHOOT, 4);
-        integer(SLIPPERINESS, 3);
-        integer(AI_PRIZE_LIMIT, 3);
-        integer(PENALTY_WALKING_ON_WATER, 2);
-
         bool(MULTIPLE, false);
         integer(PLAYERS_PER_ROOM, 20);
         // включен ли режим раундов
@@ -112,12 +104,28 @@ public class GameSettings extends SettingsImpl implements SettingsReader<GameSet
         // сколько тиков должно пройти от начала раунда, чтобы засчитать победу
         integer(MIN_TICKS_FOR_WIN, 1);
 
+        integer(AI_TICKS_PER_SHOOT, 10);
+        integer(TANK_TICKS_PER_SHOOT, 4);
+        integer(SLIPPERINESS, 3);
+        integer(PENALTY_WALKING_ON_WATER, 2);
+        bool(SHOW_MY_TANK_UNDER_TREE, false);
+
+        integer(SPAWN_AI_PRIZE, 4);
+        integer(KILL_HITS_AI_PRIZE, 3);
+        integer(PRIZE_ON_FIELD, 50);
+        integer(PRIZE_WORKING, 30);
+        integer(AI_PRIZE_LIMIT, 3);
+
         integer(CHANCE_RESERVED, 30);
         integer(CHANCE_IMMORTALITY, 20);
         integer(CHANCE_BREAKING_WALLS, 20);
         integer(CHANCE_WALKING_ON_WATER, 20);
         integer(CHANCE_VISIBILITY, 20);
         integer(CHANCE_NO_SLIDING, 20);
+
+        integer(KILL_YOUR_TANK_PENALTY, 0);
+        integer(KILL_OTHER_HERO_TANK_SCORE, 50);
+        integer(KILL_OTHER_AI_TANK_SCORE, 25);
 
         multiline(LEVEL_MAP,
                 "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n" +
