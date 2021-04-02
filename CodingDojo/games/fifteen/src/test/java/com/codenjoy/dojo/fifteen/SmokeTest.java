2c320b50b9f35dd921381e337ab24d058e02e61d
Oleksandr_Baglai <apofig@gmail.com>
2021-04-02 16:19:04 +0300

    [Fifteen][smoke] Improved test.

--- CodingDojo/games/fifteen/src/test/java/com/codenjoy/dojo/fifteen/SmokeTest.java
+++ CodingDojo/games/fifteen/src/test/java/com/codenjoy/dojo/fifteen/SmokeTest.java
@@ -23,257 +23,40 @@ package com.codenjoy.dojo.fifteen;
  */
 
 
+import com.codenjoy.dojo.client.local.LocalGameRunner;
 import com.codenjoy.dojo.fifteen.client.Board;
 import com.codenjoy.dojo.fifteen.client.ai.AISolver;
-import com.codenjoy.dojo.client.local.LocalGameRunner;
 import com.codenjoy.dojo.fifteen.services.GameRunner;
+import com.codenjoy.dojo.fifteen.services.GameSettings;
 import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.utils.Smoke;
 import org.junit.Test;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class SmokeTest {
+
     @Test
     public void test() {
-        // given
-        List<String> messages = new LinkedList<>();
+        Dice dice = LocalGameRunner.getDice("435874345435874365843564398", 100, 200);
 
-        LocalGameRunner.timeout = 0;
-        LocalGameRunner.out = (e) -> messages.add(e);
-        LocalGameRunner.countIterations = 15;
+        // about 2s
+        Smoke.play(1000, "SmokeTest.data", false,
+                new GameRunner() {
+                    @Override
+                    public Dice getDice() {
+                        return dice;
+                    }
 
-        Dice dice = LocalGameRunner.getDice(
-                1, 2, 3, 0, 3, 2, 2, 0,
-                0, 3, 2, 1, 3, 1, 3, 3,
-                0, 3, 2, 1, 3, 2, 3, 0,
-                1, 1, 2, 1, 3, 2, 1, 2,
-                0, 2, 1, 2, 1, 3, 3, 1,
-                1, 3, 1, 1, 3, 2, 3, 0,
-                1, 0, 2, 1, 3, 2, 1, 2,
-                0, 2, 1, 2, 1, 3, 3, 1,
-                3, 3, 2, 1, 2, 3, 2, 1,
-                2, 3, 2, 1, 2, 3, 1, 2,
-                3, 1, 2, 3, 1, 1, 2, 0);
-
-        GameRunner gameType = new GameRunner() {
-            @Override
-            public Dice getDice() {
-                return dice;
-            }
-        };
-
-        // when
-        LocalGameRunner.run(gameType,
-                new AISolver(dice),
-                new Board());
-
-        // thend
-        assertEquals("DICE:1\n" +
-                        "DICE:2\n" +
-                        "DICE:3\n" +
-                        "DICE:0\n" +
-                        "DICE:3\n" +
-                        "DICE:2\n" +
-                        "DICE:2\n" +
-                        "DICE:0\n" +
-                        "DICE:0\n" +
-                        "DICE:3\n" +
-                        "DICE:2\n" +
-                        "DICE:1\n" +
-                        "DICE:3\n" +
-                        "DICE:1\n" +
-                        "DICE:3\n" +
-                        "DICE_CORRECTED < 2 :1\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*+jli*\n" +
-                        "1:*mkon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "DICE:0\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: DOWN\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*mjli*\n" +
-                        "1:*+kon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*mjli*\n" +
-                        "1:*k+on*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: UP\n" +
-                        "1:Fire Event: moveCount: 4, number: 0\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*m+li*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: LEFT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*+mli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*m+li*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: LEFT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*+mli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:0\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: DOWN\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*kmli*\n" +
-                        "1:*+jon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: UP\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*hfeg*\n" +
-                        "1:*+mli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: UP\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*+feg*\n" +
-                        "1:*hmli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*f+eg*\n" +
-                        "1:*hmli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "DICE:3\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: LEFT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*+feg*\n" +
-                        "1:*hmli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*f+eg*\n" +
-                        "1:*hmli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*fe+g*\n" +
-                        "1:*hmli*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:0\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: DOWN\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:******\n" +
-                        "1:*felg*\n" +
-                        "1:*hm+i*\n" +
-                        "1:*kjon*\n" +
-                        "1:*adcb*\n" +
-                        "1:******\n" +
-                        "1:\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: RIGHT\n" +
-                        "------------------------------------------",
-                String.join("\n", messages));
-
+                    @Override
+                    public GameSettings getSettings() {
+                        return new GameSettings();
+                    }
+                },
+                Arrays.asList(new AISolver(dice)),
+                Arrays.asList(new Board()),
+                (o1, o2) -> assertEquals(o1, o2));
     }
 }
