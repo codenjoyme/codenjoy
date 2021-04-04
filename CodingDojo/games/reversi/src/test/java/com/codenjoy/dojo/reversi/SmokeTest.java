d104629d985a83ad873e57e5eece5eb0ac6dc024
Oleksandr_Baglai <apofig@gmail.com>
2021-04-04 18:39:56 +0300

    [reversi][smoke] Added smoke test.

--- CodingDojo/games/reversi/src/test/java/com/codenjoy/dojo/reversi/SmokeTest.java
+++ CodingDojo/games/reversi/src/test/java/com/codenjoy/dojo/reversi/SmokeTest.java
@@ -23,365 +23,42 @@ package com.codenjoy.dojo.reversi;
  */
 
 
-import com.codenjoy.dojo.client.ClientBoard;
 import com.codenjoy.dojo.client.local.LocalGameRunner;
-import com.codenjoy.dojo.client.Solver;
 import com.codenjoy.dojo.reversi.client.Board;
 import com.codenjoy.dojo.reversi.client.ai.AISolver;
 import com.codenjoy.dojo.reversi.services.GameRunner;
 import com.codenjoy.dojo.reversi.services.GameSettings;
 import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.utils.Smoke;
 import org.junit.Test;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
-import static com.codenjoy.dojo.reversi.services.GameSettings.Keys.LEVEL_MAP;
 import static org.junit.Assert.assertEquals;
 
 public class SmokeTest {
 
     @Test
     public void test() {
-        // given
-        List<String> messages = new LinkedList<>();
+        Dice dice = LocalGameRunner.getDice("435874345435874365843564398", 100, 200);
 
-        LocalGameRunner.timeout = 0;
-        LocalGameRunner.out = (e) -> messages.add(e);
-        LocalGameRunner.countIterations = 15;
+        // about 2.3 sec
+        int ticks = 1000;
 
-        Dice dice = LocalGameRunner.getDice(
-                0, 2, 3, 1, 3, 3,
-                0, 2, 2, 3, 1,
-                0, 1, 3, 1, 2,
-                1, 3, 2, 1, 1,
-                1, 3, 0, 0, 1,
-                0, 0, 1, 0, 2,
-                1, 3, 2, 2, 1,
-                0, 1, 1, 3, 3,
-                0, 2, 0, 0, 2,
-                0, 1, 2, 2, 1,
-                1, 3, 1, 3, 3,
-                1, 3, 2, 2, 1,
-                0, 1, 3, 1, 1);
+        Smoke.play(ticks, "SmokeTest.data",
+                new GameRunner() {
+                    @Override
+                    public Dice getDice() {
+                        return dice;
+                    }
 
-        GameRunner gameType = new GameRunner() {
-            @Override
-            public Dice getDice() {
-                return dice;
-            }
-
-            @Override
-            public GameSettings getSettings() {
-                return super.getSettings()
-                        .string(LEVEL_MAP,
-                                "    " +
-                                " xO " +
-                                " Ox " +
-                                "    ");
-            }
-        };
-
-        // when
-        LocalGameRunner.run(gameType,
-                new LinkedList<Solver>(){{
-                    add(new AISolver(dice));
-                    add(new AISolver(dice));
-                }},
-                new LinkedList<>(){{
-                    add(new Board());
-                    add(new Board());
-                }});
-
-        // then
-        assertEquals("1:Board:\n" +
-                        "1:    \n" +
-                        "1: xO \n" +
-                        "1: Ox \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "DICE:0\n" +
-                        "1:Scores: 0\n" +
-                        "1:Answer: ACT(0,2)\n" +
-                        "2:Board:\n" +
-                        "2:    \n" +
-                        "2: +O \n" +
-                        "2: O+ \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "2:Scores: 0\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:    \n" +
-                        "1:... \n" +
-                        "1: .X \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "1:Scores: 1\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:    \n" +
-                        "2:ooo \n" +
-                        "2: oX \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "DICE:2\n" +
-                        "2:Scores: 0\n" +
-                        "2:Answer: ACT(2,3)\n" +
-                        "2:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  x \n" +
-                        "1:OOx \n" +
-                        "1: Ox \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "1:Scores: 1\n" +
-                        "1:Answer: ACT(3,3)\n" +
-                        "2:Board:\n" +
-                        "2:  + \n" +
-                        "2:OO+ \n" +
-                        "2: O+ \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "2:Scores: 1\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  X.\n" +
-                        "1:... \n" +
-                        "1: .X \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "1:Scores: 2\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:  Xo\n" +
-                        "2:ooo \n" +
-                        "2: oX \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "DICE:1\n" +
-                        "2:Scores: 1\n" +
-                        "2:Answer: ACT(0,1)\n" +
-                        "2:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  xO\n" +
-                        "1:OxO \n" +
-                        "1:xxx \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "DICE:3\n" +
-                        "1:Scores: 2\n" +
-                        "1:Answer: ACT(2,0)\n" +
-                        "2:Board:\n" +
-                        "2:  +O\n" +
-                        "2:O+O \n" +
-                        "2:+++ \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "2:Scores: 3\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  X.\n" +
-                        "1:.X. \n" +
-                        "1:X.. \n" +
-                        "1:  . \n" +
-                        "1:\n" +
-                        "1:Scores: 4\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:  Xo\n" +
-                        "2:oXo \n" +
-                        "2:Xoo \n" +
-                        "2:  o \n" +
-                        "2:\n" +
-                        "DICE:3\n" +
-                        "2:Scores: 3\n" +
-                        "2:Answer: ACT(3,1)\n" +
-                        "2:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  xO\n" +
-                        "1:OxO \n" +
-                        "1:xxxx\n" +
-                        "1:  O \n" +
-                        "1:\n" +
-                        "DICE:0\n" +
-                        "1:Scores: 4\n" +
-                        "1:Answer: ACT(0,0)\n" +
-                        "2:Board:\n" +
-                        "2:  +O\n" +
-                        "2:O+O \n" +
-                        "2:++++\n" +
-                        "2:  O \n" +
-                        "2:\n" +
-                        "2:Scores: 5\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  X.\n" +
-                        "1:.X. \n" +
-                        "1:..XX\n" +
-                        "1:. . \n" +
-                        "1:\n" +
-                        "1:Scores: 6\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:  Xo\n" +
-                        "2:oXo \n" +
-                        "2:ooXX\n" +
-                        "2:o o \n" +
-                        "2:\n" +
-                        "DICE:2\n" +
-                        "2:Scores: 5\n" +
-                        "2:Answer: ACT(3,2)\n" +
-                        "2:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  xO\n" +
-                        "1:Oxxx\n" +
-                        "1:OOxx\n" +
-                        "1:O O \n" +
-                        "1:\n" +
-                        "DICE:2\n" +
-                        "1:Scores: 6\n" +
-                        "1:Answer: ACT(3,0)\n" +
-                        "2:Board:\n" +
-                        "2:  +O\n" +
-                        "2:O+++\n" +
-                        "2:OO++\n" +
-                        "2:O O \n" +
-                        "2:\n" +
-                        "2:Scores: 6\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  X.\n" +
-                        "1:.XX.\n" +
-                        "1:..X.\n" +
-                        "1:. ..\n" +
-                        "1:\n" +
-                        "1:Scores: 8\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:  Xo\n" +
-                        "2:oXXo\n" +
-                        "2:ooXo\n" +
-                        "2:o oo\n" +
-                        "2:\n" +
-                        "DICE:3\n" +
-                        "DICE_CORRECTED < 1 :0\n" +
-                        "2:Scores: 6\n" +
-                        "2:Answer: ACT(1,0)\n" +
-                        "2:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:  xO\n" +
-                        "1:OxxO\n" +
-                        "1:OxxO\n" +
-                        "1:OxOO\n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "1:Scores: 8\n" +
-                        "1:Answer: ACT(0,3)\n" +
-                        "2:Board:\n" +
-                        "2:  +O\n" +
-                        "2:O++O\n" +
-                        "2:O++O\n" +
-                        "2:O+OO\n" +
-                        "2:\n" +
-                        "2:Scores: 7\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:2\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:. X.\n" +
-                        "1:..X.\n" +
-                        "1:.X..\n" +
-                        "1:.X..\n" +
-                        "1:\n" +
-                        "1:Scores: 10\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:o Xo\n" +
-                        "2:ooXo\n" +
-                        "2:oXoo\n" +
-                        "2:oXoo\n" +
-                        "2:\n" +
-                        "DICE:0\n" +
-                        "2:Scores: 7\n" +
-                        "2:Answer: ACT(1,3)\n" +
-                        "2:Fire Event: FLIP:1\n" +
-                        "1:Fire Event: WIN:1\n" +
-                        "2:Fire Event: LOOSE:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:OxxO\n" +
-                        "1:OxxO\n" +
-                        "1:OxOO\n" +
-                        "1:OxOO\n" +
-                        "1:\n" +
-                        "1:Scores: 110\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2:O++O\n" +
-                        "2:O++O\n" +
-                        "2:O+OO\n" +
-                        "2:O+OO\n" +
-                        "2:\n" +
-                        "2:Scores: 8\n" +
-                        "2:Answer:\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1:    \n" +
-                        "1: xO \n" +
-                        "1: Ox \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "DICE:1\n" +
-                        "1:Scores: 110\n" +
-                        "1:Answer: ACT(1,3)\n" +
-                        "2:Board:\n" +
-                        "2:    \n" +
-                        "2: +O \n" +
-                        "2: O+ \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "2:Scores: 8\n" +
-                        "2:Answer:\n" +
-                        "1:Fire Event: FLIP:1\n" +
-                        "------------------------------------------\n" +
-                        "1:Board:\n" +
-                        "1: .  \n" +
-                        "1: .. \n" +
-                        "1: .X \n" +
-                        "1:    \n" +
-                        "1:\n" +
-                        "1:Scores: 111\n" +
-                        "1:Answer:\n" +
-                        "2:Board:\n" +
-                        "2: o  \n" +
-                        "2: oo \n" +
-                        "2: oX \n" +
-                        "2:    \n" +
-                        "2:\n" +
-                        "DICE:3\n" +
-                        "DICE_CORRECTED < 3 :0\n" +
-                        "2:Scores: 8\n" +
-                        "2:Answer: ACT(0,1)\n" +
-                        "2:Fire Event: FLIP:1\n" +
-                        "------------------------------------------",
-                String.join("\n", messages));
-
+                    @Override
+                    public GameSettings getSettings() {
+                        return new GameSettings();
+                    }
+                },
+                Arrays.asList(new AISolver(dice), new AISolver(dice)),
+                Arrays.asList(new Board(), new Board()),
+                (o1, o2) -> assertEquals(o1, o2));
     }
 }
