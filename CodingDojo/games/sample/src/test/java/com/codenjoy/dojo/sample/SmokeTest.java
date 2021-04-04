a2d69386cff6f5a80e2c8b1e877260729c2035aa
Oleksandr_Baglai <apofig@gmail.com>
2021-04-04 18:40:32 +0300

    [sample][smoke] Updated performance results in test.

--- CodingDojo/games/sample/src/test/java/com/codenjoy/dojo/sample/SmokeTest.java
+++ CodingDojo/games/sample/src/test/java/com/codenjoy/dojo/sample/SmokeTest.java
@@ -46,11 +46,13 @@ public class SmokeTest {
     public void test() {
         Dice dice = LocalGameRunner.getDice("435874345435874365843564398", 100, 200);
 
-        // about 3s
+        // about 2.2 sec
         int players = 2;
+        int ticks = 1000;
         Supplier<Solver> solver = () -> new AISolver(dice);
+
         LocalGameRunner.showPlayers = "1";
-        Smoke.play(1000, "SmokeTest.data",
+        Smoke.play(ticks, "SmokeTest.data",
                 new GameRunner() {
                     @Override
                     public Dice getDice() {
