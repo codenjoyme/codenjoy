721efa73e9d28e7af5167a330f22d03a84cf09aa
Olena_Isaienko <olena_isaienko@epam.com>
2021-04-12 12:52:50 +0300

    Change solver

--- CodingDojo/games/battlecity/src/main/python/solver.py
+++ CodingDojo/games/battlecity/src/main/python/solver.py
@@ -5,10 +5,9 @@ from board import Board
 from command import Command
 from random import choice
 
-_COURSES = ('LEFT','RIGHT','DOWN','UP')
+_COURSES = ('LEFT', 'RIGHT', 'DOWN', 'UP')
 
 """ This class should contain the movement generation algorithm."""
-
 
 
 class DirectionSolver:
@@ -24,59 +23,35 @@ class DirectionSolver:
         """ The function that should be implemented."""
         self._board = Board(board_string)
         _command = str(self.find_command())
+        print(self._board.to_string())
         print("Sending Command {}".format(_command))
-        # return ''.join(_command)
-        return ''.join(_command)
+
+        return _command
 
     def find_command(self):
 
         course = list(_COURSES)
         _direction = choice(course)
-        print(self._board.to_string())
-        all_tanks = self._board.tanks
         if not self._board.my_tank:
-            __dir = choice((course))
+            __dir = choice(course)
         else:
             tank = self._board.my_tank[0]
             tank_x, tank_y = tank.x, tank.y
             my_direction = self._board.direction(tank_x, tank_y)
-            _direction = self.next_step(tank_x, tank_y,my_direction)
-
-
-        print("Sending Command: {}\n".format( _direction))
+            _direction = self.next_step(tank_x, tank_y, my_direction)
 
         return _direction
 
-    def next_step(self,x,y,_direction):
-        course = list(_COURSES)
-        ''''find new coordinates for the direction'''
-
+    def next_step(self, x, y, _direction):
+        """   find new coordinates for the direction
+        """
         _direction = Command(_direction)
         _x, _y = _direction.new_x(x), _direction.new_y(y)
 
-        """analyzing a new course and choice of old or new course"""
-
+        """analyzing a new course and choice of old or new course
+        """
         return _direction
-
-
-
-
-
 
 
 if __name__ == '__main__':
     raise RuntimeError("This module is not intended to be ran from CLI")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
