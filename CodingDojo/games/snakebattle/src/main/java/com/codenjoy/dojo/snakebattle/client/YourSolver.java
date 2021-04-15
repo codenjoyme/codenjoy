51d6677a1ad1ec239147c3089c9b43858e8ffd7a
Oleksandr_Baglai <apofig@gmail.com>
2021-04-15 12:23:26 +0300

    [snakebattle][solver] Rollback

--- CodingDojo/games/snakebattle/src/main/java/com/codenjoy/dojo/snakebattle/client/YourSolver.java
+++ CodingDojo/games/snakebattle/src/main/java/com/codenjoy/dojo/snakebattle/client/YourSolver.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.snakebattle.client;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -29,8 +29,6 @@ import com.codenjoy.dojo.client.WebSocketRunner;
 import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.RandomDice;
 
-import java.util.Calendar;
-
 /**
  * User: your name
  * Это твой алгоритм AI для игры. Реализуй его на свое усмотрение.
@@ -41,7 +39,6 @@ public class YourSolver implements Solver<Board> {
 
     private Dice dice;
     private Board board;
-    private long time;
 
     public YourSolver(Dice dice) {
         this.dice = dice;
@@ -52,27 +49,14 @@ public class YourSolver implements Solver<Board> {
         this.board = board;
         if (board.isGameOver()) return "";
 
-        long delta = now() - time;
-        if (Math.abs(delta - 1000) > 50) {
-            System.out.println(Calendar.getInstance().getTime().toString() + " > " + delta);
-        } else {
-            // System.out.print("+");
-        }
-        time = now();
-
         return Direction.RIGHT.toString();
     }
 
-    public long now() {
-        return Calendar.getInstance().getTimeInMillis();
-    }
-
     public static void main(String[] args) {
-        WebSocketRunner.PRINT_TO_CONSOLE = false;
         WebSocketRunner.runClient(args,
                 // paste here board page url from browser after registration
                 // or put it as command line parameter
-                "https://dojorena.io/codenjoy-contest/board/player/dojorena5?code=953820862434373766",
+                "http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789",
                 new YourSolver(new RandomDice()),
                 new Board());
     }
