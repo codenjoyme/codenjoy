c7326b452753352d33541d7d5ffd54e6d8930e59
Oleksandr_Baglai <apofig@gmail.com>
2021-04-18 18:32:36 +0300

    [bomberman][events-assert] Added javadocs.

--- CodingDojo/games/bomberman/src/test/java/com/codenjoy/dojo/bomberman/model/MockitoMocker.java
+++ CodingDojo/games/bomberman/src/test/java/com/codenjoy/dojo/bomberman/model/MockitoMocker.java
@@ -6,36 +6,38 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
+/**
+ * Некоторые тестовые утилитные классы находятся в engine и используются в играх как библиотеки.
+ * Но вот проблема, в них надо использовать и Assert и Mockito, а эти библиотеки импортируются Maven
+ * только для тестов. Потому через этого малого мы инкапсулируем доступ к Maven и JUnit.Assert через reflections.
+ * Вот так вот! И такой код тоже бывает. Но не забываем, что любой подход - это упражнение по программированию.
+ * Если найдешь лучшее решение, кроме как импортировать Maven и JUnit не для тестов - ждем PullRequest.
+ */
 public class MockitoMocker implements EventsListenersAssert.Mocker {
 
-    private Class<?> assertClass;
-    private Class<?> mockitoClass;
-    private Class<?> mockitoVerificationModeClass;
-    private Class<?> argumentCaptorClass;
+    private Class<?> Assert;
+    private Class<?> Mockito;
+    private Class<?> VerificationMode;
+    private Class<?> ArgumentCaptor;
 
     public MockitoMocker() {
         try {
             ClassLoader classLoader = this.getClass().getClassLoader();
-            assertClass = classLoader.loadClass("org.junit.Assert");
-            mockitoClass = classLoader.loadClass("org.mockito.Mockito");
-            mockitoVerificationModeClass = classLoader.loadClass("org.mockito.verification.VerificationMode");
-            argumentCaptorClass = classLoader.loadClass("org.mockito.ArgumentCaptor");
+            Assert = classLoader.loadClass("org.junit.Assert");
+            Mockito = classLoader.loadClass("org.mockito.Mockito");
+            VerificationMode = classLoader.loadClass("org.mockito.verification.VerificationMode");
+            ArgumentCaptor = classLoader.loadClass("org.mockito.ArgumentCaptor");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public void assertEquals(Object o1, Object o2) {
-        callStatic(assertClass, "assertEquals",
-                new Class[]{Object.class, Object.class},
-                new Object[]{o1, o2});
-    }
-
+    // так вызываются статические методы класса
     public Object callStatic(Class<?> clazz, String name, Class[] argsTypes, Object[] args) {
         return call(clazz, name, argsTypes, clazz, args);
     }
 
+    // а так вызываются методы на объекте
     public Object call(Class<?> clazz, String name, Class[] argsTypes, Object thizz, Object[] args) {
         try {
             Method method = clazz.getDeclaredMethod(name, argsTypes);
@@ -46,6 +48,8 @@ public class MockitoMocker implements EventsListenersAssert.Mocker {
         return null;
     }
 
+    // тут мы разворачиваем исключения так, чтобы тесты валились в IDE
+    // привычно с подсветкой Diff actual vs expected
     public void process(Exception e) {
         if (e instanceof InvocationTargetException) {
             Throwable target = ((InvocationTargetException) e).getTargetException();
@@ -60,45 +64,58 @@ public class MockitoMocker implements EventsListenersAssert.Mocker {
     }
 
     @Override
+    public void assertEquals(Object o1, Object o2) {
+        callStatic(Assert, "assertEquals",
+                new Class[]{Object.class, Object.class},
+                new Object[]{o1, o2});
+    }
+
+    @Override
     public <T> T verify(T mock, Object mode) {
-        return (T) callStatic(mockitoClass, "verify",
-                new Class[]{Object.class, mockitoVerificationModeClass},
+        return (T) callStatic(Mockito, "verify",
+                new Class[]{Object.class, VerificationMode},
                 new Object[]{mock, mode});
     }
 
     @Override
     public <T> void reset(T... mocks) {
-        callStatic(mockitoClass, "reset",
+        callStatic(Mockito, "reset",
                 new Class[]{Object[].class},
                 new Object[]{(Object) mocks});
     }
 
     @Override
     public void verifyNoMoreInteractions(Object... mocks) {
-        callStatic(mockitoClass, "verifyNoMoreInteractions",
+        callStatic(Mockito, "verifyNoMoreInteractions",
                 new Class[]{Object[].class},
                 new Object[]{(Object) mocks});
     }
 
     @Override
     public <T> T any(Class<T> type) {
-        return (T) callStatic(mockitoClass, "any",
+        return (T) callStatic(Mockito, "any",
                 new Class[]{Class.class},
                 new Object[]{type});
     }
 
     @Override
     public Object never() {
-        return callStatic(mockitoClass, "never",
+        return callStatic(Mockito, "never",
                 new Class[]{},
                 new Object[]{});
     }
 
     @Override
     public Object atLeast(int minNumberOfInvocations) {
-        return callStatic(mockitoClass, "atLeast",
+        return callStatic(Mockito, "atLeast",
                 new Class[]{int.class},
                 new Object[]{minNumberOfInvocations});
+    }
+
+    public Object getArgumentCaptor(Class clazz) {
+        return callStatic(ArgumentCaptor, "forClass",
+                new Class[]{Class.class},
+                new Object[]{clazz});
     }
 
     @Override
@@ -107,23 +124,17 @@ public class MockitoMocker implements EventsListenersAssert.Mocker {
         return new EventsListenersAssert.Captor<T>() {
             @Override
             public T capture() {
-                return (T) call(argumentCaptorClass, "capture",
+                return (T) call(ArgumentCaptor, "capture",
                         new Class[]{},
                         captor, new Object[]{});
             }
 
             @Override
             public List<T> getAllValues() {
-                return (List<T>) call(argumentCaptorClass, "getAllValues",
+                return (List<T>) call(ArgumentCaptor, "getAllValues",
                         new Class[]{},
                         captor, new Object[]{});
             }
         };
-    }
-
-    public Object getArgumentCaptor(Class clazz) {
-        return callStatic(argumentCaptorClass, "forClass",
-                new Class[]{Class.class},
-                new Object[]{clazz});
     }
 }
