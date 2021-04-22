de24e7367b199bb050c03d19ce34dd7473afb06f
Denis Kirilov <38264680+DenisKirilov@users.noreply.github.com>
2021-04-22 12:29:23 +0300

    Retry functionality added when repo create fails, end-point to change user's username and repository added, unit tests, refactor

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/SaveService.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/SaveService.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.services;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -34,6 +34,7 @@ public interface SaveService {
     void load(String id, String game, String room, String save, String repositoryUrl);
     void loadAll(String room);
     long save(String id);
+    long save(Player player);
     List<PlayerInfo> getSaves();
     long saveAll();
     long saveAll(String room);
