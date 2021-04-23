4188d692a6220e4b4a6de916e251327e33bddee5
Oleksandr_Baglai <apofig@gmail.com>
2021-04-23 09:02:04 +0300

    [loderunner] Added new levels.

---
+++ CodingDojo/games/loderunner/src/main/java/com/codenjoy/dojo/loderunner/services/Level1Small.java
@@ -0,0 +1,158 @@
+package com.codenjoy.dojo.loderunner.services;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+public class Level1Small {
+
+    public static String get1() {
+        return  "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n" +
+                "☼                            ☼\n" +
+                "☼##H########################H☼\n" +
+                "☼  H                        H☼\n" +
+                "☼H☼☼#☼☼H    H#########H     H☼\n" +
+                "☼H     H    H         H#####H☼\n" +
+                "☼H#☼#☼#H    H         H  ~~~ ☼\n" +
+                "☼H  ~  H~~~~H~~~~~~   H      ☼\n" +
+                "☼H     H    H     H###☼☼☼☼☼☼H☼\n" +
+                "☼H     H    H#####H         H☼\n" +
+                "☼☼###☼##☼##☼H         H###H##☼\n" +
+                "☼☼###☼~~~~  H         H   H##☼\n" +
+                "☼☼   ☼      H   ~~~~~~H   H  ☼\n" +
+                "☼########H###☼☼☼☼     H  ####☼\n" +
+                "☼        H            H      ☼\n" +
+                "☼H##########################H☼\n" +
+                "☼H                 ~~~      H☼\n" +
+                "☼#######H#######            H☼\n" +
+                "☼       H~~~~~~~~~~         H☼\n" +
+                "☼       H    ##H   #######H##☼\n" +
+                "☼       H    ##H          H  ☼\n" +
+                "☼##H#####    ########H#######☼\n" +
+                "☼  H                 H       ☼\n" +
+                "☼#########H##########H       ☼\n" +
+                "☼         H          H       ☼\n" +
+                "☼☼☼       H~~~~~~~~~~H       ☼\n" +
+                "☼    H######         #######H☼\n" +
+                "☼H☼  H  ◄                   H☼\n" +
+                "☼##########☼☼☼######☼☼######H☼\n" +
+                "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n";
+    }
+
+    public static String get2() {
+        return  "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n" +
+                "☼H            ~~H~~~~☼☼☼☼☼☼☼H☼\n" +
+                "☼H~~~~  ######  H         H☼H☼\n" +
+                "☼H              ##H#######H☼H☼\n" +
+                "☼H#########       H    ~~~H☼H☼\n" +
+                "☼H        ###H####H##H     ☼H☼\n" +
+                "☼H           H      #######☼H☼\n" +
+                "☼~~~~~~~~~~~~H       H~~~~~☼H☼\n" +
+                "☼     H              H     ☼H☼\n" +
+                "☼ ### #############H H#####☼H☼\n" +
+                "☼H                 H       ☼H☼\n" +
+                "☼H#####         H##H####    H☼\n" +
+                "☼H      H######### H   ######☼\n" +
+                "☼H##    H          H~~~~~~   ☼\n" +
+                "☼~~~~#####H#   ~~~~H         ☼\n" +
+                "☼         H        H      ~~~☼\n" +
+                "☼   ########H    ######H##   ☼\n" +
+                "☼           H          H     ☼\n" +
+                "☼H    ###########H     H#####☼\n" +
+                "☼H###            H     H     ☼\n" +
+                "☼H  ######  ##H######  H     ☼\n" +
+                "☼H            H ~~~~~##H###H ☼\n" +
+                "☼    H########H#       H   ##☼\n" +
+                "☼ ###H        H         ~~~~~☼\n" +
+                "☼    H########H#########     ☼\n" +
+                "☼H   H                       ☼\n" +
+                "☼H  ####H######         #####☼\n" +
+                "☼H      H      H#######H     ☼\n" +
+                "☼##############H       H#####☼\n" +
+                "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n";
+    }
+
+    public static String get3() {
+        return  "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n" +
+                "☼~~~~~~~~~~           ~~~~~~~☼\n" +
+                "☼HH       H##########H       ☼\n" +
+                "☼######H  H          H#☼☼☼☼☼#☼\n" +
+                "☼      H#####H#####H##  ~~~~~☼\n" +
+                "☼      H ~   H     H  ~~     ☼\n" +
+                "☼#####H#     H     H    ~~   ☼\n" +
+                "☼     H   H######H##      ~~ ☼\n" +
+                "☼     H~~~H      H          #☼\n" +
+                "☼     H      H#########H     ☼\n" +
+                "☼    H##     H#       ##     ☼\n" +
+                "☼H###H######### H###H #####H#☼\n" +
+                "☼H   H          H# #H      H ☼\n" +
+                "☼H#######   ###### ##########☼\n" +
+                "☼H                           ☼\n" +
+                "☼H#######~~~####H############☼\n" +
+                "☼H              H            ☼\n" +
+                "☼##H~~~~      ############H  ☼\n" +
+                "☼  H                      H  ☼\n" +
+                "☼########~~~~~~~H######## H  ☼\n" +
+                "☼               H         H  ☼\n" +
+                "☼~~~~  ~~~#########~~~~~  H  ☼\n" +
+                "☼H                    ~~~~H  ☼\n" +
+                "☼##☼☼☼☼☼☼#   ☼☼☼☼☼☼☼      H  ☼\n" +
+                "☼~~      ~~~              H  ☼\n" +
+                "☼  H#####   ###########   H  ☼\n" +
+                "☼  H        ~~~~~~~~~~~~~~H  ☼\n" +
+                "☼  H####H                 H  ☼\n" +
+                "☼☼☼☼☼☼☼☼☼######☼☼☼☼☼☼☼#######☼\n" +
+                "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n";
+    }
+
+    public static String get4() {
+        return  "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n" +
+                "☼☼☼☼☼☼☼☼       H   ~~~~~~~~~H☼\n" +
+                "☼H         ####H  ☼         H☼\n" +
+                "☼HH######H     ###☼☼☼☼☼☼☼☼ ~H☼\n" +
+                "☼HH~~~   H~~~~~ ##        ~ H☼\n" +
+                "☼H       H     ###☼☼☼☼☼☼ ~  H☼\n" +
+                "☼H#####  H#####   ~~~~~~~ ~ H☼\n" +
+                "☼H~~~~~  H             ~ ~  H☼\n" +
+                "☼H     ##########H          H☼\n" +
+                "☼H              H ########  H☼\n" +
+                "☼H#######        H          H☼\n" +
+                "☼           ###H#########   H☼\n" +
+                "☼######        H            H☼\n" +
+                "☼              H #######H## H☼\n" +
+                "☼########H     H        H   H☼\n" +
+                "☼~~~~~   H     H        H   H☼\n" +
+                "☼     ##############    H   H☼\n" +
+                "☼   ~~~~~           ##H#####H☼\n" +
+                "☼H         H##H       H     H☼\n" +
+                "☼###########  ##H###  H     H☼\n" +
+                "☼               H   ##H###  H☼\n" +
+                "☼    #########H##           H☼\n" +
+                "☼####         H             H☼\n" +
+                "☼H      ##H###H####H###     H☼\n" +
+                "☼H        H        H        H☼\n" +
+                "☼H        H        H        H☼\n" +
+                "☼H########H##      H#####   H☼\n" +
+                "☼                  H        H☼\n" +
+                "☼############################☼\n" +
+                "☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼\n";
+    }
+}
