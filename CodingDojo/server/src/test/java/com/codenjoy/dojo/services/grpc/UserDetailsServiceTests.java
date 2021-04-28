f838d18b6d919599ddefb9d8a63b381e642fb5f0
Elitsa Marinova <Elitsa_Marinova@epam.com>
2021-04-28 15:02:24 +0300

    change request by id name

--- CodingDojo/server/src/test/java/com/codenjoy/dojo/services/grpc/UserDetailsServiceTests.java
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/services/grpc/UserDetailsServiceTests.java
@@ -1,6 +1,28 @@
 package com.codenjoy.dojo.services.grpc;
 
-import com.codenjoy.dojo.UserDetailsRequest;
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 - 2021 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.codenjoy.dojo.UserDetailsIdRequest;
 import com.codenjoy.dojo.UserDetailsResponse;
 import com.codenjoy.dojo.services.dao.Registration;
 import io.grpc.stub.StreamObserver;
@@ -20,7 +42,7 @@ public class UserDetailsServiceTests {
     private static final String USER_ID = "id";
     private static final String EMAIL = "email";
 
-    private UserDetailsRequest request;
+    private UserDetailsIdRequest request;
     private UserDetailsResponse response;
 
     @Mock
@@ -33,7 +55,7 @@ public class UserDetailsServiceTests {
 
     @Before
     public void init() {
-        this.request = UserDetailsRequest.newBuilder().setId(USER_ID).build();
+        this.request = UserDetailsIdRequest.newBuilder().setId(USER_ID).build();
         this.response = UserDetailsResponse.newBuilder().setId(USER_ID).setEmail(EMAIL).build();
         this.userDetailsService = new UserDetailsService(registration);
     }
@@ -42,7 +64,7 @@ public class UserDetailsServiceTests {
     public void getUserDetailsTest() {
         when(registration.getEmailById(USER_ID)).thenReturn(EMAIL);
 
-        userDetailsService.getUserDetails(request, streamObserver);
+        userDetailsService.getUserDetailsById(request, streamObserver);
 
         verify(streamObserver, times(1)).onNext(response);
         verify(streamObserver, times(1)).onCompleted();
