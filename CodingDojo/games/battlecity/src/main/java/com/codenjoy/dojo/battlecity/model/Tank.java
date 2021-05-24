16d8c4211eca55a0b709f451262738e8fccd4c80
Roman Kulyniak <roman.kulyniak@gmail.com>
2021-05-24 14:20:06 +0300

    [battlecity][test] condition 'tree != null' is always 'true'

--- CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/model/Tank.java
+++ CodingDojo/games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/model/Tank.java
@@ -228,9 +228,7 @@ public class Tank extends RoundPlayerHero<Field> implements State<Elements, Play
         }
 
         if (tank == this && settings().bool(SHOW_MY_TANK_UNDER_TREE)) {
-            if (tree != null) {
-                return null;
-            }
+            return null;
         }
 
         return Elements.TREE;
