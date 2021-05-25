e477a293adc30cd4d913c2b8ff905ed223477773
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-05-25 20:52:56 +0300

    [script][git] Update git push with submodules.

--- CodingDojo/git-push-submodules.bat
+++ CodingDojo/git-push-submodules.bat
@@ -1,41 +1,4 @@
-cd .\games\chess
-git push https://github.com/codenjoyme/codenjoy-chess.git master
-cd .\..\..
-
-cd .\games\excitebike
-git push https://github.com/codenjoyme/codenjoy-excitebike.git master 
-cd .\..\..
-
-cd .\games\japanese
-git push https://github.com/codenjoyme/codenjoy-japanese.git master 
-cd .\..\..
-
-cd .\games\selfdefense
-git push https://github.com/codenjoyme/codenjoy-selfdefense.git master 
-cd .\..\..
-
-cd .\games\vacuum
-git push https://github.com/codenjoyme/codenjoy-vacuum.git master
-cd .\..\..
-
-cd .\games\xonix
-git push https://github.com/codenjoyme/codenjoy-xonix.git master
-cd .\..\..
-
-cd .\client-runner
-git push https://github.com/codenjoyme/codenjoy-client-runner.git master
-cd .\..
-
-cd .\portable\linux-docker
-git push https://github.com/codenjoyme/codenjoy-portable-linux-lite.git master 
-cd .\..\..
-
-cd .\portable\linux-docker-compose
-git push https://github.com/codenjoyme/codenjoy-portable-linux.git master 
-cd .\..\..
-
-cd .\portable\windows-cmd
-git push https://github.com/codenjoyme/codenjoy-portable-windows.git master 
-cd .\..\..
-
-git push https://github.com/codenjoyme/codenjoy.git master 
+git checkout master
+git submodule update
+git push origin master
+git submodule foreach git push origin master
