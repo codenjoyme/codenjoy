12815d09bed1377d67ec6f414cbf3dd1282d00ef
Roman Kulyniak <roman_kulyniak@epam.com>
2021-06-02 12:27:18 +0300

    [bomberman][go] add main.go

---
+++ CodingDojo/games/bomberman/src/main/Go/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dnahurnyi/bomberman"
+)
+
+func main() {
+	// paste here board page url from browser after registration
+	// or put it as command line parameter
+	browserURL := "http://codenjoy.com:80/codenjoy-contest/board/player/3edq63tw0bq4w4iem7nb?code=1234567890123456789"
+	game, c := bomberman.StartGame(browserURL)
+
+	for {
+		select {
+		case <-c.Done:
+			log.Fatal("It's done")
+		case <-c.Read:
+			// Make your move
+
+			game.Move(bomberman.UP)
+			c.Write <- struct{}{}
+		}
+	}
+}
