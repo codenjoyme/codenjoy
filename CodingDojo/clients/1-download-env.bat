58c9b4359fdf301af120d160105e3abf86e55e3b
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-07 02:11:18 +0300

    [java-client] Check language before install jdk.

--- CodingDojo/clients/1-download-env.bat
+++ CodingDojo/clients/1-download-env.bat
@@ -1,18 +1,12 @@
 call 0-settings.bat
 
-echo off
-echo [44;93m
-echo        +-------------------------------------+
-echo        !           Installing JDK            !
-echo        +-------------------------------------+
-echo [0m
-echo on
+IF "%LANGUAGE%"=="java" (
+    call :jdk
+)
 
-rd /S /Q %JAVA_HOME%
-powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('https://aka.ms/download-jdk/microsoft-jdk-11.0.11.9.1-windows-x64.zip','%TOOLS%\jdk.zip') }"
-%TOOLS%\7z x -y -o%ROOT% %TOOLS%\jdk.zip
-rename jdk-11.0.11+9 .jdk
-cd %ROOT%
+IF "%LANGUAGE%"=="pseudo" (
+    call :jdk
+)
 
 echo off
 echo [44;93m
@@ -24,3 +18,21 @@ echo on
 
 echo Press any key to exit
 pause >nul
+
+goto :eof
+
+:jdk
+    echo off
+    echo [44;93m
+    echo        +-------------------------------------+
+    echo        !           Installing JDK            !
+    echo        +-------------------------------------+
+    echo [0m
+    echo on
+
+    rd /S /Q %JAVA_HOME%
+    powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('https://aka.ms/download-jdk/microsoft-jdk-11.0.11.9.1-windows-x64.zip','%TOOLS%\jdk.zip') }"
+    %TOOLS%\7z x -y -o%ROOT% %TOOLS%\jdk.zip
+    rename jdk-11.0.11+9 .jdk
+    cd %ROOT%
+goto :eof
