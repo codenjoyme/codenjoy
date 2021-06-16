1b2cdccef4af02d2b1268f99d68f8f4649eb89fb
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-16 13:39:05 +0300

    [git] Make git pull with submodules smarter.

--- CodingDojo/git-pull-submodules.bat
+++ CodingDojo/git-pull-submodules.bat
@@ -1,1 +1,17 @@
+git config --global credential.helper wincred
+
+for /f %%i in ('git describe --contains --all HEAD') do set BRANCH=%%i
+
+git pull origin %BRANCH%
+git pull origin develop
+
 git submodule update --remote --init
+
+git submodule foreach git checkout -b temp
+git submodule foreach git checkout master
+git submodule foreach git merge temp
+git submodule foreach git branch -d temp
+
+git submodule foreach git pull origin master
+
+git config --global --unset  credential.helper
