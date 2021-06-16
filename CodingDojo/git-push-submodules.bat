ace5312c14eabf959983cf91cdc4c8b69b348521
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-16 09:55:51 +0300

    [git] Updated script to push also current branch.

--- CodingDojo/git-push-submodules.bat
+++ CodingDojo/git-push-submodules.bat
@@ -1,6 +1,8 @@
 git config --global credential.helper wincred
 
-git push origin
+for /f %%i in ('git describe --contains --all HEAD') do set BRANCH=%%i
+
+git push origin %BRANCH%
 git submodule foreach git push origin master
 
 git config --global --unset  credential.helper
