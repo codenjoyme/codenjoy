0b81a5ce23b8464e714513d4e34a5e09bfab3dc9
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-16 10:19:07 +0300

    [git] Added script to sync develop with master (including origin) for all subrepos. Fixed script to push changes.

--- CodingDojo/git-push-submodules.bat
+++ CodingDojo/git-push-submodules.bat
@@ -4,5 +4,6 @@ for /f %%i in ('git describe --contains --all HEAD') do set BRANCH=%%i
 
 git push origin %BRANCH%
 git submodule foreach git push origin master
+git submodule foreach git push origin develop
 
 git config --global --unset  credential.helper
