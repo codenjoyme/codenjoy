4c8694825765a76c0056cc6e45abfb7d9747817d
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-25 16:26:56 +0300

    [git] Updated script.

--- CodingDojo/git-pull-submodules.bat
+++ CodingDojo/git-pull-submodules.bat
@@ -5,12 +5,16 @@ for /f %%i in ('git describe --contains --all HEAD') do set BRANCH=%%i
 git pull origin %BRANCH%
 git pull origin develop
 
+git submodule foreach git checkout -b temp
+
 git submodule update --remote --init
 
-git submodule foreach git checkout -b temp
+git submodule foreach git checkout -b temp2
 git submodule foreach git checkout master
 git submodule foreach git merge temp
+git submodule foreach git merge temp2
 git submodule foreach git branch -d temp
+git submodule foreach git branch -d temp2
 
 git submodule foreach git pull origin master
 
