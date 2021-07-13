9f4e9040a69ea5bf9514962aba1bc469224b7277
Roman Kulyniak <roman_kulyniak@epam.com>
2021-07-13 09:46:36 +0300

    [server] add additional checks

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/multiplayer/GameRoom.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/multiplayer/GameRoom.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.services.multiplayer;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -54,12 +54,14 @@ public class GameRoom {
         if (!isFree()) {
             return false;
         }
-        Integer maxTeams = player.settings.integer(ROUNDS_MAX_TEAMS_PER_ROOM);
-        if (!containsTeam(player.getTeamId()) && countTeams() >= maxTeams) {
-            return false;
-        }
-        if (countMembers(player.getTeamId()) >= count / maxTeams) {
-            return false;
+        if (player.settings != null && player.settings.hasParameter(ROUNDS_MAX_TEAMS_PER_ROOM.key())) {
+            int maxTeams = player.settings.integer(ROUNDS_MAX_TEAMS_PER_ROOM);
+            if (!containsTeam(player.getTeamId()) && countTeams() >= maxTeams) {
+                return false;
+            }
+            if (countMembers(player.getTeamId()) >= count / maxTeams) {
+                return false;
+            }
         }
         return true;
     }
