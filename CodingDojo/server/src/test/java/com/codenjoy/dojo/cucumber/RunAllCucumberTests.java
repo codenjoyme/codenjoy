1f0b6d5b9fce1e4e2f817cc23bcb5e7b767611f1
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-09-07 16:55:41 +0300

    [server][cucumber] Revert bad commit.

--- CodingDojo/server/src/test/java/com/codenjoy/dojo/cucumber/RunAllCucumberTests.java
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/cucumber/RunAllCucumberTests.java
@@ -26,8 +26,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html", "junit:target/junit-report.xml"},
-//        features = "src/test/resources/cucumber")
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html", "junit:target/junit-report.xml"},
+        features = "src/test/resources/cucumber")
 public class RunAllCucumberTests {
 }
