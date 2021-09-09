d3e71973a0a9268b2668e63cfd7bdee45d439fe2
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-09-10 00:10:09 +0300

    [server][ws][test] Replace " -> ' for remove escaping in "\"" case.

--- CodingDojo/server/src/test/java/com/codenjoy/dojo/services/WebSocketRunnerMock.java
+++ CodingDojo/server/src/test/java/com/codenjoy/dojo/services/WebSocketRunnerMock.java
@@ -93,7 +93,7 @@ public class WebSocketRunnerMock {
     }
 
     public String messages() {
-        return messages.toString();
+        return messages.toString().replace("\"", "'");
     }
 
     @SneakyThrows
