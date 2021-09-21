db75a7c891c648751b92e72967dbe8c27340a064
Roman Kulyniak <roman_kulyniak@epam.com>
2021-09-21 12:59:16 +0300

    [clifford] implement hero_pit_left & hero_pit_right

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/GuiPlotColorDecoder.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/GuiPlotColorDecoder.java
@@ -35,7 +35,7 @@ import java.util.stream.Collector;
 @AllArgsConstructor
 public class GuiPlotColorDecoder {
 
-    public static String GUI = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒ";
+    public static String GUI = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞß";
     private CharElement[] values;
 
     private char getGuiChar(char ch) {
