ae5fd9670fb03c9034031f05f18c3c1efc75d5cb
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-06 18:58:39 +0300

    [sample][test] Refactoring.

--- CodingDojo/games/sample/src/test/java/com/codenjoy/dojo/sample/model/AbstractGameCheckTest.java
+++ CodingDojo/games/sample/src/test/java/com/codenjoy/dojo/sample/model/AbstractGameCheckTest.java
@@ -23,6 +23,7 @@ package com.codenjoy.dojo.sample.model;
  */
 
 
+import com.codenjoy.dojo.sample.model.check.Pending;
 import com.codenjoy.dojo.sample.services.GameSettings;
 import com.codenjoy.dojo.services.Game;
 import com.codenjoy.dojo.utils.TestUtils;
@@ -87,45 +88,6 @@ public abstract class AbstractGameCheckTest extends AbstractGameTest {
         end();
 
         checkFile();
-    }
-
-    static class Pending {
-        private boolean enabled = false;
-        private String value = null;
-
-        public void value(String value) {
-            this.value = value;
-        }
-
-        public boolean enabled() {
-            return enabled;
-        }
-
-        public boolean hasValue() {
-            return value != null;
-        }
-
-        public String value() {
-            return value;
-        }
-
-        private Pending copy() {
-            Pending result = new Pending();
-            result.value = value;
-            result.enabled = enabled;
-            return result;
-        }
-
-        public Pending disable() {
-            Pending result = copy();
-            enabled = false;
-            value = null;
-            return result;
-        }
-
-        public void enable() {
-            enabled = true;
-        }
     }
 
     protected String messages() {
---
+++ CodingDojo/games/sample/src/test/java/com/codenjoy/dojo/sample/model/check/Pending.java
@@ -0,0 +1,41 @@
+package com.codenjoy.dojo.sample.model.check;
+
+public class Pending {
+
+    private boolean enabled = false;
+    private String value = null;
+
+    public void value(String value) {
+        this.value = value;
+    }
+
+    public boolean enabled() {
+        return enabled;
+    }
+
+    public boolean hasValue() {
+        return value != null;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    private Pending copy() {
+        Pending result = new Pending();
+        result.value = value;
+        result.enabled = enabled;
+        return result;
+    }
+
+    public Pending disable() {
+        Pending result = copy();
+        enabled = false;
+        value = null;
+        return result;
+    }
+
+    public void enable() {
+        enabled = true;
+    }
+}
