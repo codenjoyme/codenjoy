9062082d6382054fe16fdb15f0b30425926b14d0
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-08 14:33:07 +0300

    [server][admin][settings] Refactoring. Moved saveSettings method from controller to service.

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/AdminService.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/services/AdminService.java
@@ -26,16 +26,35 @@ import com.codenjoy.dojo.services.dao.ActionLogger;
 import com.codenjoy.dojo.services.dao.Registration;
 import com.codenjoy.dojo.services.incativity.InactivitySettings;
 import com.codenjoy.dojo.services.incativity.InactivitySettingsImpl;
+import com.codenjoy.dojo.services.level.LevelsSettings;
+import com.codenjoy.dojo.services.level.LevelsSettingsImpl;
 import com.codenjoy.dojo.services.log.DebugService;
 import com.codenjoy.dojo.services.room.RoomService;
+import com.codenjoy.dojo.services.round.RoundSettings;
+import com.codenjoy.dojo.services.round.RoundSettingsImpl;
+import com.codenjoy.dojo.services.security.GameAuthorities;
 import com.codenjoy.dojo.services.security.ViewDelegationService;
 import com.codenjoy.dojo.services.semifinal.SemifinalService;
+import com.codenjoy.dojo.services.semifinal.SemifinalSettingsImpl;
+import com.codenjoy.dojo.services.settings.CheckBox;
 import com.codenjoy.dojo.services.settings.Parameter;
+import com.codenjoy.dojo.services.settings.Settings;
+import com.codenjoy.dojo.web.controller.AdminSettings;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static com.codenjoy.dojo.services.incativity.InactivitySettings.INACTIVITY;
+import static com.codenjoy.dojo.services.level.LevelsSettings.LEVELS;
+import static com.codenjoy.dojo.services.round.RoundSettings.ROUNDS;
+import static com.codenjoy.dojo.services.semifinal.SemifinalSettings.SEMIFINAL;
 import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @Component
@@ -71,5 +90,224 @@ public class AdminService {
 
     public InactivitySettingsImpl inactivitySettings(String room) {
         return InactivitySettings.get(roomService.settings(room));
+    }
+
+    public void saveSettings(AdminSettings settings) {
+        String game = settings.getGame();
+        String room = settings.getRoom();
+
+        if (settings.getPlayers() != null) {
+            playerService.updateAll(settings.getPlayers());
+        }
+
+        if (settings.getSemifinal() != null) {
+            try {
+                semifinalSettings(room)
+                        .update(settings.getSemifinal());
+                semifinal.clean(room);
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+
+        if (settings.getLevels() != null) {
+            try {
+                levelsSettings(room)
+                        .updateFrom(settings.getLevels().getParameters());
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+
+        if (settings.getRounds() != null) {
+            try {
+                roundSettings(room)
+                        .update(settings.getRounds());
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+
+        if (settings.getInactivity() != null) {
+            try {
+                updateInactivity(room, settings.getInactivity());
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+
+        if (settings.getTimerPeriod() != null) {
+            try {
+                timerService.changePeriod(Integer.parseInt(settings.getTimerPeriod()));
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        if (settings.getProgress() != null) {
+            playerService.loadSaveForAll(room, settings.getProgress());
+        }
+
+        if (settings.getGames() != null) {
+            List<Parameter> games = (List) settings.getGames();
+            setEnable(games);
+        }
+
+        Settings gameSettings = gameService.getGameType(game, room).getSettings();
+        List<Exception> errors = new LinkedList<>();
+        if (settings.getOtherValues() != null) {
+            List<Object> updated = settings.getOtherValues();
+            updateParameters(gameSettings, onlyUngrouped(), updated, errors);
+        }
+        if (settings.getLevelsValues() != null) {
+            List<Object> updated = settings.getLevelsValues();
+            updateParameters(gameSettings, onlyLevels(), updated, errors);
+        }
+        if (!errors.isEmpty()) {
+            throw new IllegalArgumentException("There are errors during save settings: " + errors.toString());
+        }
+
+        if (settings.getGenerateNameMask() != null) {
+            String mask = settings.getGenerateNameMask();
+            int count = Integer.parseInt(settings.getGenerateCount());
+            String generateRoom = settings.getGenerateRoom();
+            generateNewPlayers(game, generateRoom, mask, count);
+        }
+    }
+
+    private void setEnable(List<Parameter> games) {
+        List<String> opened = new LinkedList<>();
+        List<String> allGames = gameService.getGames();
+        if (games.size() != allGames.size()) {
+            throw new IllegalStateException("Список игр к активации регистрации не полный");
+        }
+        for (int i = 0; i < allGames.size(); i++) {
+            if (games.get(i) != null) {
+                opened.add(allGames.get(i));
+            }
+        }
+
+        // TODO #4FS тут проверить
+        roomService.setOpenedGames(opened);
+    }
+
+    public void updateParameters(Settings gameSettings, Predicate<Parameter> filter,
+                                 List<Object> updated, List<Exception> errors)
+    {
+        List<Parameter> actual = gameSettings.getParameters().stream()
+                .filter(filter)
+                .collect(toList());
+        for (int index = 0; index < actual.size(); index++) {
+            try {
+                Parameter parameter = actual.get(index);
+                Object value = updated.get(index);
+                value = fixForCheckbox(parameter, value);
+                parameter.update(value);
+            } catch (Exception e) {
+                errors.add(e);
+            }
+        }
+    }
+
+    private Object fixForCheckbox(Parameter parameter, Object value) {
+        if (value == null && parameter.getType().equals(CheckBox.TYPE)) {
+            return false; // потому что так работает <form:checkbox
+        }
+        return value;
+    }
+
+    public Predicate<Parameter> onlyUngrouped() {
+        return Predicate.not(
+                p -> p.getName().startsWith(SEMIFINAL)
+                        || p.getName().startsWith(ROUNDS)
+                        || p.getName().startsWith(LEVELS)
+                        || p.getName().startsWith(INACTIVITY));
+    }
+
+    public Predicate<Parameter> onlyLevels() {
+        return p -> p.getName().startsWith(LEVELS);
+    }
+
+    public void generateNewPlayers(String game, String room, String mask, int count) {
+        int numLength = String.valueOf(count).length();
+
+        int created = 0;
+        int index = 0;
+        while (created != count) {
+            String number = StringUtils.leftPad(String.valueOf(++index), numLength, "0");
+            String id = mask.replaceAll("%", number);
+
+            if (playerService.contains(id) && index < playerService.getAll().size()) {
+                continue;
+            }
+
+            created++;
+            String code = register(id);
+            playerService.register(id, game, room, "127.0.0.1");
+        }
+    }
+
+    private String register(String id) {
+        if (registration.registered(id)) {
+            return registration.login(id, id);
+        } else {
+            return registration.register(id, id, id, id, "", GameAuthorities.USER.roles()).getCode();
+        }
+    }
+
+    public SemifinalSettingsImpl semifinalSettings(String room) {
+        return semifinal.semifinalSettings(room);
+    }
+
+    private RoundSettingsImpl roundSettings(String room) {
+        return RoundSettings.get(roomService.settings(room));
+    }
+
+    private LevelsSettingsImpl levelsSettings(String room) {
+        return LevelsSettings.get(roomService.settings(room));
+    }
+
+    public AdminSettings getAdminSettings(GameType gameType, String room) {
+        AdminSettings result = new AdminSettings();
+
+        // сохраняем для отображения semifinal settings pojo
+        result.setSemifinal(semifinalSettings(room)); // TODO тут снова берем getSettings().getParameters()
+        // сохраняем для отображения round settings pojo
+        result.setRounds(roundSettings(room)); // TODO тут снова берем getSettings().getParameters()
+        // сохраняем для отображения round settings pojo
+        LevelsSettingsImpl levels = levelsSettings(room);  // TODO тут снова берем getSettings().getParameters()
+        result.setLevels(levels); // TODO а точно тут нада эту строчку?
+        result.setLevelsValues(levels
+                .getParameters().stream()
+                .map(Parameter::getValue)
+                .collect(toList()));
+        // сохраняем для отображения inactivity settings pojo
+        result.setInactivity(inactivitySettings(room));
+        // отдельно оставшиеся параметры
+        List<Parameter> parameters = gameType.getSettings().getParameters();
+        parameters.removeIf(Predicate.not(onlyUngrouped()));
+        result.setOther(parameters);
+        result.setOtherValues(parameters.stream()
+                .map(Parameter::getValue)
+                .collect(toList()));
+
+        // TODO #4FS тут проверить
+        List<String> enabled = roomService.openedGames();
+        result.setGames(gameService.getGames().stream()
+                .map(name -> enabled.contains(name))
+                .collect(toList()));
+
+        List<PlayerInfo> saves = saveService.getSaves(room);
+        result.setPlayers(preparePlayers(room, saves));
+
+        return result;
+    }
+
+    private List<PlayerInfo> preparePlayers(String room, List<PlayerInfo> players) {
+        for (PlayerInfo player : players) {
+            player.setHidden(!room.equals(player.getRoom()));
+        }
+
+        return players;
     }
 }
--- CodingDojo/server/src/main/java/com/codenjoy/dojo/web/controller/AdminController.java
+++ CodingDojo/server/src/main/java/com/codenjoy/dojo/web/controller/AdminController.java
@@ -26,24 +26,14 @@ package com.codenjoy.dojo.web.controller;
 import com.codenjoy.dojo.services.*;
 import com.codenjoy.dojo.services.dao.ActionLogger;
 import com.codenjoy.dojo.services.dao.Registration;
-import com.codenjoy.dojo.services.level.LevelsSettings;
-import com.codenjoy.dojo.services.level.LevelsSettingsImpl;
 import com.codenjoy.dojo.services.log.DebugService;
 import com.codenjoy.dojo.services.nullobj.NullGameType;
 import com.codenjoy.dojo.services.room.RoomService;
-import com.codenjoy.dojo.services.round.RoundSettings;
-import com.codenjoy.dojo.services.round.RoundSettingsImpl;
-import com.codenjoy.dojo.services.security.GameAuthorities;
 import com.codenjoy.dojo.services.security.GameAuthoritiesConstants;
 import com.codenjoy.dojo.services.security.ViewDelegationService;
 import com.codenjoy.dojo.services.semifinal.SemifinalService;
-import com.codenjoy.dojo.services.semifinal.SemifinalSettingsImpl;
-import com.codenjoy.dojo.services.settings.CheckBox;
-import com.codenjoy.dojo.services.settings.Parameter;
-import com.codenjoy.dojo.services.settings.Settings;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -51,16 +41,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Predicate;
 
-import static com.codenjoy.dojo.services.incativity.InactivitySettings.INACTIVITY;
-import static com.codenjoy.dojo.services.level.LevelsSettings.LEVELS;
-import static com.codenjoy.dojo.services.round.RoundSettingsImpl.ROUNDS;
-import static com.codenjoy.dojo.services.semifinal.SemifinalSettingsImpl.SEMIFINAL;
 import static java.util.function.Predicate.not;
-import static java.util.stream.Collectors.toList;
 
 @Controller
 @RequestMapping(AdminController.URI)
@@ -75,7 +57,6 @@ public class AdminController {
     public static final String CUSTOM_ADMIN_PAGE_KEY = "custom";
 
     private final TimerService timerService;
-    private final TimeService timeService;
     private final PlayerService playerService;
     private final SaveService saveService;
     private final GameService gameService;
@@ -340,184 +321,12 @@ public class AdminController {
             // do nothing
         }
 
-        String game = settings.getGame();
+        adminService.saveSettings(settings);
+
         String room = settings.getRoom();
-
-        if (settings.getPlayers() != null) {
-            playerService.updateAll(settings.getPlayers());
-        }
-
-        if (settings.getSemifinal() != null) {
-            try {
-                semifinalSettings(room)
-                        .update(settings.getSemifinal());
-                semifinal.clean(room);
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-
-        if (settings.getLevels() != null) {
-            try {
-                levelsSettings(room)
-                        .updateFrom(settings.getLevels().getParameters());
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-
-        if (settings.getRounds() != null) {
-            try {
-                roundSettings(room)
-                        .update(settings.getRounds());
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-
-        if (settings.getInactivity() != null) {
-            try {
-                adminService.updateInactivity(room, settings.getInactivity());
-            } catch (Exception e) {
-                // do nothing
-            }
-        }
-
-        if (settings.getTimerPeriod() != null) {
-            try {
-                timerService.changePeriod(Integer.parseInt(settings.getTimerPeriod()));
-            } catch (NumberFormatException e) {
-                // do nothing
-            }
-        }
-
-        if (settings.getProgress() != null) {
-            playerService.loadSaveForAll(room, settings.getProgress());
-        }
-
-        if (settings.getGames() != null) {
-            List<Parameter> games = (List)settings.getGames();
-            setEnable(games);
-        }
-
-        Settings gameSettings = gameService.getGameType(game, room).getSettings();
-        List<Exception> errors = new LinkedList<>();
-        if (settings.getOtherValues() != null) {
-            List<Object> updated = settings.getOtherValues();
-            updateParameters(gameSettings, onlyUngrouped(), updated, errors);
-        }
-        if (settings.getLevelsValues() != null) {
-            List<Object> updated = settings.getLevelsValues();
-            updateParameters(gameSettings, onlyLevels(), updated, errors);
-        }
-        if (!errors.isEmpty()) {
-            throw new IllegalArgumentException("There are errors during save settings: " + errors.toString());
-        }
-
-        if (settings.getGenerateNameMask() != null) {
-            String mask = settings.getGenerateNameMask();
-            int count = Integer.parseInt(settings.getGenerateCount());
-            String generateRoom = settings.getGenerateRoom();
-            generateNewPlayers(game, generateRoom, mask, count);
-        }
-
         request.setAttribute("room", room);
         return getAdmin(room);
     }
-
-    public SemifinalSettingsImpl semifinalSettings(String room) {
-        return semifinal.semifinalSettings(room);
-    }
-
-    private RoundSettingsImpl roundSettings(String room) {
-        return RoundSettings.get(roomService.settings(room));
-    }
-
-    private LevelsSettingsImpl levelsSettings(String room) {
-        return LevelsSettings.get(roomService.settings(room));
-    }
-
-    private void setEnable(List<Parameter> games) {
-        List<String> opened = new LinkedList<>();
-        List<String> allGames = gameService.getGames();
-        if (games.size() != allGames.size()) {
-            throw new IllegalStateException("Список игр к активации регистрации не полный");
-        }
-        for (int i = 0; i < allGames.size(); i++) {
-            if (games.get(i) != null) {
-                opened.add(allGames.get(i));
-            }
-        }
-
-        // TODO #4FS тут проверить
-        roomService.setOpenedGames(opened);
-    }
-
-    public void updateParameters(Settings gameSettings, Predicate<Parameter> filter,
-                                 List<Object> updated, List<Exception> errors)
-    {
-        List<Parameter> actual = gameSettings.getParameters().stream()
-                .filter(filter)
-                .collect(toList());
-        for (int index = 0; index < actual.size(); index++) {
-            try {
-                Parameter parameter = actual.get(index);
-                Object value = updated.get(index);
-                value = fixForCheckbox(parameter, value);
-                parameter.update(value);
-            } catch (Exception e) {
-                errors.add(e);
-            }
-        }
-    }
-
-    public Predicate<Parameter> onlyUngrouped() {
-        return Predicate.not(
-                p -> p.getName().startsWith(SEMIFINAL)
-                        || p.getName().startsWith(ROUNDS)
-                        || p.getName().startsWith(LEVELS)
-                        || p.getName().startsWith(INACTIVITY));
-    }
-
-    public Predicate<Parameter> onlyLevels() {
-        return p -> p.getName().startsWith(LEVELS);
-    }
-
-    public void generateNewPlayers(String game, String room, String mask, int count) {
-        int numLength = String.valueOf(count).length();
-
-        int created = 0;
-        int index = 0;
-        while (created != count) {
-            String number = StringUtils.leftPad(String.valueOf(++index), numLength, "0");
-            String id = mask.replaceAll("%", number);
-
-            if (playerService.contains(id) && index < playerService.getAll().size()) {
-                continue;
-            }
-
-            created++;
-            String code = register(id);
-            playerService.register(id, game, room, "127.0.0.1");
-        }
-    }
-
-    private String register(String id) {
-        if (registration.registered(id)) {
-            return registration.login(id, id);
-        } else {
-            return registration.register(id, id, id, id, "", GameAuthorities.USER.roles()).getCode();
-        }
-    }
-
-    private Object fixForCheckbox(Parameter parameter, Object value) {
-        if (value == null && parameter.getType().equals(CheckBox.TYPE)) {
-            return false; // потому что так работает <form:checkbox
-        }
-        return value;
-    }
-
-    // ----------------
 
     private String getAdmin(HttpServletRequest request) {
         return getAdmin(getGameRoom(request));
@@ -533,8 +342,6 @@ public class AdminController {
     private String getAdmin() {
         return getAdmin(gameService.getDefaultRoom());
     }
-
-    // ----------------
 
     @GetMapping()
     public String getAdmin(Model model,
@@ -598,47 +405,11 @@ public class AdminController {
         model.addAttribute("autoSave", autoSaver.isWorking());
         model.addAttribute("debugLog", debugService.isWorking());
         model.addAttribute("opened", playerService.isRegistrationOpened());
-        model.addAttribute("adminSettings", getAdminSettings(gameType, room));
+        model.addAttribute("adminSettings", adminService.getAdminSettings(gameType, room));
         model.addAttribute("gamesRooms", roomService.gamesRooms());
         model.addAttribute("playersCount", playerService.getRoomCounts());
 
         return "admin";
-    }
-
-    public AdminSettings getAdminSettings(GameType gameType, String room) {
-        AdminSettings result = new AdminSettings();
-
-        // сохраняем для отображения semifinal settings pojo
-        result.setSemifinal(semifinalSettings(room)); // TODO тут снова берем getSettings().getParameters()
-        // сохраняем для отображения round settings pojo
-        result.setRounds(roundSettings(room)); // TODO тут снова берем getSettings().getParameters()
-        // сохраняем для отображения round settings pojo
-        LevelsSettingsImpl levels = levelsSettings(room);  // TODO тут снова берем getSettings().getParameters()
-        result.setLevels(levels); // TODO а точно тут нада эту строчку?
-        result.setLevelsValues(levels
-                .getParameters().stream()
-                .map(Parameter::getValue)
-                .collect(toList()));
-        // сохраняем для отображения inactivity settings pojo
-        result.setInactivity(adminService.inactivitySettings(room));
-        // отдельно оставшиеся параметры
-        List<Parameter> parameters = gameType.getSettings().getParameters();
-        parameters.removeIf(Predicate.not(onlyUngrouped()));
-        result.setOther(parameters);
-        result.setOtherValues(parameters.stream()
-                .map(Parameter::getValue)
-                .collect(toList()));
-
-        // TODO #4FS тут проверить
-        List<String> enabled = roomService.openedGames();
-        result.setGames(gameService.getGames().stream()
-                .map(name -> enabled.contains(name))
-                .collect(toList()));
-
-        List<PlayerInfo> saves = saveService.getSaves(room);
-        result.setPlayers(preparePlayers(room, saves));
-
-        return result;
     }
 
     private String getGameRoom(HttpServletRequest request) {
@@ -657,13 +428,4 @@ public class AdminController {
         return result;
     }
 
-    private List<PlayerInfo> preparePlayers(String room, List<PlayerInfo> players) {
-        for (PlayerInfo player : players) {
-            player.setHidden(!room.equals(player.getRoom()));
-        }
-
-        return players;
-    }
-
-    // ----------------
 }
