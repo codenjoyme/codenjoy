0bb9c2686fde61dc92b31f9c99ac81ea4e9c22af
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-18 21:43:52 +0300

    [clients][go] Completely reorganized the build scripts.

--- CodingDojo/clients/0-settings.bat
+++ CodingDojo/clients/0-settings.bat
@@ -18,23 +18,15 @@ echo on
 rem Set to true if you want to ignore jdk and maven installed on the system
 if "%INSTALL_LOCALLY%"=="" ( set INSTALL_LOCALLY=true)
 
-rem if "%INSTALL_LOCALLY%"=="true" ( set TOOLS=%ROOT%\.tools)
-rem if "%INSTALL_LOCALLY%"=="true" ( set ARCH=%TOOLS%\7z\7za.exe)
-rem if "%INSTALL_LOCALLY%"=="true" ( set NPM=%NODE_HOME%\npm)
-rem if "%INSTALL_LOCALLY%"=="true" ( set GOPATH=%ROOT%\.golang\go)
-rem if "%INSTALL_LOCALLY%"=="true" ( set GO=%GOPATH%\bin\go)
 rem if "%INSTALL_LOCALLY%"=="true" ( set GIT_HOME=%ROOT%\.gitTool)
 rem if "%INSTALL_LOCALLY%"=="true" ( set GIT=%GIT_HOME%\cmd\git)
 
-rem if "%GO_CLIENT_HOME%"==""         ( set GO_CLIENT_HOME=%ROOT%\go)
 rem if "%PHP_CLIENT_HOME%"==""        ( set PHP_CLIENT_HOME=%ROOT%\php)
 rem if "%PHP_HOME%"==""               ( set PHP_HOME=%ROOT%\.php)
 rem if "%PYTHON_CLIENT_HOME%"==""     ( set PYTHON_CLIENT_HOME=%ROOT%\python)
 rem if "%PYTHON_HOME%"==""            ( set PYTHON_HOME=%ROOT%\.python)
 
 rem if "%ARCH_GIT%"==""          ( set ARCH_GIT=https://github.com/git-for-windows/git/releases/download/v2.18.0.windows.1/MinGit-2.18.0-64-bit.zip)
-rem if "%ARCH_GO%"==""           ( set ARCH_GO=https://golang.org/dl/go1.16.5.windows-amd64.zip)
-rem if "%ARCH_GO_FOLDER%"==""    ( set ARCH_GO_FOLDER=go)
 rem if "%ARCH_PHP%"==""          ( set ARCH_PHP=https://windows.php.net/downloads/releases/archives/php-8.0.8-nts-Win32-vs16-x64.zip)
 rem if "%ARCH_PHP_COMPOSER%"=="" ( set ARCH_PHP_COMPOSER=https://getcomposer.org/download/latest-2.x/composer.phar)
 rem if "%ARCH_PYTHON%"==""       ( set ARCH_PYTHON=https://www.python.org/ftp/python/3.9.6/python-3.9.6-embed-amd64.zip)
