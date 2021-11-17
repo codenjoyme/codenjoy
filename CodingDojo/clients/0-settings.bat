bd766f534f7ef21aa649d074e4f8c4d83d44f9fc
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 10:18:57 +0200

    [clients][bat] Reordering lines.

--- CodingDojo/clients/0-settings.bat
+++ CodingDojo/clients/0-settings.bat
@@ -2,6 +2,9 @@ if "%GAME_TO_RUN%"=="" ( set GAME_TO_RUN=mollymage)
 if "%BOARD_URL%"==""   ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000)
 if "%LANGUAGE%"==""    ( set LANGUAGE=java)
 
+rem Set to true if you want to ignore dev-tools installed on the System
+if "%INSTALL_LOCALLY%"=="" ( set INSTALL_LOCALLY=true)
+
 set CLIENTS_ROOT=%CD%
 
 if "%SKIP_TESTS%"=="" ( set SKIP_TESTS=true)
@@ -14,6 +17,3 @@ echo        [44;93mGAME_TO_RUN=%GAME_TO_RUN%[0m
 echo        [44;93mBOARD_URL=%BOARD_URL%[0m
 echo        [44;93mLANGUAGE=%LANGUAGE%[0m
 echo on
-
-rem Set to true if you want to ignore jdk and maven installation on the system
-if "%INSTALL_LOCALLY%"=="" ( set INSTALL_LOCALLY=true)
