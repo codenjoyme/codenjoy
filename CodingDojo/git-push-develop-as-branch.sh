b5c45cb93718d0425e40da4bc65ad037a644e2b1
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-15 00:45:29 +0200

    [build][git] Updated push script.

--- CodingDojo/git-push-develop-as-branch.sh
+++ CodingDojo/git-push-develop-as-branch.sh
@@ -12,13 +12,18 @@ eval_echo "`ssh-agent -s`"
 eval_echo "ssh-add ~/.ssh/*_rsa"
 
 echo Please enter branch name
-read BRANCH
+read branch
 
-eval_echo "git submodule foreach git push origin master"
-eval_echo "git checkout -B $BRANCH"
-eval_echo "git push origin $BRANCH"
+echo Do we need to commit submudules: y/n?
+read submodule_commit
+if [[ "$submodule_commit" == "y" ]]; then
+	eval_echo "git submodule foreach git push origin master"
+fi
+
+eval_echo "git checkout -B $branch"
+eval_echo "git push origin $branch"
 eval_echo "git checkout develop"
-eval_echo "git branch -D $BRANCH"
+eval_echo "git branch -D $branch"
 
 echo Press any key to continue
 read
