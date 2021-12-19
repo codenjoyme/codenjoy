659dd7c57992cb37319e0e6e7501903dd5e0779b
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-12-19 12:01:55 +0200

    [clients][all][build][bash][batch] Do not set LANGUAGE if already defined.

--- CodingDojo/clients/build/run.sh
+++ CodingDojo/clients/build/run.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-eval_echo() {
-    command=$1
-    color=94 # blue
-    echo "[${color}m$command[0m"
-    echo
-    eval $command
-}
-
-LANGUAGE=python
+if [[ "$LANGUAGE" == "" ]]; then
+    LANGUAGE=java
+fi
 cd ./../$LANGUAGE/build
 bash run.sh
 
 echo Press Enter to continue
-read
+pause >nul
