ac1742eb187428c0ab28d4ea26f5672482d365f1
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-04 01:33:55 +0200

    [git][build] Fixed script after moving repo folder to CodingDojo/..

--- CodingDojo/build/git-snapshot-push.sh
+++ CodingDojo/build/git-snapshot-push.sh
@@ -9,6 +9,7 @@ eval_echo() {
 }
 
 eval_echo "OUT=$(pwd)/out"
+eval_echo "mkdir $OUT"
 eval_echo "cd .."
 eval_echo "PROJECT_ROOT=$(pwd)"
 eval_echo "cd .."
@@ -18,21 +19,16 @@ eval_echo "REPO=$GIT_ROOT/repo"
 eval_echo "`ssh-agent -s`"
 eval_echo "ssh-add ~/.ssh/*_rsa"
 
-eval_echo "$PROJECT_ROOT/mvnw -f $PROJECT_ROOT/pom.xml -DaltDeploymentRepository=snapshots::default::file:$REPO/snapshots clean deploy -DskipTests=true -DgitDir=$GIT_ROOT 2>&1 | tee $OUT/snapshot-deploy.log" 
+eval_echo "cd $PROJECT_ROOT"
+eval_echo "./mvnw -f ./pom.xml -DaltDeploymentRepository=snapshots::default::file:$REPO/snapshots clean deploy -DskipTests=true -DgitDir=$GIT_ROOT 2>&1 | tee $OUT/snapshot-deploy.log" 
 
-eval_echo "cd ../repo"
-
-eval_echo "mv .git_ .git"
-eval_echo "mv .gitignore_ .gitignore"
+eval_echo "cd $REPO"
 
 eval_echo "git add ."
 eval_echo "git commit -m'New release'"
 eval_echo "git push origin master"
 
-eval_echo "mv .git .git_"
-eval_echo "mv .gitignore .gitignore_"
-
-eval_echo "cd .."
+eval_echo "cd $OUT/.."
 
 echo Press Enter to continue
 read
