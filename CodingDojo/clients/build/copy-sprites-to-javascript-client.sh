622f405b578457bba625714b7a45fcb6f6f60656
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-09 01:32:04 +0200

    [clients][build] Added script to copy sprites to the js-client.

---
+++ CodingDojo/clients/build/copy-sprites-to-javascript-client.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2012 - 2022 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+BLUE=94
+GRAY=89
+
+eval_echo() {
+    command=$1
+    if [[ "$2" == "" ]]; then
+        color=$BLUE
+    else
+        color=$2
+    fi
+    echo "[${color}m$command[0m"
+    echo
+    eval $command
+}
+
+color() {
+    message=$1
+    echo "[93m$message[0m"
+}
+
+copy_from_game() {
+   game=$1
+   from=$ROOT/../games/$game/src/main/webapp/resources/$game/sprite
+   to=$ROOT/java-script/games/$game/sprites
+   eval_echo "rm -rf $to" $GRAY
+   eval_echo "mkdir $to" $GRAY
+   eval_echo "cp $from/*.* $to" $GRAY
+}
+
+eval_echo "ROOT=$(pwd)/.."
+eval_echo "copy_from_game 'clifford'"
+eval_echo "copy_from_game 'mollymage'"
+eval_echo "copy_from_game 'sample'"
+eval_echo "copy_from_game 'verland'"
+eval_echo "copy_from_game 'rawelbbub'"
+
+echo
+color "Press Enter to continue"
+read
