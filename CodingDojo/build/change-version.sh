3d44ff84c4777011e6b198f6a3245aa47973d955
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-11 01:30:01 +0200

    [utilities][build] Added change version script.

---
+++ CodingDojo/build/change-version.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2012 - 2022 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+BLUE=94
+GRAY=89
+YELLOW=93
+
+color() {
+    message=$1
+    [[ "$2" == "" ]] && color=$YELLOW || color=$2
+    echo "[${color}m${message}[0m"
+}
+
+eval_echo() {
+    command=$1
+    [[ "$2" == "" ]] && color=$BLUE || color=$2
+    color "${command}" $color
+    echo
+    eval $command
+}
+
+check_machine() {
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        CYGWIN*)    machine=Cygwin;;
+        MINGW*)     machine=MinGw;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo ${machine}
+}
+
+parameter_prefix() {
+    machine=$(check_machine)
+    if [[ "$machine" == "Mac" || "$machine" == "Linux" ]]; then
+        echo --
+    else
+        echo -D
+    fi
+}
+
+eval_echo "from=1.1.1"
+eval_echo "to=1.1.1"
+
+eval_echo "ROOT=$(pwd)/.."
+
+eval_echo "cd $ROOT/utilities"
+eval_echo "preffix=$(parameter_prefix)"
+eval_echo "../mvnw -f ./pom.xml clean compile -Pchange-version ${preffix}replace.from=$from ${preffix}replace.to=$to"
+
+eval_echo "cd $ROOT"
+
+color "Press Enter to continue"
+read
--- CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
+++ CodingDojo/utilities/src/main/groovy/com/codenjoy/dojo/build/ChangeVersion.groovy
@@ -1,22 +1,62 @@
-def fromVersion = '1.1.1'
-def toVersion = '1.1.2'
+package com.codenjoy.dojo.build
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2012 - 2022 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+def fromVersion = properties['replace.from']
+def toVersion = properties['replace.to']
+if (fromVersion == null || toVersion == null) {
+    println "Please set 'replace.from' and 'replace.to' env variables"
+    return
+}
 
 // select root directories
-def base = '..\\..\\..\\..\\..\\..\\..\\..\\';
+def base = new File('.').canonicalPath.contains('src')
+        ? '../../../../../../../../'
+        : '../'
+
+println "Processing folder: " + new File(base).canonicalPath
+println "Replace '$fromVersion' to '$toVersion'"
+
 def dirs = []
 new File(base).eachDir() { dir ->
-    dirs << dir.path
+    dirs << dir.canonicalPath
 }
 
 // extract games directory
 def dirs2 = []
-dirs.each {
-    if (it.contains('games')) {
-        new File(it).eachDir {
-            dirs2 << it.path
+process(dirs, 'games', dirs2)
+process(dirs, 'clients', dirs2)
+
+private List<Object> process(ArrayList dirs, folder, dirs2) {
+    dirs.each {
+        if (it.contains(folder)) {
+            new File(it).eachDir {
+                dirs2 << it.canonicalPath
+            }
+        } else {
+            dirs2.contains(it) ? null : dirs2 << it
         }
-    } else {
-        dirs2 << it
     }
 }
 
@@ -35,28 +75,28 @@ def files = dirs3.collect {
 } + dirs3.collect {
     new File("$it/README.md")
 }
-files << new File("${base}games\\engine\\setup.bat")
-files << new File("${base}games\\engine\\setup.sh")
-files << new File("${base}portable\\windows-cmd\\00-settings.bat")
-files << new File("${base}portable\\linux-docker-compose\\balancer.yml")
-files << new File("${base}portable\\linux-docker-compose\\codenjoy.yml")
-files << new File("${base}portable\\linux-docker-compose\\rebuild.sh")
-files << new File("${base}portable\\linux-docker\\2-build.sh")
-files << new File("${base}portable\\linux-docker\\3-start.sh")
-files << new File("${base}portable\\linux-docker\\run.sh")
-files << new File("${base}games\\pom.xml")
-files << new File("${base}games\\README.md")
+files << new File("${base}games/engine/build/setup.bat")
+files << new File("${base}games/engine/build/setup.sh")
+
+files << new File("${base}portable/windows-cmd/00-settings.bat")
+
+files << new File("${base}portable/linux-docker-compose/.env")
+files << new File("${base}portable/linux-docker-compose/README.md")
+
+files << new File("${base}portable/linux-docker/2-build.sh")
+files << new File("${base}portable/linux-docker/3-start.sh")
+files << new File("${base}portable/linux-docker/README.md")
+files << new File("${base}portable/linux-docker/run.sh")
+
 files << new File("${base}pom.xml")
 files << new File("${base}README.md")
+
+Collections.sort(files)
 
 // replace in all files
 files.each {
     if (!it.exists()) return
-    println it
-    def text = it.text
-    for (i in 1..5) {
-        text = (text =~ /$fromVersion/).replaceFirst("$toVersion")
-    }
-    it.write(text)
-}
+    println "Processing file: " + it.canonicalPath
 
+    it.text = it.text.replace(fromVersion, toVersion)
+}
