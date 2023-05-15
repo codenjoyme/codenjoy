9d2b8dde3652c0d264ecda60fe9eaf290431e6ed
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-05-15 10:17:29 +0300

    [engine][server] Refactoring. Moved ChatPost to engine.

--- CodingDojo/server/src/main/java/com/codenjoy/dojo/services/chat/ChatPost.java
+++
@@ -1,8 +0,0 @@
-package com.codenjoy.dojo.services.chat;
-
-public interface ChatPost {
-
-    void postFieldFor(String recipientId, String text, String room);
-
-    void postField(String text, String room);
-}
